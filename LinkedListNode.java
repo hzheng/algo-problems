533e1fae7525ed1b44dd553c83a483efee7ffe55
Hui Zheng <xyzdll@gmail.com>
2016-04-04 18:43:14 +0800

    factor class LinkedListNode out

---
+++ LinkedListNode.java
@@ -0,0 +1,41 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class LinkedListNode
+{
+    Object data;
+    LinkedListNode next;
+
+    public LinkedListNode(Object data) {
+        this.data = data;
+    }
+
+    public LinkedListNode(Object[] array) {
+        if (array == null || array.length == 0) return;
+
+        data = array[0];
+        LinkedListNode tail = this;
+        for (int i = 1; i < array.length; i++) {
+            tail.next = new LinkedListNode(array[i]);
+            tail = tail.next;
+        }
+    }
+
+    public boolean equals(LinkedListNode that) {
+        if (that == null) return false;
+
+        if (data == null && that.data == null) return true;
+
+        if (data == null && that.data != null) return false;
+
+        return data.equals(that.data);
+    }
+
+    public Object[] toArray() {
+        List<Object> list = new ArrayList<Object>();
+        for (LinkedListNode l = this; l != null; l = l.next) {
+            list.add(l.data);
+        }
+        return list.toArray();
+    }
+}
--- RemoveDupLink.java
+++ RemoveDupLink.java
@@ -10,43 +10,6 @@ import static org.junit.Assert.*;
  * Cracking the Coding Interview(5ed) Problem 2.1:
  * Write code to remove duplicates from an unsorted linked list.
  */
-class LinkedListNode {
-    Object data;
-    LinkedListNode next;
-
-    public LinkedListNode(Object data) {
-        this.data = data;
-    }
-
-    public LinkedListNode(Object[] array) {
-        if (array == null || array.length == 0) return;
-
-        data = array[0];
-        LinkedListNode tail = this;
-        for (int i = 1; i < array.length; i++) {
-            tail.next = new LinkedListNode(array[i]);
-            tail = tail.next;
-        }
-    }
-
-    public boolean equals(LinkedListNode that) {
-        if (that == null) return false;
-
-        if (data == null && that.data == null) return true;
-
-        if (data == null && that.data != null) return false;
-
-        return data.equals(that.data);
-    }
-
-    public Object[] toArray() {
-        List<Object> list = new ArrayList<Object>();
-        for (LinkedListNode l = this; l != null; l = l.next) {
-            list.add(l.data);
-        }
-        return list.toArray();
-    }
-}
 
 public class RemoveDupLink {
     /**
