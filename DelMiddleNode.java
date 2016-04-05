3eeb36955aa92f55716dafc4b4e007b75bf6bdc5
Hui Zheng <xyzdll@gmail.com>
2016-04-05 09:55:12 +0800

    add DelMiddleNode

---
+++ DelMiddleNode.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Cracking the Coding Interview(5ed) Problem 2.3:
+ * Delete a node in the middle of a singly linked list,
+ * given only access to that node.
+ */
+public class DelMiddleNode {
+    // time complexity: O(1), space complexity: O(1)
+    public void delMiddle(LinkedListNode middle) {
+        if (middle == null || middle.next == null)
+            throw new IllegalArgumentException();
+
+        middle.data = middle.next.data;
+        middle.next = middle.next.next;
+    }
+}
