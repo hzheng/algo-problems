aa973cf32a18ccfeb10410e274d32f2c90829a33
Hui Zheng <xyzdll@gmail.com>
2016-04-08 00:04:43 +0800

    add StackWithMin.java

---
+++ StackWithMin.java
@@ -0,0 +1,40 @@
+import java.util.Stack;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Cracking the Coding Interview(5ed) Problem 3.2:
+ * Design a stack which, in addition to push and pop, also has a function min
+ * which returns the minimum element. Push, pop and min operate in 0(1) time.
+ */
+public class StackWithMin {
+    Stack<Integer> data = new Stack<Integer>();
+    Stack<Integer> mins = new Stack<Integer>();
+
+    public void push(int value) {
+        data.push(value);
+        if (value <= min()) {
+            mins.push(value);
+        }
+    }
+
+    public int pop() {
+        int value = data.pop();
+        if (value == mins.peek()) {
+            mins.pop();
+        }
+        return value;
+    }
+
+    public int min() {
+        if (mins.isEmpty()) {
+            return Integer.MAX_VALUE;
+        }
+        return mins.peek();
+    }
+
+    public static void main(String[] args) {
+        // org.junit.runner.JUnitCore.main("StackWithMin");
+    }
+}
