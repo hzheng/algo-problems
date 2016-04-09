9acab95b7f03da7f9c9d99db1302df1722044dd7
Hui Zheng <xyzdll@gmail.com>
2016-04-09 22:10:29 +0800

    add AnimalShelter.java

---
+++ AnimalShelter.java
@@ -0,0 +1,83 @@
+import java.util.LinkedList;
+import java.util.Arrays;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Cracking the Coding Interview(5ed) Problem 3.7:
+ * An animal shelter holds only dogs and cats, and operates on a FIFO basis.
+ * People must adopt either the "oldest" (based on arrival time) of all animals
+ * at the shelter, or they can select whether they would prefer a dog or a cat
+ * (and will receive the oldest animal of that type). Create a data structures
+ * to maintain this system and implement operations: enqueue, dequeueAny,
+ * dequeueDog and dequeueCat.
+ */
+public class AnimalShelter {
+    public abstract class Animal {
+        private int id;
+        private int order;
+
+        public Animal(int id) {
+            this.id = id;
+        }
+
+        public void setOrder(int order) {
+            this.order = order;
+        }
+
+        public boolean olderThan(Animal other) {
+            return order < other.order;
+        }
+    }
+
+    public class Cat extends Animal {
+        Cat(int id) {
+            super(id);
+        }
+    }
+
+    public class Dog extends Animal {
+        Dog(int id) {
+            super(id);
+        }
+    }
+
+    private LinkedList<Dog> dogs = new LinkedList<Dog>();
+    private LinkedList<Cat> cats = new LinkedList<Cat>();
+    int order = 0;
+
+    public void enqueue(Animal animal) {
+        animal.setOrder(++order);
+        if (animal instanceof Dog) {
+            dogs.addLast((Dog)animal);
+        } else if (animal instanceof Cat) {
+            cats.addLast((Cat)animal);
+        }
+    }
+
+    public Animal dequeueAny() {
+        if (dogs.size() == 0) {
+            return cats.poll();
+        }
+        if (cats.size() == 0) {
+            return dogs.poll();
+        }
+        if (dogs.peek().olderThan(cats.peek())) {
+            return dogs.poll();
+        } else {
+            return cats.poll();
+        }
+    }
+
+    public Dog dequeueDog() {
+        return dogs.removeFirst();
+    }
+
+    public Cat dequeueCat() {
+        return cats.removeFirst();
+    }
+
+    public static void main(String[] args) {
+    }
+}
