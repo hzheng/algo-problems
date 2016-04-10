70c0c105d3db0f77ccd35bcd5c1c1b2f0d12d07c
Hui Zheng <xyzdll@gmail.com>
2016-04-10 16:06:43 +0800

    add MinBST.java

---
+++ tree_graph/MinBST.java
@@ -0,0 +1,39 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import tree_graph.TreeNode;
+
+/**
+ * Cracking the Coding Interview(5ed) Problem 4.3:
+ * Given an increasing order array with unique integer elements, create a
+ * binary search tree with minimal height.
+ */
+public class MinBST {
+    private static TreeNode createMinBST(int[] n, int start, int end) {
+        if (start > end) return null;
+
+        int middle = (end + start) / 2;
+        TreeNode node = new TreeNode(n[middle]);
+        node.left = createMinBST(n, start, middle - 1);
+        node.right = createMinBST(n, middle + 1, end);
+        return node;
+    }
+
+    public static TreeNode createMinBST(int[] n) {
+        return createMinBST(n, 0, n.length - 1);
+    }
+
+    void test(int[] n, int[] expected) {
+        TreeNode tree = createMinBST(n);
+        // assertArrayEquals(expected, treeArray);
+    }
+
+    @Test
+    public void test1() {
+        test(new int[] {1}, new int[] {1});
+    }
+
+    public static void main(String[] args) {
+        org.junit.runner.JUnitCore.main("MinBST");
+    }
+}
--- tree_graph/TreeNode.java
+++ tree_graph/TreeNode.java
@@ -4,7 +4,15 @@ public class TreeNode
 {
     public TreeNode left;
     public TreeNode right;
+    public int data;
 
+    public TreeNode() {
+    }
+
+    public TreeNode(int data) {
+        this.data = data;
+    }
+    
     public static void main(String[] args) {
     }
 }
