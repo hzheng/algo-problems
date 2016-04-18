05401fca057abf4e50720cf743ef72b2aa725e41
Hui Zheng <xyzdll@gmail.com>
2016-04-18 21:54:50 +0800

    add one more solution for StrPermute

--- recursion_DP/StrPermute.java
+++ recursion_DP/StrPermute.java
@@ -37,22 +37,49 @@ public class StrPermute {
         return builder.toString();
     }
 
+    public static List<String> permutate2(String str) {
+        List<String> perms = new ArrayList<String>();
+        permutate2("", str, perms);
+        return perms;
+    }
+
+    private static void permutate2(String prefix, String str,
+                                   List<String> perms) {
+        int len = str.length();
+        if (len == 0) {
+            perms.add(prefix);
+            return;
+        }
+
+        for (int i = 0; i < len; i++) {
+            permutate2(prefix + str.charAt(i),
+                       str.substring(0, i) + str.substring(i + 1, len), perms);
+        }
+    }
+
     private static int factorial(int n) {
         return (n == 0) ? 1 : n * factorial(n - 1);
     }
 
-    private void test(String str, Function<String, List<String>> perm) {
+    private void test(String str, Function<String, List<String> > perm) {
+        long t = System.nanoTime();
         List<String> perms = perm.apply(str);
-        for (String s : perms) {
-            System.out.println(s);
+        System.out.format("%.03f ms\n", (System.nanoTime() - t) * 1e-6);
+        if (str.length() < 5) {
+            for (String s : perms) {
+                System.out.println(s);
+            }
         }
         assertEquals(factorial(str.length()), perms.size());
     }
 
     @Test
     public void test() {
+        String str = "123456";
         System.out.println("test permutate");
-        test("abcd", StrPermute::permutate);
+        test(str, StrPermute::permutate);
+        System.out.println("test permutate2");
+        test(str, StrPermute::permutate2);
     }
 
     public static void main(String[] args) {
