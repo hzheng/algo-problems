a36d2e33c512b09917c13b4175fc7cf2d70daaf5
Hui Zheng <xyzdll@gmail.com>
2016-05-02 18:11:31 +0800

    add SumNode.java

---
+++ leetcode/SumNode.java
@@ -0,0 +1,61 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+public class SumNode {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null || l2 == null) return null;
+
+        ListNode sumNode = null;
+        ListNode sumCur = null;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int v1 = (l1 == null) ? 0 : l1.val;
+            int v2 = (l2 == null) ? 0 : l2.val;
+            int sum = v1 + v2 + carry;
+            carry = sum / 10;
+            ListNode partialSum = new ListNode(sum % 10);
+            if (sumNode == null) {
+                sumNode = partialSum;
+            } else {
+                sumCur.next = partialSum;
+            }
+            sumCur = partialSum;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry > 0) {
+            sumCur.next = new ListNode(1);
+        }
+        return sumNode;
+    }
+
+    // from the answer
+    public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
+}
