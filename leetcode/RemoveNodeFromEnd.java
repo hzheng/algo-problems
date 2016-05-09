9d068dcbdabbd740ed54e0f861a9593a00d11808
Hui Zheng <xyzdll@gmail.com>
2016-05-09 23:32:48 +0800

    add RemoveNodeFromEnd.java

---
+++ leetcode/RemoveNodeFromEnd.java
@@ -0,0 +1,57 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+// Given a linked list, remove the nth node from the end of list.
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+public class RemoveNodeFromEnd {
+    // beats 7.54%
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode tail = head;
+        for (int i = n; i > 0; i--, tail = tail.next) {
+            // if (tail == null) return head; // non-null guaranteed by req.
+        }
+        if (tail == null) return head.next;
+
+        ListNode oldHead = head;
+        while (tail.next != null) {
+            tail = tail.next;
+            head = head.next;
+        }
+        head.next = head.next.next;
+        return oldHead;
+    }
+
+    // from the solution
+    // beats 7.54%
+    public ListNode removeNthFromEnd2(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode first = dummy;
+        ListNode second = dummy;
+        for (int i = n; i >= 0; i--) {
+            first = first.next;
+        }
+
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummy.next;
+    }
+
+    @Test
+    public void test1() {
+    }
+
+    public static void main(String[] args) {
+        org.junit.runner.JUnitCore.main("RemoveNodeFromEnd");
+    }
+}
