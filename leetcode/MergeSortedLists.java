d391c20c12702258a15c42e82585b6fe0631cee1
Hui Zheng <xyzdll@gmail.com>
2016-05-10 00:33:50 +0800

    add MergeSortedLists.java

---
+++ leetcode/MergeSortedLists.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+// Merge two sorted linked lists and return it as a new list.
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+public class MergeSortedLists {
+    // beats 11.81%
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        for (ListNode cur = dummy; l1 != null || l2 != null; cur = cur.next) {
+            if (l1 == null || (l2 != null && l1.val > l2.val)) {
+                cur.next = l2;
+                l2 = l2.next;
+            } else {
+                cur.next = l1;
+                l1 = l1.next;
+            }
+        }
+        return dummy.next;
+    }
+
+    ListNode createList(int[] l) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        for (int i : l) {
+            cur.next = new ListNode(i);
+            cur = cur.next;
+        }
+        return dummy.next;
+    }
+
+    int[] toArray(ListNode l) {
+        List<Integer> list = new ArrayList<>();
+        for (; l != null; l = l.next) {
+            list.add(l.val);
+        }
+        return list.stream().mapToInt(i->i).toArray();
+    }
+
+    void test(int[] n1, int[] n2, int[] expected) {
+        ListNode l1 = createList(n1);
+        ListNode l2 = createList(n2);
+        ListNode l = mergeTwoLists(l1, l2);
+        assertArrayEquals(expected, toArray(l));
+    }
+
+    @Test
+    public void test1() {
+        test(new int[]{1, 3, 5}, new int[]{2, 4, 6},
+            new int[] {1, 2, 3, 4, 5, 6});
+        test(new int[]{3}, new int[]{2, 4, 6}, new int[] {2, 3, 4, 6});
+        test(new int[]{}, new int[]{2, 4, 6}, new int[] {2, 4, 6});
+        test(new int[]{2, 10}, new int[]{}, new int[] {2, 10});
+    }
+
+    public static void main(String[] args) {
+        org.junit.runner.JUnitCore.main("MergeSortedLists");
+    }
+}
