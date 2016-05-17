5124b5eeae36bf17c18567e8adacfafd672a3847
Hui Zheng <xyzdll@gmail.com>
2016-05-17 17:59:46 +0800

    add one more solution for NextPermutation

--- leetcode/NextPermutation.java
+++ leetcode/NextPermutation.java
@@ -28,16 +28,36 @@ public class NextPermutation {
         }
     }
 
-    void swap(int[] nums, int i, int j) {
+    // beats 69.33%
+    public void nextPermutation2(int[] nums) {
+        int len = nums.length;
+        int i = len - 1;
+        for (; i > 0 && nums[i - 1] >= nums[i]; i--);
+
+        if (i > 0) {
+            int j = len - 1;
+            for ( ; j >= 0 && nums[j] <= nums[i - 1]; j--) ;
+            swap(nums, i - 1, j);
+        }
+        // reverse from i to the end
+        for (int j = len - 1; i < j; i++, j--) {
+            swap(nums, i, j);
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
         int tmp = nums[i];
         nums[i] = nums[j];
         nums[j] = tmp;
     }
 
     void test(int[] nums, int[] expected) {
-        nextPermutation(nums);
-        System.out.println(Arrays.toString(nums));
+        int[] input = nums.clone();
+        nextPermutation(input);
+        assertArrayEquals(expected, input);
+        nextPermutation2(nums);
         assertArrayEquals(expected, nums);
+        System.out.println(Arrays.toString(nums));
     }
 
     @Test
@@ -48,6 +68,7 @@ public class NextPermutation {
         test(new int[] {3, 2, 1}, new int[] {1, 2, 3});
         test(new int[] {1, 1, 5}, new int[] {1, 5, 1});
         test(new int[] {1, 3, 2}, new int[] {2, 1, 3});
+        test(new int[] {1, 5, 1}, new int[] {5, 1, 1});
     }
 
     public static void main(String[] args) {
