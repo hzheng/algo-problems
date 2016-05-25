174fcc959a9ccf075410e8408550d4eadeb63440
Hui Zheng <xyzdll@gmail.com>
2016-05-25 23:53:51 +0800

    add one more solution for ValidNumber

--- leetcode/ValidNumber.java
+++ leetcode/ValidNumber.java
@@ -50,8 +50,82 @@ public class ValidNumber {
         return !needDigit;
     }
 
+    // finite automata
+    // http://blog.csdn.net/kenden23/article/details/18696083
+    // beats 52.09%
+    public boolean isNumber2(String s) {
+        int i = 0;
+        // input values
+        final int SPACE = i++;
+        final int SIGN = i++;
+        final int DIGIT = i++;
+        final int DOT = i++;
+        final int EXP = i++;
+
+        // state values
+        i = -1;
+        final int ERROR = i++; // error state
+        final int INIT = i++;  // initial state
+        final int DIGIT_STATE = i++;   // digit state
+        final int DOT_STATE = i++;   // dot state
+        final int SIGN_STATE = i++;   // sign state
+        final int NUM_DOT = i++;   // digit+dot state
+        final int EXP_STATE = i++;   // exponential state
+        final int EXP_SIGN = i++;   // exponential+sign state
+        final int EXP_NUM = i++;   // exponential+digit state
+        final int TERM = i++;   // terminate after space
+
+        int transitionTable[][] = {
+            //SPACE SIGN        DIGIT        DOT        EXP
+            {INIT,  SIGN_STATE, DIGIT_STATE, DOT_STATE, ERROR},     // INIT
+            {TERM,  ERROR,      DIGIT_STATE, NUM_DOT,   EXP_STATE}, // DIGIT_STATE
+            {ERROR, ERROR,      NUM_DOT,     ERROR,     ERROR},     // DOT_STATE
+            {ERROR, ERROR,      DIGIT_STATE, DOT_STATE, ERROR},     // SIGN_STATE
+            {TERM,  ERROR,      NUM_DOT,     ERROR,     EXP_STATE}, // NUM_DOT
+            {ERROR, EXP_SIGN,   EXP_NUM,     ERROR,     ERROR},     // EXP_STATE
+            {ERROR, ERROR,      EXP_NUM,     ERROR,     ERROR},     // EXP_SIGN
+            {TERM,  ERROR,      EXP_NUM,     ERROR,     ERROR},     // EXP_NUM
+            {TERM,  ERROR,      ERROR,       ERROR,     ERROR}      // TERM
+        };
+        int state = 0;
+        for (char c : s.toCharArray()) {
+            int input;
+            switch (c) {
+            case ' ':
+                input = SPACE;
+                break;
+            case '+': case '-':
+                input = SIGN;
+                break;
+            case '.':
+                input = DOT;
+                break;
+            case 'e':
+                input = EXP;
+                break;
+            case '0': case '1': case '2': case '3': case '4':
+            case '5': case '6': case '7': case '8': case '9':
+                input = DIGIT;
+                break;
+            default:
+                return false;
+            }
+
+            state = transitionTable[state][input];
+            if (state == ERROR) return false;
+        }
+        return (state == DIGIT_STATE) || (state == NUM_DOT)
+               || (state == EXP_NUM) || (state == TERM);
+    }
+
+    // TODO: regular expression
+    // s = s.trim();
+    // if (s.matches("[+-]?\\d*\\.?\\d+")) return true;
+    // if (s.matches("[+-]?\\d+\\.?\\d*")) return true;
+
     void test(String s, boolean expected) {
         assertEquals(expected, isNumber(s));
+        assertEquals(expected, isNumber2(s));
     }
 
     @Test
@@ -74,6 +148,7 @@ public class ValidNumber {
         test("+12e1. ", false);
         test("+12e1 ", true);
         test("12e", false);
+        test("1.2e1", true);
         test("12e1", true);
         test("12e13e4", false);
     }
