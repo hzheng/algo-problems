3db8fa16604c3f34fabfdba8e75706be396d8903
Hui Zheng <xyzdll@gmail.com>
2016-05-25 17:24:55 +0800

    add AddBinary.java

---
+++ leetcode/AddBinary.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+// Given two binary strings, return their sum (also a binary string).
+public class AddBinary {
+    // beats 98.05%
+    public String addBinary(String a, String b) {
+        boolean carry = false;
+        int len1 = a.length();
+        int len2 = b.length();
+        int len = Math.max(len1, len2) + 1;
+        char[] res = new char[len];
+        for (int i = len1 - 1, j = len2 - 1, k = len - 1;
+             k > 0; i--, j--, k--) {
+            char d1 = (i >= 0) ? a.charAt(i) : '0';
+            char d2 = (j >= 0) ? b.charAt(j) : '0';
+            res[k] = ((d1 == d2) ^ carry) ? '0' : '1';
+            if (carry) {
+                carry = (d1 == '1') || (d2 == '1');
+            } else {
+                carry = (d1 == '1') && (d2 == '1');
+            }
+        }
+        if (carry) {
+            res[0] = '1';
+            return new String(res);
+        }
+        return new String(res, 1, len - 1);
+    }
+
+    void test(String a, String b, String expected) {
+        assertEquals(expected, addBinary(a, b));
+    }
+
+    @Test
+    public void test1() {
+        test("10", "11", "101");
+        test("10", "1", "11");
+        test("11", "1", "100");
+        test("10", "0", "10");
+        test("0", "10000", "10000");
+        test("1", "10000", "10001");
+        test("11", "11111", "100010");
+    }
+
+    public static void main(String[] args) {
+        org.junit.runner.JUnitCore.main("AddBinary");
+    }
+}
