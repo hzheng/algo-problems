5cc0236837d3261cd88583bd412bba790970443b
Hui Zheng <xyzdll@gmail.com>
2016-05-27 11:24:40 +0800

    add one more solution for ZeroMatrix

--- array_string/ZeroMatrix.java
+++ array_string/ZeroMatrix.java
@@ -8,6 +8,7 @@ import static org.junit.Assert.*;
  */
 public class ZeroMatrix {
     // time complexity: O(M*N), space complexity: O(M+N)
+    // beats 19.41%
     public void zeroMatrix(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
@@ -31,14 +32,65 @@ public class ZeroMatrix {
         }
     }
 
+    // time complexity: O(M*N), space complexity: O(1)
+    // beats 8.64%
+    public void zeroMatrix2(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean firstRowZero = false;
+        for (int i = 0; i < n; i++) {
+            if (matrix[0][i] == 0) {
+                firstRowZero = true;
+            }
+        }
+        boolean firstColZero = false;
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                firstColZero = true;
+            }
+        }
+
+        // make the topmost row and leftmost column to flags
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        // fill the inside grids
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        // fill the edge girds
+        if (firstRowZero) {
+            for (int i = 0; i < n; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+        if (firstColZero) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+
     void test(int[][] matrix, int[][] expected) {
-        zeroMatrix(matrix);
+        zeroMatrix2(matrix);
+        // zeroMatrix(matrix);
         assertArrayEquals(expected, matrix);
     }
 
     @Test
     public void test1() {
         test(new int[][] {{1}}, new int[][] {{1}});
+        test(new int[][] {{0, 1}}, new int[][] {{0, 0}});
+        test(new int[][] {{0}, {1}}, new int[][] {{0}, {0}});
     }
 
     @Test
