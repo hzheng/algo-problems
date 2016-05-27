a774165529c75f101e953aa126d40d372ae5bbeb
Hui Zheng <xyzdll@gmail.com>
2016-05-27 14:24:32 +0800

    add one more solutin for MatrixSearch

--- leetcode/MatrixSearch.java
+++ leetcode/MatrixSearch.java
@@ -99,6 +99,41 @@ public class MatrixSearch {
         return false;
     }
 
+    // beats 6.15%
+    public boolean searchMatrix3(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        // find row first
+        int low = 0;
+        int high = m - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (matrix[mid][0] == target) return true;
+
+            if (matrix[mid][0] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        int row = high;
+        if (row < 0) return false;
+
+        low = 0;
+        high = n - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (matrix[row][mid] == target) return true;
+
+            if (matrix[row][mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return false;
+    }
 
     @FunctionalInterface
     interface Function<A, B, C> {
@@ -117,11 +152,13 @@ public class MatrixSearch {
         for (int i = 0; i < tgts.length; i++) {
             test(m::searchMatrix, "searchMatrix", matrix, tgts[i], expected[i] > 0);
             test(m::searchMatrix2, "searchMatrix2", matrix, tgts[i], expected[i] > 0);
+            test(m::searchMatrix3, "searchMatrix3", matrix, tgts[i], expected[i] > 0);
         }
     }
 
     @Test
     public void test1() {
+        test(new int[][] {{1}}, new int[] {0}, new int[] {0});
         test(new int[][] {{1,   3,  5,  7},
                           {10, 11, 16, 20},
                           {23, 30, 34, 50}},
