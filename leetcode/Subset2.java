6d9686bd7ee8e12a9e019f5b4098d782027f9792
Hui Zheng <xyzdll@gmail.com>
2016-05-28 19:19:11 +0800

    add Subset2.java

--- leetcode/Subset2.java
+++ leetcode/Subset2.java
@@ -4,63 +4,34 @@ import java.util.function.Function;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-// Given a set of distinct integers, nums, return all possible subsets.
-public class Subset {
-    // beats 10.68%(15.24% if not using Arrays.binarySearch)
+// Given a collection of integers that might contain duplicates, return all
+// possible subsets.
+public class Subset2 {
+    // beats 41.85%
     public List<List<Integer> > subsets(int[] nums) {
         List<List<Integer> > res = new ArrayList<>();
         Arrays.sort(nums);
-        for (int i = 0; i <= nums.length; i++) {
-            subsets(nums, res, i, new ArrayList<Integer>());
-        }
+        subsets(nums, res, 0, new ArrayList<>());
         return res;
     }
 
-    private void subsets(int[] nums, List<List<Integer> > res, int total,
+    private void subsets(int[] nums, List<List<Integer> > res, int start,
                          List<Integer> cur) {
-        int count = cur.size();
-        if (count == total) {
-            res.add(new ArrayList<Integer>(cur));
-            return;
-        }
-
-        int nextIndex = 0;
-        if (count > 0) {
-            nextIndex = Arrays.binarySearch(
-                nums, count - 1, nums.length, cur.get(count - 1)) + 1;
-        }
-
-        for (int i = nextIndex; i < nums.length; i++) {
-            cur.add(nums[i]);
-            subsets(nums, res, total, cur);
-            cur.remove(count);
-        }
-    }
-
-    // beats 15.24%
-    public List<List<Integer> > subsets2(int[] nums) {
-        List<List<Integer> > res = new ArrayList<>();
-        Arrays.sort(nums);
-        subsets2(nums, res, 0, new ArrayList<>());
-        return res;
-    }
-
-    private void subsets2(int[] nums, List<List<Integer> > res, int start,
-                          List<Integer> cur) {
         int count = cur.size();
         res.add(new ArrayList<>(cur));
         for (int i = start; i < nums.length; i++) {
-            cur.add(nums[i]);
-            subsets2(nums, res, i + 1, cur);
-            cur.remove(count);
+            if (i == start || nums[i - 1] != nums[i]) {
+                cur.add(nums[i]);
+                subsets(nums, res, i + 1, cur);
+                cur.remove(count);
+            }
         }
     }
 
-    // from recursive_DP/Subsets.java
-    // beats 59.76%
-    public List<List<Integer> > subsets3(int[] nums) {
+    // beats 13.21%
+    public List<List<Integer> > subsets2(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer> > res = new ArrayList<>();
+        Set<List<Integer> > res = new HashSet<>();
         int n = nums.length;
         for (int i = ((1 << n) - 1); i >= 0; i--) {
             List<Integer> subset = new ArrayList<>();
@@ -69,15 +40,9 @@ public class Subset {
                     subset.add(nums[j]);
                 }
             }
-            // or:
-            // for (int j = i, index = 0; j > 0; j >>= 1, index++) {
-            //     if ((j & 1) > 0) {
-            //         subset.add(nums[index]);
-            //     }
-            // }
             res.add(subset);
         }
-        return res;
+        return new ArrayList<>(res);
     }
 
     void test(Function<int[], List<List<Integer>>> subsets,
@@ -104,16 +69,15 @@ public class Subset {
     }
 
     void test(int[] nums, Integer[][] expected) {
-        Subset s = new Subset();
+        Subset2 s = new Subset2();
         test(s::subsets, "subsets", nums, expected);
         test(s::subsets2, "subsets2", nums, expected);
-        test(s::subsets3, "subsets3", nums, expected);
     }
 
     @Test
     public void test1() {
-        test(new int[] {1, 2, 3}, new Integer[][] {
-            {}, {1}, {2}, {3}, {1, 2}, {1, 3}, {2, 3}, {1, 2, 3}
+        test(new int[] {1, 2, 2}, new Integer[][] {
+            {}, {1}, {2}, {1, 2}, {2, 2}, {1, 2, 2}
         });
         test(new int[] {4, 2, 3}, new Integer[][] {
             {}, {4}, {2}, {3}, {4, 2}, {4, 3}, {2, 3}, {4, 2, 3}
@@ -121,6 +85,6 @@ public class Subset {
     }
 
     public static void main(String[] args) {
-        org.junit.runner.JUnitCore.main("Subset");
+        org.junit.runner.JUnitCore.main("Subset2");
     }
 }
