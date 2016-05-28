08e714c7689406b6312ae5224470a1424905525f
Hui Zheng <xyzdll@gmail.com>
2016-05-28 15:46:24 +0800

    improve Subset.java by binarysearch yet beat rate drops; add two more solutions for it; improve recursion_DP/Subsets.java

--- leetcode/Subset.java
+++ leetcode/Subset.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.*;
 
 // Given a set of distinct integers, nums, return all possible subsets.
 public class Subset {
-    // beats 15.24%
+    // beats 10.68%(15.24% if not using Arrays.binarySearch)
     public List<List<Integer> > subsets(int[] nums) {
         List<List<Integer> > res = new ArrayList<>();
         Arrays.sort(nums);
@@ -26,13 +26,8 @@ public class Subset {
 
         int nextIndex = 0;
         if (count > 0) {
-            int last = cur.get(count - 1);
-            for (int i = 0; i < nums.length; i++) {
-                if (nums[i] == last) {
-                    nextIndex = i + 1;
-                    break;
-                }
-            }
+            nextIndex = Arrays.binarySearch(
+                nums, count - 1, nums.length, cur.get(count - 1)) + 1;
         }
 
         for (int i = nextIndex; i < nums.length; i++) {
@@ -40,6 +35,49 @@ public class Subset {
             subsets(nums, res, total, cur);
             cur.remove(count);
         }
+    }
+
+    // beats 15.24%
+    public List<List<Integer> > subsets2(int[] nums) {
+        List<List<Integer> > res = new ArrayList<>();
+        Arrays.sort(nums);
+        subsets2(nums, res, 0, new ArrayList<Integer>());
+        return res;
+    }
+
+    private void subsets2(int[] nums, List<List<Integer> > res, int start,
+                          List<Integer> cur) {
+        int count = cur.size();
+        res.add(new ArrayList<Integer>(cur));
+        for (int i = start; i < nums.length; i++) {
+            cur.add(nums[i]);
+            subsets2(nums, res, i + 1, cur);
+            cur.remove(count);
+        }
+    }
+
+    // from recursive_DP/Subsets.java
+    // beats 59.76%
+    public List<List<Integer> > subsets3(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer> > res = new ArrayList<>();
+        int n = nums.length;
+        for (int i = ((1 << n) - 1); i >= 0; i--) {
+            List<Integer> subset = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                if ((i & (1 << j)) > 0) {
+                    subset.add(nums[j]);
+                }
+            }
+            // or:
+            // for (int j = i, index = 0; j > 0; j >>= 1, index++) {
+            //     if ((j & 1) > 0) {
+            //         subset.add(nums[index]);
+            //     }
+            // }
+            res.add(subset);
+        }
+        return res;
     }
 
     void test(Function<int[], List<List<Integer>>> subsets,
@@ -68,6 +106,8 @@ public class Subset {
     void test(int[] nums, Integer[][] expected) {
         Subset s = new Subset();
         test(s::subsets, "subsets", nums, expected);
+        test(s::subsets2, "subsets2", nums, expected);
+        test(s::subsets3, "subsets3", nums, expected);
     }
 
     @Test
--- recursion_DP/Subsets.java
+++ recursion_DP/Subsets.java
@@ -11,22 +11,22 @@ import static org.junit.Assert.*;
  * Write a method to return all subsets of a set.
  */
 public class Subsets {
-    public static List<ArrayList<Integer> > getSubsets(List<Integer> set) {
+    public static List<List<Integer> > getSubsets(List<Integer> set) {
         return getSubsets(set, set.size() - 1);
     }
 
-    private static List<ArrayList<Integer> > getSubsets(List<Integer> set, int index) {
+    private static List<List<Integer> > getSubsets(List<Integer> set, int index) {
         if (index == -1) {
-            List<ArrayList<Integer> > emptySet = new ArrayList<ArrayList<Integer> >();
+            List<List<Integer> > emptySet = new ArrayList<>();
             emptySet.add(new ArrayList<Integer>());
             return emptySet;
         }
 
-        List<ArrayList<Integer> > subsets = getSubsets(set, index - 1);
-        List<ArrayList<Integer> > moreSubsets = new ArrayList<ArrayList<Integer> >();
+        List<List<Integer> > subsets = getSubsets(set, index - 1);
+        List<List<Integer> > moreSubsets = new ArrayList<>();
         Integer newElement = set.get(index);
-        for (ArrayList<Integer> subset : subsets) {
-            ArrayList<Integer> cloned =  new ArrayList<Integer>(subset);
+        for (List<Integer> subset : subsets) {
+            List<Integer> cloned = new ArrayList<>(subset);
             cloned.add(newElement);
             moreSubsets.add(cloned);
         }
@@ -35,21 +35,21 @@ public class Subsets {
     }
 
     // From the book
-    public static List<ArrayList<Integer> > getSubsets2(List<Integer> set) {
+    public static List<List<Integer> > getSubsets2(List<Integer> set) {
         return getSubsets2(set, 0);
     }
 
-    private static List<ArrayList<Integer> > getSubsets2(List<Integer> set, int index) {
-        List<ArrayList<Integer> > allsubsets;
+    private static List<List<Integer> > getSubsets2(List<Integer> set, int index) {
+        List<List<Integer> > allsubsets;
         if (set.size() == index) { // Base case - add empty set
-            allsubsets = new ArrayList<ArrayList<Integer> >();
-            allsubsets.add(new ArrayList<Integer>());
+            allsubsets = new ArrayList<>();
+            allsubsets.add(new ArrayList<>());
         } else {
             allsubsets = getSubsets2(set, index + 1);
             int item = set.get(index);
-            List<ArrayList<Integer> > moresubsets = new ArrayList<ArrayList<Integer> >();
-            for (ArrayList<Integer> subset : allsubsets) {
-                ArrayList<Integer> newsubset = new ArrayList<Integer>();
+            List<List<Integer> > moresubsets = new ArrayList<>();
+            for (List<Integer> subset : allsubsets) {
+                List<Integer> newsubset = new ArrayList<>();
                 newsubset.addAll(subset); //
                 newsubset.add(item);
                 moresubsets.add(newsubset);
@@ -59,11 +59,11 @@ public class Subsets {
         return allsubsets;
     }
 
-    public static List<ArrayList<Integer> > getSubsets3(List<Integer> set) {
-        List<ArrayList<Integer> > subsets = new ArrayList<ArrayList<Integer> >();
+    public static List<List<Integer> > getSubsets3(List<Integer> set) {
+        List<List<Integer> > subsets = new ArrayList<>();
         int n = set.size();
         for (int i = ((1 << n) - 1); i >= 0; i--) {
-            ArrayList<Integer> subset = new ArrayList<Integer>();
+            List<Integer> subset = new ArrayList<>();
             for (int j = 0; j < n; j++) {
                 if ((i & (1 << j)) > 0) {
                     subset.add(set.get(j));
@@ -75,8 +75,8 @@ public class Subsets {
     }
 
     // From the book
-    public static ArrayList<Integer> convertIntToSet(int x, List<Integer> set) {
-        ArrayList<Integer> subset = new ArrayList<Integer>();
+    public static List<Integer> convertIntToSet(int x, List<Integer> set) {
+        List<Integer> subset = new ArrayList<Integer>();
         int index = 0;
         for (int k = x; k > 0; k >>= 1) {
             if ((k & 1) == 1) {
@@ -87,28 +87,28 @@ public class Subsets {
         return subset;
     }
 
-    public static List<ArrayList<Integer> > getSubsets4(List<Integer> set) {
-        List<ArrayList<Integer> > allsubsets = new ArrayList<ArrayList<Integer> >();
+    public static List<List<Integer> > getSubsets4(List<Integer> set) {
+        List<List<Integer> > allsubsets = new ArrayList<>();
         int max = 1 << set.size();
         for (int k = 0; k < max; k++) {
-            ArrayList<Integer> subset = convertIntToSet(k, set);
+            List<Integer> subset = convertIntToSet(k, set);
             allsubsets.add(subset);
         }
         return allsubsets;
     }
 
     private void test(int n,
-                      Function<List<Integer>, List<ArrayList<Integer>>> getSubset) {
+                      Function<List<Integer>, List<List<Integer>>> getSubset) {
         List<Integer> set = new ArrayList<Integer>();
         for (int i = 1; i <= n; i++) {
             set.add(i);
         }
 
         long t = System.nanoTime();
-        List<ArrayList<Integer> > subsets = getSubset.apply(set);
+        List<List<Integer> > subsets = getSubset.apply(set);
         System.out.format("%.03f ms\n", (System.nanoTime() - t) * 1e-6);
         if (n < 5) {
-            for (ArrayList<Integer> subset : subsets) {
+            for (List<Integer> subset : subsets) {
                 System.out.println(subset);
             }
         }
