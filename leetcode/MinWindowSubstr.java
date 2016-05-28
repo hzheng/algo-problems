7e97d9cca3f54c1dbbc11fa27e1e823cceffafbf
Hui Zheng <xyzdll@gmail.com>
2016-05-28 11:26:44 +0800

    add one more solution for MinWindowSubstr

--- leetcode/MinWindowSubstr.java
+++ leetcode/MinWindowSubstr.java
@@ -59,8 +59,42 @@ public class MinWindowSubstr {
         return minEnd < 0 ? "" : s.substring(minStart, minEnd + 1);
     }
 
+    // beats 84.34%
+    // simpler than last solution, but need rescan source string.
+    public String minWindow2(String s, String t) {
+        int[] map = new int[256];
+        for (char c : t.toCharArray()) {
+            map[c]++;
+        }
+
+        int minStart = 0;
+        int minEnd = Integer.MAX_VALUE;
+        int matched = 0;
+        int toMatch = t.length();
+        for (int i = 0, j = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (map[c] > 0) {
+                matched++;
+            }
+            map[c]--;
+            if (matched < toMatch) continue;
+
+            while (map[s.charAt(j)] < 0) { // rescan
+                map[s.charAt(j)]++;
+                j++;
+            }
+            if (i - j < minEnd - minStart) {
+                minStart = j;
+                minEnd = i;
+            }
+        }
+        return minEnd == Integer.MAX_VALUE ?
+               "" : s.substring(minStart, minEnd + 1);
+    }
+
     void test(String s, String t, String expected) {
         assertEquals(expected, minWindow(s, t));
+        assertEquals(expected, minWindow2(s, t));
     }
 
     @Test
