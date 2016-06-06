b6af003cfddad9da456f949832db128e1054a73c
Hui Zheng <xyzdll@gmail.com>
2016-06-06 20:58:28 +0800

    add one more solution for InterleaveStr

--- leetcode/recursion_DP/InterleaveStr.java
+++ leetcode/recursion_DP/InterleaveStr.java
@@ -87,6 +87,7 @@ public class InterleaveStr {
         if (len1 + len2 != s3.length()) return false;
 
         boolean[][] dp = new boolean[len1 + 1][len2 + 1];
+        // could improve by shrinking len1 and len2 by checking heads and tails
         dp[0][0] = true;
         for (int i1 = 0; i1 < len1; i1++) {
             dp[i1 + 1][0] = s3.charAt(i1) == s1.charAt(i1);
@@ -94,42 +95,82 @@ public class InterleaveStr {
         }
         for (int i2 = 0; i2 < len2; i2++) {
             dp[0][i2 + 1] = s3.charAt(i2) == s2.charAt(i2);
+
             if (!dp[0][i2 + 1]) break; // save time
         }
-        for (int i1 = 1; i1 <= len1; i1++) {
-            for (int i2 = 1; i2 <= len2; i2++) {
-                char c = s3.charAt(i1 + i2 - 1);
-                if (c == s1.charAt(i1 - 1)) {
-                    dp[i1][i2] = dp[i1 - 1][i2];
+        for (int i1 = 0; i1 < len1; i1++) {
+            for (int i2 = 0; i2 < len2; i2++) {
+                char c = s3.charAt(i1 + i2 + 1);
+                if (c == s1.charAt(i1)) {
+                    dp[i1 + 1][i2 + 1] = dp[i1][i2 + 1];
                 }
-                if (c == s2.charAt(i2 - 1)) {
-                    dp[i1][i2] |= dp[i1][i2 - 1];
+                if (c == s2.charAt(i2)) {
+                    dp[i1 + 1][i2 + 1] |= dp[i1 + 1][i2];
                 }
             }
         }
         return dp[len1][len2];
     }
 
+    // beats 79.69%%
+    public boolean isInterleave4(String s1, String s2, String s3) {
+        int len1 = s1.length();
+        int len2 = s2.length();
+        if (len1 + len2 != s3.length()) return false;
+
+        if (len1 == 0) return s2.equals(s3);
+        if (len2 == 0) return s1.equals(s3);
+
+        boolean[] dp = new boolean[len2 + 1];
+        // first row
+        dp[0] = true;
+        for (int i2 = 0; i2 < len2; i2++) {
+            dp[i2 + 1] = dp[i2] & (s3.charAt(i2) == s2.charAt(i2));
+        }
+        for (int i1 = 0; i1 < len1; i1++) {
+            // first column
+            dp[0] &= (s3.charAt(i1) == s1.charAt(i1));
+            for (int i2 = 0; i2 < len2; i2++) {
+                if (!dp[i2] && !dp[i2 + 1]) continue;
+
+                char c = s3.charAt(i1 + i2 + 1);
+                dp[i2 + 1] &= (c == s1.charAt(i1));
+                if (dp[i2]) {
+                    dp[i2 + 1] |= (c == s2.charAt(i2));
+                }
+            }
+        }
+        return dp[len2];
+    }
+
     void test(String s1, String s2, String s3, boolean expected) {
         assertEquals(expected, isInterleave(s1, s2, s3));
         assertEquals(expected, isInterleave2(s1, s2, s3));
         assertEquals(expected, isInterleave3(s1, s2, s3));
+        assertEquals(expected, isInterleave4(s1, s2, s3));
     }
 
     @Test
     public void test1() {
-        test("a", "b", "a", false);
-        test("bc", "bbca", "bbcbca", true);
-        test("aa", "aadaae", "aadaaeaa", true);
-        test("aabaac", "aadaaeaaf", "aadaaeaabaafaac", true);
         test("", "b", "b", true);
+        test("a", "b", "a", false);
+        test("ab", "bc", "bbac", false);
+        test("bc", "bb", "bbcb", true);
+        test("bc", "bbca", "bbcbca", true);
+        test("ab", "bca", "abbac", false);
+        test("ab", "ca", "abac", false);
+        test("ab", "cd", "abdc", false);
+        test("aa", "aae", "aaeaa", true);
         test("aa", "aae", "aaeaa", true);
         test("aabc", "a", "aabca", true);
+        test("aa", "aadaae", "aadaaeaa", true);
         test("aabc", "abad", "aabcabad", true);
         test("bcc", "bbca", "bbcbcac", true);
         test("aabcc", "dbbca", "aadbbcbcac", true);
         test("aabcc", "dbbca", "aadbbbcacc", true);
         test("aabcc", "dbbca", "aadbbbaccc", false);
+        test("aacaac", "aacaaeaac", "aacaaeaaeaacaac", false);
+        test("aabaac", "aadaaeaaf", "aadaaeaabaafaac", true);
     }
 
     public static void main(String[] args) {
