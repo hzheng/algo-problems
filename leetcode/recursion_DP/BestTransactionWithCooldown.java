f17c361d55a25a0ca8bd22d5e5d1bd9946014530
Hui Zheng <xyzdll@gmail.com>
2016-06-12 19:06:47 +0800

    add 3 more solutions for BestTransactionWithCooldown

--- leetcode/recursion_DP/BestTransactionWithCooldown.java
+++ leetcode/recursion_DP/BestTransactionWithCooldown.java
@@ -14,6 +14,7 @@ import static org.junit.Assert.*;
 // After you sell your stock, you cannot buy stock on next day.
 public class BestTransactionWithCooldown {
     // beats 50.82%
+    // space complexity: O(N)
     public int maxProfit(int[] prices) {
         int n = prices.length;
         // profits[i] is the max profit ending before day i
@@ -39,6 +40,62 @@ public class BestTransactionWithCooldown {
         return n == 0 ? 0 : profits[n - 1];
     }
 
+    // beats 50.82%
+    // space complexity: O(N)
+    public int maxProfit2(int[] prices) {
+        int n = prices.length;
+        if (n < 2) return 0;
+
+        int[] buy = new int[n];
+        int[] sell = new int[n];
+        sell[1] = Math.max(0, prices[1] - prices[0]);
+        buy[0] = -prices[0];
+        buy[1] = Math.max(-prices[0], -prices[1]);
+        for (int i = 2; i < prices.length; i++) {
+            int price = prices[i];
+            buy[i]  = Math.max(sell[i - 2] - price, buy[i - 1]);
+            sell[i] = Math.max(buy[i - 1] + price, sell[i - 1]);
+        }
+        return sell[n - 1];
+    }
+
+    // beats 50.82%
+    // space complexity: O(1)
+    public int maxProfit3(int[] prices) {
+        int n = prices.length;
+        if (n < 2) return 0;
+
+        int buy = -prices[0];
+        int sell = 0;
+        for (int i = 1, prevSell = 0; i < prices.length; i++) {
+            int price = prices[i];
+            int prevBuy = buy;
+            buy = Math.max(prevSell - price, prevBuy);
+            prevSell = sell;
+            sell = Math.max(prevBuy + price, prevSell);
+        }
+        return sell;
+    }
+
+    // beats 8.28%
+    // space complexity: O(1)
+    public int maxProfit4(int[] prices) {
+        int n = prices.length;
+        if (n < 2) return 0;
+
+        int[][] dp = {{Integer.MIN_VALUE, Integer.MIN_VALUE}, {0, 0}, {0, 0}};
+        for (int i = 0; i < n; i++) {
+            int index = i & 1;
+            // buy state, bought before or buy today
+            dp[0][index] = Math.max(dp[0][1 - index], dp[2][1 - index] - prices[i]);
+            // sell state, bought before
+            dp[1][index] = dp[0][1 - index] + prices[i];
+            // cooldown state, cooldown yesterday or sold yesterday
+            dp[2][index] = Math.max(dp[2][1 - index], dp[1][1 - index]);
+        }
+        return Math.max(dp[1][(n - 1) % 2], dp[2][(n - 1) % 2]);
+    }
+
     void test(Function<int[], Integer> maxProfit, int expected, int ... prices) {
         assertEquals(expected, (int)maxProfit.apply(prices));
     }
@@ -46,6 +103,9 @@ public class BestTransactionWithCooldown {
     void test(int expected, int ... prices) {
         BestTransactionWithCooldown b = new BestTransactionWithCooldown();
         test(b::maxProfit, expected, prices);
+        test(b::maxProfit2, expected, prices);
+        test(b::maxProfit3, expected, prices);
+        test(b::maxProfit4, expected, prices);
     }
 
     @Test
