e2842b90f656e0378fb1e16b017d1972d519d89c
Hui Zheng <xyzdll@gmail.com>
2016-06-13 16:46:22 +0800

    add one more solution for WordLadder2, but still TLE

--- leetcode/recursion_DP/WordLadder2.java
+++ leetcode/recursion_DP/WordLadder2.java
@@ -12,7 +12,7 @@ import java.nio.file.Paths;
 // 1. Only one letter can be changed at a time
 // 2. Each intermediate word must exist in the word list
 public class WordLadder2 {
-    // beats %
+    // Time Limit Exceeded
     static class WordPath {
         String word;
         List<String> path;
@@ -76,6 +76,62 @@ public class WordLadder2 {
         return words;
     }
 
+    // Time Limit Exceeded
+    public List<List<String> > findLadders2(String beginWord, String endWord,
+                                            Set<String> wordList) {
+        List<List<String> > res = new ArrayList<>();
+        if (beginWord.equals(endWord)) {
+            res.add(Arrays.asList(beginWord, endWord));
+            return res;
+        }
+
+        Queue<WordNode> queue = new LinkedList<>();
+        queue.add(new WordNode(beginWord, null));
+        wordList.add(endWord);
+        int maxLevel = Integer.MAX_VALUE;
+        boolean found = false;
+        Set<String> visited = new HashSet<>();
+        while (!queue.isEmpty()) {
+            WordNode node = queue.poll();
+            String word = node.word;
+            int level = 0;
+            for (WordNode n = node; n != null; n = n.prev) {
+                visited.add(n.word);
+                level++;
+            }
+            if (found && level >= maxLevel) break;
+
+            for (String w : oneEditSets(word, wordList)) {
+                if (w.equals(endWord)) {
+                    List<String> path = new LinkedList<>();
+                    while (node != null) {
+                        path.add(0, node.word);
+                        node = node.prev;
+                    }
+                    path.add(endWord);
+                    res.add(path);
+                    maxLevel = path.size();
+                    found = true;
+                    break;
+                }
+                if (!found && !visited.contains(w)) {
+                    queue.add(new WordNode(w, node));
+                }
+            }
+        }
+        return res;
+    }
+
+    static class WordNode {
+        String word;
+        WordNode prev;
+
+        WordNode(String word, WordNode prev) {
+            this.word = word;
+            this.prev = prev;
+        }
+    }
+
     interface Function<A, B, C, D> {
         public D apply(A a, B b, C c);
     }
@@ -110,6 +166,7 @@ public class WordLadder2 {
         WordLadder2 w = new WordLadder2();
         try {
             test(w::findLadders, "findLadders", begin, end, expected, words);
+            test(w::findLadders2, "findLadders2", begin, end, expected, words);
         } catch (Exception e) {
             e.printStackTrace();
         }
