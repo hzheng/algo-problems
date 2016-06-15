ffdce113f74038a35c09c7dae36e01ddebf05520
Hui Zheng <xyzdll@gmail.com>
2016-06-15 14:04:32 +0800

    add one more solution for CloneGraph

--- leetcode/tree_graph/CloneGraph.java
+++ leetcode/tree_graph/CloneGraph.java
@@ -10,6 +10,7 @@ import common.UndirectedGraphNode;
 // Clone an undirected graph. Each node in the graph contains a label and a
 // list of its neighbors.
 public class CloneGraph {
+    // DFS
     // beats 66.04%
     public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
         return clone(node, new HashMap<>());
@@ -20,7 +21,7 @@ public class CloneGraph {
         if (node == null) return null;
 
         int label = node.label;
-        if (map.containsKey(label)) return map.get(label);
+        if (map.containsKey(label)) return map.get(label); // visited
 
         UndirectedGraphNode cloned = new UndirectedGraphNode(label);
         map.put(label, cloned);
@@ -31,9 +32,42 @@ public class CloneGraph {
         return cloned;
     }
 
+    // BFS
+    // beats 19.51%
+    public UndirectedGraphNode cloneGraph2(UndirectedGraphNode node) {
+        if (node == null) return null;
+
+        Queue<UndirectedGraphNode> queue = new LinkedList<>();
+        Map<Integer, UndirectedGraphNode> map = new HashMap<>();
+        UndirectedGraphNode root = clone2(node, map);
+        queue.offer(node);
+        while (!queue.isEmpty()) {
+            UndirectedGraphNode n = queue.poll();
+            UndirectedGraphNode cloned = clone2(n, map);
+            for (UndirectedGraphNode neighbor : n.neighbors) {
+                if (!map.containsKey(neighbor.label)) {
+                    queue.offer(neighbor);
+                }
+                cloned.neighbors.add(clone2(neighbor, map));
+            }
+        }
+        return root;
+    }
+
+    private UndirectedGraphNode clone2(UndirectedGraphNode node,
+                                       Map<Integer, UndirectedGraphNode> map) {
+        int label = node.label;
+        if (map.containsKey(label)) return map.get(label);
+
+        UndirectedGraphNode cloned = new UndirectedGraphNode(label);
+        map.put(label, cloned);
+        return cloned;
+    }
+
     void test(String s) {
         UndirectedGraphNode node = UndirectedGraphNode.of(s);
         assertEquals("{" + s + "}", cloneGraph(node).toString());
+        assertEquals("{" + s + "}", cloneGraph2(node).toString());
     }
 
     @Test
