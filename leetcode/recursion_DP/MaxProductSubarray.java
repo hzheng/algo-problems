9d5f03773a107593b3ac52fbcd3012f7e3fe515c
Hui Zheng <xyzdll@gmail.com>
2016-06-22 23:46:53 +0800

    add two more solutions for MaxProductSubarray

--- leetcode/recursion_DP/MaxProductSubarray.java
+++ leetcode/recursion_DP/MaxProductSubarray.java
@@ -96,6 +96,66 @@ public class MaxProductSubarray {
         return max;
     }
 
+    // beats 80.08%(3 ms)
+    public int maxProduct3(int[] nums) {
+        int n = nums.length;
+        int localMax = nums[0];
+        int localMin = localMax;
+        int max = localMax;
+        for (int i = 1; i < n; i++) {
+            int num = nums[i];
+            int curMax = num * localMax;
+            int curMin = num;
+            if (curMax < num) {
+                curMin = curMax;
+                curMax = num;
+            }
+            int local = num * localMin;
+            localMax = Math.max(curMax, local);
+            localMin = Math.min(curMin, local);
+            max = Math.max(max, localMax);
+        }
+        return max;
+    }
+
+    // beats 80.08%(3 ms)
+    public int maxProduct4(int[] nums) {
+        int n = nums.length;
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+
+        boolean isPostive = false;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) continue;
+
+            if (nums[i] > 0 || (i > 0 && nums[i - 1] < 0)) {
+                isPostive = true;
+                break;
+            }
+        }
+        if (!isPostive) return 0;
+
+        int localMax = 1;
+        int localMin = 1;
+        int max = 1;
+        for (int num : nums) {
+            if (num > 0) {
+                localMax *= num;
+                localMin *= num;
+                localMin = Math.min(localMin, 1);
+            } else if (num == 0) {
+                localMax = 1;
+                localMin = 1;
+            } else {
+                int oldLocalMax = localMax;
+                localMax = Math.max(localMin * num, 1);
+                localMin = oldLocalMax * num;
+            }
+            max = Math.max(max, localMax);
+        }
+        return max;
+    }
+
     void test(Function<int[], Integer> maxProduct, int expected, int ... nums) {
         assertEquals(expected, (int)maxProduct.apply(nums));
     }
@@ -104,15 +164,17 @@ public class MaxProductSubarray {
         MaxProductSubarray m = new MaxProductSubarray();
         test(m::maxProduct, expected, nums);
         test(m::maxProduct2, expected, nums);
+        test(m::maxProduct3, expected, nums);
+        test(m::maxProduct4, expected, nums);
     }
 
     @Test
     public void test1() {
+        test(0, -4, 0, -2);
+        test(-4, -4);
         test(3, -2, 3);
         test(2, 0, 2);
         test(6, 2, 3, -2, 4);
-        test(-4, -4);
-        test(0, -4, 0, -2);
         test(12, -3, -4);
         test(12, -3, -4, 0, -2, -5);
         test(4320, -2, -3, 8, 9, 10, 0, 3, 8, 9, -2, 5, 3);
