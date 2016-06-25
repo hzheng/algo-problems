40369ecd714709e71ce2b9b10b3426435a3b9550
Hui Zheng <xyzdll@gmail.com>
2016-06-26 00:18:00 +0800

    improve MajorityElement2

--- leetcode/array_string/MajorityElement2.java
+++ leetcode/array_string/MajorityElement2.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.*;
 // Given an integer array of size n, find all elements that appear more than
 // ⌊ n/3 ⌋ times. The algorithm should run in linear time and in O(1) space.
 public class MajorityElement2 {
-    // beats 22.91%(7 ms)
+    // beats 22.90%(7 ms)
     // time complexity: O(N), space complexity: O(1)
     public List<Integer> majorityElement(int[] nums) {
         int major1 = 0;
@@ -17,27 +17,22 @@ public class MajorityElement2 {
         int major2 = 0;
         int count2 = 0;
         for (int num : nums) {
-            if (count1 == 0) {
+            if (num == major1) {
+                count1++;
+            } else if (num == major2) {
+                count2++;
+            } else if (count1 == 0) {
                 major1 = num;
                 count1 = 1;
-            } else if (num == major1) {
-                count1++;
+            } else if (count2 == 0) {
+                major2 = num;
+                count2 = 1;
             } else {
-                if (count2 == 0) {
-                    major2 = num;
-                    count2 = 1;
-                } else if (num == major2) {
-                    count2++;
-                } else {
-                    count2--;
-                    if (--count1 == 0 && count2 > 0) {
-                        count1 = count2;
-                        count2 = 0;
-                        major1 = major2;
-                    }
-                }
+                count1--;
+                count2--;
             }
         }
+
         List<Integer> majors = new ArrayList<>();
         if (count1 > 0 && isMajor(nums, major1)) {
             majors.add(major1);
@@ -76,12 +71,15 @@ public class MajorityElement2 {
 
     @Test
     public void test1() {
+        test(new Integer[0], 0, 1, 2);
+        test(new Integer[] {1}, -1, 1, 1, 1, 2, 1);
+        test(new Integer[] {100}, -1, 100, 2, 100, 100, 4, 100);
         test(new Integer[0], 1, 2, 3);
-        test(new Integer[]{5}, 5);
-        test(new Integer[]{5}, 5, 5);
-        test(new Integer[]{5, 6}, 6, 5);
-        test(new Integer[]{5, 6}, 6, 5, 5, 6);
-        test(new Integer[]{3, 7}, 3, 7, 3, 7, 3, 7, 8, 7, 3, 7, 3, 3);
+        test(new Integer[] {5}, 5);
+        test(new Integer[] {5}, 5, 5);
+        test(new Integer[] {5, 6}, 6, 5);
+        test(new Integer[] {5, 6}, 6, 5, 5, 6);
+        test(new Integer[] {3, 7}, 3, 7, 3, 7, 3, 7, 8, 7, 3, 7, 3, 3);
     }
 
     public static void main(String[] args) {
