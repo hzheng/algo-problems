3de56d6a9953ee5bac1f322517eac10a853468a5
Hui Zheng <xyzdll@gmail.com>
2016-06-25 12:50:21 +0800

    minor change

--- leetcode/sort_search/MaxGap.java
+++ leetcode/sort_search/MaxGap.java
@@ -13,6 +13,8 @@ import java.util.function.Function;
 // Assume all elements in the array are non-negative integers and fit in the
 // 32-bit signed integer range.
 public class MaxGap {
+    // bucket sort(exactly the same as leetcode solution)
+    // time complexity: O(N), space complexity: O(N)
     // beats 84.93%(4 ms)
     public int maximumGap(int[] nums) {
         int n = nums.length;
@@ -34,7 +36,7 @@ public class MaxGap {
         for (int num : nums) {
             int index = (num - min) / bucketSize;
             if (buckets[index] == null) {
-                buckets[index] = new int[]{num, num};
+                buckets[index] = new int[] {num, num};
             } else {
                 int[] range = buckets[index];
                 range[0] = Math.min(range[0], num);
@@ -43,7 +45,8 @@ public class MaxGap {
         }
         int lastBound = -1;
         int maxGap = 0;
-        // max gap must be in different buckets
+        // max gap must be in different buckets, since at least one *middle*
+        // bucket is empty(buckets number == nums.length + 1)
         for (int[] range : buckets) {
             if (range != null) {
                 if (lastBound >= 0) {
@@ -55,13 +58,88 @@ public class MaxGap {
         return maxGap;
     }
 
-    void test(Function<int[], Integer> maxGap, int expected, int... nums) {
+    // radix sort (changed input)
+    // time complexity: O(N), space complexity: O(N)
+    // beats 1.10% (72 ms)
+    public int maximumGap2(int[] nums) {
+        SortedMap<Integer, List<Integer> > map = new TreeMap<>();
+        final int maxIndex = 10;
+        final int radix = 10;
+        int power = 1;
+        for (int index = 1; index <= maxIndex; power *= radix, index++) {
+            for (int num : nums) {
+                int key = num / power;
+                if (!map.containsKey(key)) {
+                    List<Integer> list = new LinkedList<>();
+                    map.put(key, list);
+                }
+                map.get(key).add(num);
+            }
+            int i = 0;
+            for (List<Integer> list : map.values()) {
+                for (int num : list) {
+                    nums[i++] = num;
+                }
+            }
+            map.clear();
+        }
+
+        int maxGap = 0;
+        for (int i = 1; i < nums.length; i++) {
+            maxGap = Math.max(maxGap, nums[i] - nums[i - 1]);
+        }
+        return maxGap;
+    }
+
+    // radix sort + counting sort(changed input)
+    // time complexity: O(N), space complexity: O(N)
+    // beats 72.06% (5 ms)
+    public int maximumGap3(int[] nums) {
+        int n = nums.length;
+        if (n < 2) return 0;
+
+        int max = 0;
+        for (int num : nums) {
+            if (num > max) {
+                max = num;
+            }
+        }
+
+        final int maxIndex = 10;
+        final int radix = 10;
+        int[] buffer = new int[n];
+        for (int power = 1; power <= max; power *= radix) {
+            int[] count = new int[maxIndex];
+            for (int num : nums) {
+                count[(num / power) % radix]++;
+            }
+            for (int i = 1; i < maxIndex; i++) {
+                count[i] += count[i - 1];
+            }
+            for (int i = nums.length - 1; i >= 0; i--) {
+                buffer[--count[(nums[i] / power) % radix]] = nums[i];
+            }
+            for (int i = 0; i < n; i++) {
+                nums[i] = buffer[i];
+            }
+        }
+
+        int maxGap = 0;
+        for (int i = 1; i < n; i++) {
+            maxGap = Math.max(maxGap, buffer[i] - buffer[i - 1]);
+        }
+        return maxGap;
+    }
+
+    void test(Function<int[], Integer> maxGap, int expected, int ... nums) {
         assertEquals(expected, (int)maxGap.apply(nums));
     }
 
     void test(int expected, int ... nums) {
         MaxGap m = new MaxGap();
         test(m::maximumGap, expected, nums);
+        test(m::maximumGap2, expected, nums);
+        test(m::maximumGap3, expected, nums);
     }
 
     @Test
