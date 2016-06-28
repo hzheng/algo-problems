29e83855bd095e74348e524d25b030dade2fd3ef
Hui Zheng <xyzdll@gmail.com>
2016-06-28 18:01:56 +0800

    add 3 more solutions for TreeRightView

--- leetcode/tree_graph/TreeRightView.java
+++ leetcode/tree_graph/TreeRightView.java
@@ -71,6 +71,87 @@ public class TreeRightView {
         return res;
     }
 
+    // beats 10.84%(3 ms)
+    public List<Integer> rightSideView3(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        TreeNode lastNode = null;
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node == null) { // finished one level
+                res.add(lastNode.val);
+                lastNode = null;
+                continue;
+            }
+
+            if (lastNode == null) {
+                queue.offer(null);
+            }
+            lastNode = node;
+
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+        return res;
+    }
+
+    // beats 47.35%(2 ms)
+    public List<Integer> rightSideView4(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (true) {
+            int size = queue.size();
+            if (size == 0) break;
+
+            TreeNode node = null;
+            for (int i = 0; i < size; i++) {
+                node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            res.add(node.val);
+        }
+        return res;
+    }
+
+    // recursion
+    // http://www.jiuzhang.com/solutions/binary-tree-right-side-view/
+    // beats 47.35%(2 ms)
+    public List<Integer> rightSideView5(TreeNode root) {
+        Map<Integer, Integer> values = new HashMap<>();
+        dfs(values, root, 1);
+
+        List<Integer> res = new ArrayList<>();
+        for (int depth = 1; values.containsKey(depth); depth++) {
+            res.add(values.get(depth));
+        }
+        return res;
+    }
+
+    private void dfs(Map<Integer, Integer> values, TreeNode node, int depth) {
+        if (node == null) return;
+
+        values.put(depth, node.val);
+        dfs(values, node.left, depth + 1);
+        dfs(values, node.right, depth + 1);
+    }
+
+    // TODO: recursion: www.geeksforgeeks.org/print-right-view-binary-tree-2/
+
     void test(Function<TreeNode, List<Integer> > rightSideView,
               String s, Integer ... expected) {
         List<Integer> res = rightSideView.apply(TreeNode.of(s));
@@ -81,6 +162,9 @@ public class TreeRightView {
         TreeRightView t = new TreeRightView();
         test(t::rightSideView, s, expected);
         test(t::rightSideView2, s, expected);
+        test(t::rightSideView3, s, expected);
+        test(t::rightSideView4, s, expected);
+        test(t::rightSideView5, s, expected);
     }
 
     @Test
