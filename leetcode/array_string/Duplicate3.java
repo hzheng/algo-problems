7b47e80873b6cd1736ca26b018649a63dea37676
Hui Zheng <xyzdll@gmail.com>
2016-07-05 00:30:43 +0800

    add one more solution for Duplicate3

--- leetcode/array_string/Duplicate3.java
+++ leetcode/array_string/Duplicate3.java
@@ -9,9 +9,11 @@ import static org.junit.Assert.*;
 // i and j in the array such that the difference between nums[i] and nums[j]
 // is at most t and the difference between i and j is at most k.
 public class Duplicate3 {
+    // time complexity: O(N * log(K)), space complexity: O(N)
     // beats 32.20%(51 ms)
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
         if (t < 0) return false;
+        // if (k < 1 || t < 0) return false; // even slower(?)
 
         SortedMap<Long, Integer> map = new TreeMap<>();
         for (int i = 0; i < nums.length; i++) {
@@ -20,16 +22,18 @@ public class Duplicate3 {
                 if (i - map.get(almostDup) <= k) return true;
             }
             map.put(num, i);
-            // if (i >= k) {  // unnecessary, beat rate will drop to 23.42%
+            // space complexity: O(K) , beat rate will drop to 23.42%
+            // if (i >= k) {
             //     map.remove((long)nums[i - k]);
             // }
         }
         return false;
     }
 
-    // beats 41.36%(48 ms)
+    // time complexity: O(N * log(K)), space complexity: O(K)
+    // beats 63.55%(41 ms)
     public boolean containsNearbyAlmostDuplicate2(int[] nums, int k, int t) {
-        if (t < 0) return false;
+        if (k < 1 || t < 0) return false;
 
         NavigableSet<Long> set = new TreeSet<>();
         for (int i = 0; i < nums.length; i++) {
@@ -48,9 +52,10 @@ public class Duplicate3 {
         return false;
     }
 
-    // beats 44.85%(47 ms)
+    // time complexity: O(N * log(K)), space complexity: O(K)
+    // beats 55.34%%(45 ms)
     public boolean containsNearbyAlmostDuplicate3(int[] nums, int k, int t) {
-        if (t < 0) return false;
+        if (k < 1 || t < 0) return false;
 
         SortedSet<Long> set = new TreeSet<>();
         for (int i = 0; i < nums.length; i++) {
@@ -65,16 +70,42 @@ public class Duplicate3 {
         return false;
     }
 
-    void test(boolean expected, int k, int t, int... nums) {
+    // https://discuss.leetcode.com/topic/15199/ac-o-n-solution-in-java-using-buckets-with-explanation
+    // beats 86.97%(23 ms)
+    public boolean containsNearbyAlmostDuplicate4(int[] nums, int k, int t) {
+        if (k < 1 || t < 0) return false;
+
+        Map<Long, Long> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            long num = (long)nums[i] - Integer.MIN_VALUE;
+            long bucket = num / ((long)t + 1);
+            if (map.containsKey(bucket)
+                || (map.containsKey(bucket - 1) && num - map.get(bucket - 1) <= t)
+                || (map.containsKey(bucket + 1) && map.get(bucket + 1) - num <= t)) {
+                return true;
+            }
+
+            if (map.entrySet().size() >= k) {
+                map.remove(((long)nums[i - k] - Integer.MIN_VALUE) / ((long)t + 1));
+            }
+            map.put(bucket, num);
+        }
+        return false;
+    }
+
+    void test(boolean expected, int k, int t, int ... nums) {
         assertEquals(expected, containsNearbyAlmostDuplicate(nums, k, t));
         assertEquals(expected, containsNearbyAlmostDuplicate2(nums, k, t));
         assertEquals(expected, containsNearbyAlmostDuplicate3(nums, k, t));
+        assertEquals(expected, containsNearbyAlmostDuplicate4(nums, k, t));
     }
 
     @Test
     public void test1() {
+        test(false, 0, 1, 1, 2);
         test(false, 1, -1, -1, -1);
         test(true, 1, 2147483647, 0, 2147483647);
+        test(false, 1, 2147483647, -1, 2147483647);
         test(false, 1, 0, 1);
         test(true, 1, 0, 1, 1);
         test(false, 1, 0, 1, 3, 1, 5);
