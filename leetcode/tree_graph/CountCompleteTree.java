e22494e2c1027d911f7c7ce5d4a6e3c89d8716c5
Hui Zheng <xyzdll@gmail.com>
2016-07-05 22:57:01 +0800

    add two more solutin for CountCompleteTree

--- leetcode/tree_graph/CountCompleteTree.java
+++ leetcode/tree_graph/CountCompleteTree.java
@@ -10,7 +10,7 @@ import common.TreeNode;
 //
 // Given a complete binary tree, count the number of nodes.
 public class CountCompleteTree {
-    //  Time Limit Exceeded
+    // Time Limit Exceeded
     // time complexity: O(N), space complexity: O(N)
     public int countNodes(TreeNode root) {
         if (root == null) return 0;
@@ -38,15 +38,15 @@ public class CountCompleteTree {
 
         int h = height(root);
         int count = (1 << h);
-        TreeNode lastNode = root;
+        TreeNode parent = root;
         TreeNode node = root.right;
         for (int i = 1; node != null; i++, node = node.right) {
             if (height(node) + i == h) {
-                lastNode = node;
                 count += (1 << (h - i));
             } else {
-                lastNode = node = lastNode.left;
+                node = parent.left;
             }
+            parent = node;
         }
         return count;
     }
@@ -59,10 +59,76 @@ public class CountCompleteTree {
         return height;
     }
 
+    // recursion
+    // beats 20.71%(122 ms)
+    public int countNodes3(TreeNode root) {
+        if (root == null) return 0;
+
+        // one more line will work, but is slow
+        // return countNodes3(root.left) + countNodes3(root.right) + 1;
+
+        int left = leftHeight(root);
+        int right = rightHeight(root);
+        if (left == right) return (2 << left) - 1;
+
+        return countNodes3(root.left) + countNodes3(root.right) + 1;
+    }
+
+    private int leftHeight(TreeNode root) {
+        if (root == null) return 0;
+
+        int height = 0;
+        for (TreeNode n = root.left; n != null; n = n.left) {
+            height++;
+        }
+        return height;
+    }
+
+    private int rightHeight(TreeNode root) {
+        if (root == null) return 0;
+
+        int height = 0;
+        for (TreeNode n = root.right; n != null; n = n.right) {
+            height++;
+        }
+        return height;
+    }
+
+    // binary search
+    // beats 60.88%(107 ms)
+    public int countNodes4(TreeNode root) {
+        if (root == null) return 0;
+
+        int h = height(root);
+        int low = 0;
+        for (int high = 1 << h; low < high; ) {
+            int mid = low + (high - low) / 2;
+            if (isFull(root, h, mid)) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return (1 << h) + low - 1;
+    }
+
+    private boolean isFull(TreeNode node, int level, int path) {
+        for (int i = level - 1; i >= 0; i--) {
+            if (((path >> i) & 1) == 0) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+        }
+        return node != null;
+    }
+
     void test(String s, int expected) {
         TreeNode root = TreeNode.of(s);
         assertEquals(expected, countNodes(root));
         assertEquals(expected, countNodes2(root));
+        assertEquals(expected, countNodes3(root));
+        assertEquals(expected, countNodes4(root));
     }
 
     @Test
