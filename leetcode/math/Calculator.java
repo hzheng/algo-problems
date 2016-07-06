7479b4d418c0ce4c1c9bc8a7b0e8dcd274b35e00
Hui Zheng <xyzdll@gmail.com>
2016-07-07 01:53:44 +0800

    add one more solution for Calculator

--- leetcode/math/Calculator.java
+++ leetcode/math/Calculator.java
@@ -42,12 +42,73 @@ public class Calculator {
         return res + sign * num;
     }
 
+    // beats 60.30%(24 ms)
+    public int calculate2(String s) {
+        Stack<Boolean> signs = new Stack<>();
+        signs.push(true);
+        int res = 0;
+        boolean sign = true;
+        int len = s.length();
+        for (int i = 0; i < len; i++) {
+            char c = s.charAt(i);
+            if (c == ' ') continue;
+
+            if (c == '+' || c == '-') {
+                sign = c == '+';
+            } else if (c == '(') {
+                signs.push(!(signs.peek() ^ sign));
+                sign = true;
+            } else if (c == ')') {
+                signs.pop();
+            } else {
+                int num = c - '0';
+                while (++i < len && Character.isDigit(s.charAt(i))) {
+                    num = num * 10 + (s.charAt(i) - '0');
+                }
+                i--;
+                res += ((signs.peek() ^ sign) ? -1 : 1) * num;
+            }
+        }
+        return res;
+    }
+
+    // beats 43.79%(35 ms)
+    public int calculate3(String s) {
+        Stack<Integer> signs = new Stack<>();
+        signs.push(1);
+        signs.push(1);
+        int res = 0;
+        int len = s.length();
+
+        for (int i = 0; i < len; i++) {
+            char c = s.charAt(i);
+            if (c == ' ') continue;
+
+            if (c == ')') {
+                signs.pop();
+            } else if (c >= '0'){
+                int num = c - '0';
+                while (++i < len && Character.isDigit(s.charAt(i))) {
+                    num = num * 10 + (s.charAt(i) - '0');
+                }
+                i--;
+                res += signs.pop() * num;
+            } else {
+                signs.push(signs.peek() * (c == '-' ? -1 : 1));
+            }
+        }
+        return res;
+    }
+
     void test(String s, int expected) {
         assertEquals(expected, calculate(s));
+        assertEquals(expected, calculate2(s));
+        assertEquals(expected, calculate3(s));
     }
 
     @Test
     public void test1() {
+        test("(7)-(0)+(4)", 11);
         test(" 1-(5) ", -4);
         test(" 2-(1 + 1) ", 0);
         test("3 ", 3);
