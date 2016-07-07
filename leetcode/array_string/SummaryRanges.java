25de637cf6029cf8b5a717d093d9c3d4e535979c
Hui Zheng <xyzdll@gmail.com>
2016-07-07 14:50:46 +0800

    add one more solution fro SummaryRanges

--- leetcode/array_string/SummaryRanges.java
+++ leetcode/array_string/SummaryRanges.java
@@ -59,17 +59,36 @@ public class SummaryRanges {
         return res;
     }
 
-    void test(int[] nums, String... expected) {
+    // beats 3.93%(1 ms)
+    public List<String> summaryRanges3(int[] nums) {
+        int n = nums.length;
+        List<String> res = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int start = nums[i];
+            while (i + 1 < n && (nums[i + 1] == nums[i] + 1)) {
+                i++;
+            }
+            if (start != nums[i]) {
+                res.add(start + "->" + nums[i]);
+            } else {
+                res.add("" + start);
+            }
+        }
+        return res;
+    }
+
+    void test(int[] nums, String ... expected) {
         assertArrayEquals(expected, summaryRanges(nums).toArray(new String[0]));
         assertArrayEquals(expected, summaryRanges2(nums).toArray(new String[0]));
+        assertArrayEquals(expected, summaryRanges3(nums).toArray(new String[0]));
     }
 
     @Test
     public void test1() {
-        test(new int[]{0}, "0");
-        test(new int[]{1,3}, "1", "3");
-        test(new int[]{0, 1, 2, 4, 5, 7}, "0->2", "4->5", "7");
-        test(new int[]{0, 1, 2, 3, 4, 5, 7, 8, 10}, "0->5", "7->8", "10");
+        test(new int[] {0}, "0");
+        test(new int[] {1,3}, "1", "3");
+        test(new int[] {0, 1, 2, 4, 5, 7}, "0->2", "4->5", "7");
+        test(new int[] {0, 1, 2, 3, 4, 5, 7, 8, 10}, "0->5", "7->8", "10");
     }
 
     public static void main(String[] args) {
