ad33961eca868a0a270fc40747b401e0fae1b1e2
Hui Zheng <xyzdll@gmail.com>
2016-07-10 22:07:26 +0800

    add 2 more solutions for ValidAnagram

--- leetcode/array_string/ValidAnagram.java
+++ leetcode/array_string/ValidAnagram.java
@@ -30,8 +30,41 @@ public class ValidAnagram {
 
         Arrays.sort(cs1);
         Arrays.sort(cs2);
+        // return Arrays.equals(cs1, cs2); // slower?(7 ms)
         for (int i = cs1.length - 1; i >= 0; i--) {
             if (cs1[i] != cs2[i]) return false;
+        }
+        return true;
+    }
+
+    // from leetcode
+    // beats 64.12%(7 ms)
+    public boolean isAnagram3(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] counts = new int[26];
+        for (int i = s.length() - 1; i >= 0; i--) {
+            counts[s.charAt(i) - 'a']++;
+            counts[t.charAt(i) - 'a']--;
+        }
+        for (int count : counts) {
+            if (count != 0) return false;
+        }
+        return true;
+    }
+
+    // from leetcode
+    // beats 49.70%(8 ms)
+    public boolean isAnagram4(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] counts = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            counts[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            counts[t.charAt(i) - 'a']--;
+            if (counts[t.charAt(i) - 'a'] < 0) return false;
         }
         return true;
     }
@@ -39,12 +72,15 @@ public class ValidAnagram {
     void test(String s, String t, boolean expected) {
         assertEquals(expected, isAnagram(s, t));
         assertEquals(expected, isAnagram2(s, t));
+        assertEquals(expected, isAnagram3(s, t));
+        assertEquals(expected, isAnagram4(s, t));
     }
 
     @Test
     public void test1() {
         test("anagram", "nagaram", true);
         test("rat", "car", false);
+        test("rat", "tarc", false);
     }
 
     public static void main(String[] args) {
