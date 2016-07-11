1d90a3f62fbdbd042fbbe4603717f46d41e26317
Hui Zheng <xyzdll@gmail.com>
2016-07-11 18:04:41 +0800

    add 1 more solution for UglyNumber2

--- leetcode/math/UglyNumber2.java
+++ leetcode/math/UglyNumber2.java
@@ -9,6 +9,7 @@ import static org.junit.Assert.*;
 // Note that 1 is typically treated as an ugly number.
 // Write a program to find the n-th ugly number.
 public class UglyNumber2 {
+    // time complexity: O(N ^ 2)(?), space complexity: O(1)
     // beats 0.09%(601 ms)
     public int nthUglyNumber(int n) {
         if (n <= 0) return 0;
@@ -60,6 +61,7 @@ public class UglyNumber2 {
         return lastUgly;
     }
 
+    // time complexity: O(N), space complexity: O(N)
     // beats 25.23%(46 ms)
     public int nthUglyNumber2(int n) {
         if (n <= 0) return 0;
@@ -92,10 +94,72 @@ public class UglyNumber2 {
         return res;
     }
 
-    void test(int... expected) {
+    //  Time Limit Exceeded
+    public int nthUglyNumber3(int n) {
+        if (n <= 0) return 0;
+
+        int res = 1;
+        for (int count = 1; count < n; ) {
+            if (isUgly(++res)) {
+                count++;
+            }
+        }
+        return res;
+    }
+
+    private boolean isUgly(int num) {
+        if (num <= 0) return false;
+
+        int x = divideRepeately(num, 2);
+        x = divideRepeately(x, 3);
+        return divideRepeately(x, 5) == 1;
+    }
+
+    private int divideRepeately(int num, int divisor) {
+        int x = num;
+        while (x % divisor == 0) {
+            x /= divisor;
+        }
+        return x;
+    }
+
+    // DP
+    // time complexity: O(N), space complexity: O(N)
+    // beats 63.20%(10 ms)
+    public int nthUglyNumber4(int n) {
+        if (n <= 0) return 0;
+
+        int[] ugly = new int[n];
+        ugly[0] = 1;
+        int i2 = 0;
+        int i3 = 0;
+        int i5 = 0;
+        int next2 = 2;
+        int next3 = 3;
+        int next5 = 5;
+        int res = 1;
+        for (int i = 1; i < n; i++) {
+            res = Math.min(next2, Math.min(next3, next5));
+            ugly[i] = res;
+            if (res == next2) {
+                next2 = ugly[++i2] * 2;
+            }
+            if (res == next3) {
+                next3 = ugly[++i3] * 3;
+            }
+            if (res == next5) {
+                next5 = ugly[++i5] * 5;
+            }
+        }
+        return res;
+    }
+
+    void test(int ... expected) {
         for (int i = 0; i < expected.length; i++) {
             assertEquals(expected[i], nthUglyNumber(i + 1));
             assertEquals(expected[i], nthUglyNumber2(i + 1));
+            assertEquals(expected[i], nthUglyNumber3(i + 1));
+            assertEquals(expected[i], nthUglyNumber4(i + 1));
         }
     }
 
