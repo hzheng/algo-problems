c3240c21f40c82509cef01c5499876eabdb0f3c2
Hui Zheng <xyzdll@gmail.com>
2016-07-12 23:59:43 +0800

    add one more solution for AddOperators

--- leetcode/math/AddOperators.java
+++ leetcode/math/AddOperators.java
@@ -16,7 +16,7 @@ public class AddOperators {
 
         List<String> res = new ArrayList<>();
         for (int addCode = (1 << (len * 2 - 2)) - 1; addCode >= 0; addCode--) {
-            String expression = codeToStr(num, addCode);
+            String expression = codeToStr(num, addCode, 2);
             if (expression != null && evaluate(expression) == target) {
                 res.add(expression);
             }
@@ -24,12 +24,13 @@ public class AddOperators {
         return res;
     }
 
-    private String codeToStr(String num, int code) {
+    private String codeToStr(String num, int code, int bits) {
         StringBuilder sb = new StringBuilder();
         sb.append(num.charAt(0));
         int len = num.length();
+        int mask = (1 << bits) - 1;
         for (int i = 1; i < len; i++) {
-            switch (code & 3) {
+            switch (code & mask) {
             case 0:
                 if (num.charAt(i - 1) == '0') {
                     if (i == 1 || !Character.isDigit(sb.charAt(sb.length() - 2))) {
@@ -38,17 +39,17 @@ public class AddOperators {
                 }
                 break;
             case 1:
-                sb.append("+");
+                sb.append("*");
                 break;
             case 2:
-                sb.append("-");
+                sb.append("+");
                 break;
             case 3:
-                sb.append("*");
+                sb.append("-");
                 break;
             }
             sb.append(num.charAt(i));
-            code >>= 2;
+            code >>= bits;
         }
         return sb.toString();
     }
@@ -57,7 +58,7 @@ public class AddOperators {
         boolean multiply = false;
         int sign = 1;
         long lastOperand = 0;
-        int res = 0;
+        long res = 0;
         int len = s.length();
         for (int i = 0; i < len; i++) {
             char c = s.charAt(i);
@@ -87,21 +88,112 @@ public class AddOperators {
         return res + sign * lastOperand;
     }
 
-    void test(String num, int x, String ... expected) {
+    // beats 0.83%(915 ms)
+    public List<String> addOperators2(String num, int target) {
+        int len = num.length();
+        if (len == 0) return Collections.emptyList();
+
+        List<String> res = new ArrayList<>();
+        if (Long.parseLong(num) == target) {
+            if (num.charAt(0) != '0' || num.length() == 1) {
+                res.add(num);
+            }
+        }
+        for (int i = 1; i <= len; i++) {
+            for (int multCode = (1 << (i - 1)) - 1; multCode >= 0; multCode--) {
+                String leftExpr = multiplyStr(num.substring(0, i), multCode);
+                if (leftExpr == null) continue;
+
+                int partialVal = 1;
+                try {
+                    for (String factor : leftExpr.split("\\*")) {
+                        partialVal *= Integer.parseInt(factor);
+                    }
+                } catch (NumberFormatException e) {
+                    continue;
+                }
+                if (i == len) {
+                    if (multCode != 0 && partialVal == target) {
+                        res.add(leftExpr);
+                    }
+                    continue;
+                }
+                for (String rightExpr : addOperators2(num.substring(i), target - partialVal)) {
+                    res.add(leftExpr + "+" + rightExpr);
+                }
+                for (String rightExpr : addOperators2(num.substring(i), partialVal - target)) {
+                    rightExpr = rightExpr.replace('+', ' ')
+                                         .replace('-', '+').replace(' ', '-');
+                    res.add(leftExpr + "-" + rightExpr);
+                }
+            }
+        }
+        return res;
+    }
+
+    private String multiplyStr(String num, int code) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(num.charAt(0));
+        int len = num.length();
+        for (int i = 1; i < len; i++) {
+            switch (code & 1) {
+            case 0:
+                if (num.charAt(i - 1) == '0') {
+                    for (int j = sb.length() - 2;; j--) {
+                        if (j < 0) return null;
+
+                        char c = sb.charAt(j);
+                        if (c < '0' || c > '9') return null;
+
+                        if (c != '0') break;
+                    }
+                }
+                break;
+            case 1:
+                sb.append("*");
+                break;
+            }
+            sb.append(num.charAt(i));
+            code >>= 1;
+        }
+        return sb.toString();
+    }
+
+    @FunctionalInterface
+    interface Function<A, B, C> {
+        public C apply(A a, B b);
+    }
+
+    void test(Function<String, Integer, List<String> > addOperators,
+              String name, String num, int x, String ... expected) {
         Arrays.sort(expected);
-        List<String> res = addOperators(num, x);
+        long t1 = System.nanoTime();
+        List<String> res = addOperators.apply(num, x);
+        if (num.length() > 9) {
+            System.out.format("%s: %.3f ms\n", name, (System.nanoTime() - t1) * 1e-6);
+        }
         Collections.sort(res);
         assertArrayEquals(expected, res.toArray(new String[0]));
     }
 
+    void test(String num, int x, String ... expected) {
+        AddOperators a = new AddOperators();
+        test(a::addOperators, "addOperators", num, x, expected);
+        test(a::addOperators2, "addOperators2", num, x, expected);
+    }
+
     @Test
     public void test1() {
+        test("00", 0, "0+0", "0-0", "0*0");
+        test("8999999", -999991, "8-999999");
         test("123", 6, "1+2+3", "1*2*3");
         test("232", 8, "2*3+2", "2+3*2");
         test("105", 5, "1*0+5", "10-5");
-        test("00", 0, "0+0", "0-0", "0*0");
         test("3456237490", 9191);
         test("2147483648", -2147483648);
+        test("999999999", 81, "9+9+9+9+9+9+9+9+9", "999-9*99-9-9-9",
+             "999-9-9*99-9-9", "999-9-9-9*99-9", "999-9-9-9-9*99",
+             "999-9-9-9-99*9", "999-9-9-99*9-9","999-9-99*9-9-9","999-99*9-9-9-9");
     }
 
     public static void main(String[] args) {
