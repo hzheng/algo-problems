7b8e182385bb5d7af8d77a5c35b51dba53866f49
Hui Zheng <xyzdll@gmail.com>
2016-07-19 00:55:14 +0800

    add 2 more solutions for RangeSumQueryMutable

--- leetcode/recursion_DP/RangeSumQueryMutable.java
+++ leetcode/recursion_DP/RangeSumQueryMutable.java
@@ -20,6 +20,7 @@ public class RangeSumQueryMutable {
         private int[] nums;
         private SortedMap<Integer, Integer> diffMap = new TreeMap<>();
 
+        // time complexity: O(N), space complexity: O(N)
         public NumArray(int[] nums) {
             this.nums = nums;
             int n = nums.length;
@@ -56,6 +57,7 @@ public class RangeSumQueryMutable {
         private int[] sums;
         private int[] nums;
 
+        // time complexity: O(N), space complexity: O(N)
         public NumArray2(int[] nums) {
             this.nums = nums;
             int n = nums.length;
@@ -88,6 +90,7 @@ public class RangeSumQueryMutable {
         private int[] bit;
         private int[] nums;
 
+        // time complexity: O(N * log(N)), space complexity: O(N)
         public NumArray3(int[] nums) {
             int n = nums.length;
             bit = new int[n + 1];
@@ -123,6 +126,110 @@ public class RangeSumQueryMutable {
         }
     }
 
+    // from leetcode
+    // Segment Tree
+    // beats 38.11%(14 ms)
+    class NumArray4 implements INumArray {
+        private int[] tree;
+        private int n;
+
+        // time complexity: O(N), space complexity: O(N)
+        public NumArray4(int[] nums) {
+            n = nums.length;
+            if (n > 0) {
+                tree = new int[n * 2];
+                buildTree(nums);
+            }
+        }
+
+        private void buildTree(int[] nums) {
+            for (int i = n, j = 0; i < 2 * n; i++, j++) {
+                tree[i] = nums[j];
+            }
+            for (int i = n - 1; i > 0; i--) {
+                tree[i] = tree[i * 2] + tree[i * 2 + 1];
+            }
+        }
+
+        // time complexity: O(log(N)), space complexity: O(1)
+        public void update(int pos, int val) {
+            tree[pos += n] = val;
+            for (; pos > 0; pos /= 2) {
+                int left = pos;
+                int right = pos;
+                if (pos % 2 == 0) {
+                    right = pos + 1;
+                } else {
+                    left = pos - 1;
+                }
+                tree[pos / 2] = tree[left] + tree[right];
+            }
+        }
+
+        // time complexity: O(log(N)), space complexity: O(1)
+        public int sumRange(int i, int j) {
+            int sum = 0;
+            for (int l = i + n, r = j + n; l <= r; l /= 2, r /= 2) {
+                if ((l % 2) == 1) {
+                    sum += tree[l++];
+                }
+                if ((r % 2) == 0) {
+                    sum += tree[r--];
+                }
+            }
+            return sum;
+        }
+    }
+
+    // from leetcode
+    // Sqrt decomposition
+    // beats 38.11%(14 ms)
+    class NumArray5 implements INumArray {
+        private int[] block;
+        private int len;
+        private int[] nums;
+
+        // time complexity: O(N), space complexity: O(N ^ (1/2))
+        public NumArray5(int[] nums) {
+            this.nums = nums;
+            int n = nums.length;
+            len = (int) Math.ceil(nums.length / Math.sqrt(n));
+            block = new int[len];
+            for (int i = 0; i < n; i++) {
+                block[i / len] += nums[i];
+            }
+        }
+
+        // time complexity: O(N ^ (1/2))
+        public int sumRange(int i, int j) {
+            int sum = 0;
+            int startBlock = i / len;
+            int endBlock = j / len;
+            if (startBlock == endBlock) {
+                for (int k = i; k <= j; k++) {
+                    sum += nums[k];
+                }
+            } else {
+                for (int k = i; k <= (startBlock + 1) * len - 1; k++) {
+                    sum += nums[k];
+                }
+                for (int k = startBlock + 1; k <= endBlock - 1; k++) {
+                    sum += block[k];
+                }
+                for (int k = endBlock * len; k <= j; k++) {
+                    sum += nums[k];
+                }
+            }
+            return sum;
+        }
+
+        // time complexity: O(1)
+        public void update(int i, int val) {
+            block[i / len] += val - nums[i];
+            nums[i] = val;
+        }
+    }
+
     void test(INumArray obj, int[] query1, int[] update, int[] query2) {
         assertEquals(query1[2], obj.sumRange(query1[0], query1[1]));
         for (int i = 0; i < update.length - 1; i++) {
@@ -135,6 +242,8 @@ public class RangeSumQueryMutable {
         test(new NumArray(nums.clone()), query1, update, query2);
         test(new NumArray2(nums.clone()), query1, update, query2);
         test(new NumArray3(nums.clone()), query1, update, query2);
+        test(new NumArray4(nums.clone()), query1, update, query2);
+        test(new NumArray5(nums.clone()), query1, update, query2);
     }
 
     @Test
@@ -147,6 +256,9 @@ public class RangeSumQueryMutable {
         test(new int[] {7, 2, 7, 2, 0},
              new int[] {0, 2, 16}, new int[] {4, 6, 0, 2, 0, 9, 3, 8},
              new int[] {0, 4, 32});
+        test(new int[] {-28, -39, 53, 65, 11, -56, -65, -39, -43, 97},
+             new int[] {5, 6, -121}, new int[] {9, 27, 1, -82, 3, -72},
+             new int[] {1, 8, -293});
     }
 
     public static void main(String[] args) {
