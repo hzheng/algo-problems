93421f264601a157df5c9a6bc14859b1172e90aa
Hui Zheng <xyzdll@gmail.com>
2016-07-20 00:10:25 +0800

    add 3 more solutions for MinHeightTree

--- leetcode/tree_graph/MinHeightTree.java
+++ leetcode/tree_graph/MinHeightTree.java
@@ -25,6 +25,9 @@ public class MinHeightTree {
         while (adjancencies.size() > 2) {
             // keep removing all leaves
             List<Integer> leaves = new LinkedList<>();
+            // the following needs to search all adjancencies again,
+            // while BFS(e.g. next solution) only searches those vertices that
+            // are next to last leaves.
             for (Map.Entry<Integer, Set<Integer> > entry
                  : adjancencies.entrySet()) {
                 if (entry.getValue().size() == 1) {
@@ -48,15 +51,128 @@ public class MinHeightTree {
         adjancencies.get(i).add(j);
     }
 
+    // BFS
+    // beats 88.15%(32 ms)
     public List<Integer> findMinHeightTrees2(int n, int[][] edges) {
         if (edges.length == 0 || n < 2) return Arrays.asList(0);
 
-        Map<Integer, Set<Integer> > adjancencies = new HashMap<>();
-        for (int[] edge : edges) {
-            addAdjacency(adjancencies, edge[0], edge[1]);
-            addAdjacency(adjancencies, edge[1], edge[0]);
+        @SuppressWarnings("unchecked")
+        List<Integer>[] adjancencies = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            adjancencies[i] = new ArrayList<>();
         }
-        return null;
+        for (int i = 0; i < edges.length; i++) {
+            int v1 = edges[i][0];
+            int v2 = edges[i][1];
+            adjancencies[v1].add(v2);
+            adjancencies[v2].add(v1);
+        }
+        List<Integer> leaves = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (adjancencies[i].size() == 1) {
+                leaves.add(i);
+            }
+        }
+
+        for (int count = n; count > 2; ) {
+            List<Integer> nextLeaves = new LinkedList<>();
+            for (Integer leaf : leaves) {
+                for (int adjancency : adjancencies[leaf]) {
+                    adjancencies[adjancency].remove(leaf);
+                    if (adjancencies[adjancency].size() == 1) {
+                        nextLeaves.add(adjancency);
+                    }
+                }
+            }
+            count -= leaves.size();
+            leaves = nextLeaves;
+        }
+        return leaves;
+    }
+
+    // beats 15.71%(93 ms)
+    public List<Integer> findMinHeightTrees3(int n, int[][] edges) {
+        if (edges.length == 0 || n < 2) return Arrays.asList(0);
+
+        @SuppressWarnings("unchecked")
+        List<Integer>[] adjancencies = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            adjancencies[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < edges.length; i++) {
+            int v1 = edges[i][0];
+            int v2 = edges[i][1];
+            adjancencies[v1].add(v2);
+            adjancencies[v2].add(v1);
+        }
+
+        int[] degrees = new int[n];
+        for (int i = 0; i < n; i++) {
+            degrees[i] = adjancencies[i].size();
+        }
+
+        for (int vertices = n; vertices > 2; ) {
+            List<Integer> leaves = new LinkedList<>();
+            for (int i = 0; i < n; i++) {
+                if (degrees[i] == 1) {
+                    leaves.add(i);
+                    degrees[i] = -1;
+                    vertices--;
+                }
+            }
+            for (Integer leaf : leaves) {
+                Integer leafAdj = adjancencies[leaf].get(0);
+                degrees[leafAdj]--;
+                adjancencies[leafAdj].remove(leaf);
+            }
+        }
+        List<Integer> res = new ArrayList<>(2);
+        for (int i = 0; i < n; i++) {
+            if (degrees[i] != -1) {
+                res.add(i);
+            }
+        }
+        return res;
+    }
+
+    // beats 8.79%(271 ms)
+    public List<Integer> findMinHeightTrees4(int n, int[][] edges) {
+        @SuppressWarnings("unchecked")
+        Set<Integer>[] adjancencies = new HashSet[n];
+        Set<Integer> res = new HashSet<>();
+        for (int i = 0; i < n; ++i) {
+            res.add(i);
+        }
+        for (int i = 0; i < n; i++) {
+            adjancencies[i] = new HashSet<>();
+        }
+
+        for (int i = edges.length - 1; i >= 0; i--) {
+            int v1 = edges[i][0];
+            int v2 = edges[i][1];
+            adjancencies[v1].add(v2);
+            adjancencies[v2].add(v1);
+        }
+
+        Set<Integer> leaves = new HashSet<>();
+        while (res.size() > 2) {
+            for (int v : res) {
+                if (adjancencies[v].size() == 1) {
+                    leaves.add(v);
+                }
+            }
+            res.removeAll(leaves);
+            Set<Integer> nextLeaves = new HashSet<>();
+            for (int v : leaves) {
+                int leafAdj = adjancencies[v].iterator().next();
+                adjancencies[leafAdj].remove(v);
+                if (adjancencies[leafAdj].size() == 1) {
+                    nextLeaves.add(leafAdj);
+                }
+            }
+            leaves = nextLeaves;
+        }
+        return new ArrayList<>(res);
     }
 
     @FunctionalInterface
@@ -73,6 +189,9 @@ public class MinHeightTree {
     void test(int n, int[][] edges, Integer ... expected) {
         MinHeightTree m = new MinHeightTree();
         test(m::findMinHeightTrees, n, edges, expected);
+        test(m::findMinHeightTrees2, n, edges, expected);
+        test(m::findMinHeightTrees3, n, edges, expected);
+        test(m::findMinHeightTrees4, n, edges, expected);
     }
 
     @Test
