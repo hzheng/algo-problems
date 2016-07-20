ac2b2ed743364aae062af5218054672533f68030
Hui Zheng <xyzdll@gmail.com>
2016-07-20 15:33:02 +0800

    add 3 more solutions for BurstBalloons

--- leetcode/recursion_DP/BurstBalloons.java
+++ leetcode/recursion_DP/BurstBalloons.java
@@ -18,18 +18,21 @@ import static org.junit.Assert.*;
 // can not burst them.
 // (2) 0 <= n <= 500, 0 <= nums[i] <= 100
 public class BurstBalloons {
-    // beats 4.17%(32 ms)
+    // Divide and Conquer with Memoization
+    // beats 27.36%(18 ms)
     public int maxCoins(int[] nums) {
-        Integer[][] cache = new Integer[501][501];
-        return maxCoins(nums, -1, nums.length, cache);
+        int n = nums.length;
+        return maxCoins(nums, -1, n, new int[n + 1][n + 1]);
     }
 
-    private int maxCoins(int[] nums, int start, int end, Integer[][] cache) {
+    private int maxCoins(int[] nums, int start, int end, int[][] cache) {
         // String key = start + "," + end; // beats 0.93%(340 ms)
         // Pair key = Pair.create(start, end);
         // long key = (((long)end) << 32) + start; (even slower?)
         // if (cache.containsKey(key)) return cache.get(key);
-        if (cache[start + 1][end] != null) return cache[start + 1][end];
+        if (start + 1 == end) return 0; // for performance reason
+
+        if (cache[start + 1][end] > 0) return cache[start + 1][end];
 
         int max = 0;
         for (int i = start + 1; i < end; i++) {
@@ -41,6 +44,80 @@ public class BurstBalloons {
         // cache.put(key, max);
         cache[start + 1][end] = max;
         return max;
+    }
+
+    // beats 3.71%(34 ms)
+    public int maxCoins2(int[] nums) {
+        Integer[] cache = new Integer[1 << 18];
+        return maxCoins(nums, -1, nums.length, cache);
+    }
+
+    private int maxCoins(int[] nums, int start, int end, Integer[] cache) {
+        int key = (end << 9) | (start + 1);
+        if (cache[key] != null) return cache[key];
+
+        int max = 0;
+        for (int i = start + 1; i < end; i++) {
+            int cur = maxCoins(nums, start, i, cache) + maxCoins(nums, i, end, cache);
+            cur += nums[i] * (start >= 0 ? nums[start] : 1)
+                   * (end < nums.length ? nums[end] : 1);
+            max = Math.max(max, cur);
+        }
+        cache[key] = max;
+        return max;
+    }
+
+    // almost same as the first solution, but much faster(why?)
+    // beats 94.90%(11 ms)
+    public int maxCoins3(int[] nums) {
+        int[] nums2 = new int[nums.length + 2];
+        int n = 1;
+        for (int i : nums) {
+            if (i > 0) {
+                nums2[n++] = i;
+            }
+        }
+        nums2[0] = nums2[n++] = 1;
+        return burst(nums2, 0, n - 1, new int[n][n]);
+    }
+
+    private int burst(int[] nums, int start, int end, int[][] cache) {
+        if (start + 1 == end) return 0;
+
+        if (cache[start][end] > 0) return cache[start][end];
+
+        int max = 0;
+        for (int i = start + 1; i < end; ++i)
+            max = Math.max(max, nums[start] * nums[i] * nums[end]
+                           + burst(nums, start, i, cache) + burst(nums, i, end, cache));
+        cache[start][end] = max;
+        return max;
+    }
+
+    // Dynamic programming
+    // beats 61.51%(15 ms)
+    public int maxCoins4(int[] nums) {
+        int[] nums2 = new int[nums.length + 2];
+        int n = 1;
+        for (int i : nums) {
+            if (i > 0) {
+                nums2[n++] = i;
+            }
+        }
+        nums2[0] = nums2[n++] = 1;
+
+        int[][] dp = new int[n][n];
+        for (int i = 2; i < n; i++) {
+            for (int left = 0; left < n - i; left++) {
+                int right = left + i;
+                for (int j = left + 1; j < right; j++) {
+                    dp[left][right] = Math.max(dp[left][right],
+                                               nums2[left] * nums2[j] * nums2[right]
+                                               + dp[left][j] + dp[j][right]);
+                }
+            }
+        }
+        return dp[0][n - 1];
     }
 
     void test(Function<int[], Integer> maxCoins, String name,
@@ -55,6 +132,9 @@ public class BurstBalloons {
     void test(int expected, int ... nums) {
         BurstBalloons b = new BurstBalloons();
         test(b::maxCoins, "maxCoins", expected, nums);
+        test(b::maxCoins2, "maxCoins2", expected, nums);
+        test(b::maxCoins3, "maxCoins3", expected, nums);
+        test(b::maxCoins4, "maxCoins4", expected, nums);
     }
 
     @Test
