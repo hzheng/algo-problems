2bb1f9cc2a9adb295825bd60ab9365a6f76d0935
Hui Zheng <xyzdll@gmail.com>
2016-07-22 13:05:06 +0800

    add 3 more solutions to RemoveDuplicateLetters

--- leetcode/recursion_DP/RemoveDuplicateLetters.java
+++ leetcode/recursion_DP/RemoveDuplicateLetters.java
@@ -55,6 +55,7 @@ public class RemoveDuplicateLetters {
     }
 
     // Deque
+    // time complexity: O(N), space complexity: O(1)
     // beats 57.50%(9 ms)
     public String removeDuplicateLetters2(String s) {
         int[] counts = new int[26];
@@ -115,6 +116,107 @@ public class RemoveDuplicateLetters {
         return sb.toString();
     }
 
+    // https://discuss.leetcode.com/topic/35686/clean-and-easy-understand-java-stack-solution-with-explanation
+    // beats 71.41%(7 ms)
+    public String removeDuplicateLetters3(String s) {
+        boolean[] visited = new boolean[26];
+        int[] counts = new int[26];
+        for (char c : s.toCharArray()) {
+            counts[c - 'a']++;
+        }
+        Deque<Character> deque = new LinkedList<>();
+        for (char c : s.toCharArray()) {
+            int index = c - 'a';
+            counts[index]--;
+            if (visited[index]) continue;
+
+            while (!deque.isEmpty()) {
+                char d = deque.peekLast();
+                if (d > c && counts[d - 'a'] > 0) {
+                    visited[deque.pollLast() - 'a'] = false;
+                } else break;
+            }
+
+            deque.offerLast(c);
+            visited[index] = true;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!deque.isEmpty()) {
+            sb.append(deque.pollFirst());
+        }
+        return sb.toString();
+    }
+
+    // recursive
+    // https://discuss.leetcode.com/topic/31404/a-short-o-n-recursive-greedy-solution/2
+    // beats 28.13%(33 ms)
+    public String removeDuplicateLetters4(String s) {
+        int[] counts = new int[26];
+        for (char c : s.toCharArray()) {
+            counts[c - 'a']++;
+        }
+        return removeDuplicateLetters4(s, counts);
+    }
+
+    private String removeDuplicateLetters4(String s, int[] counts) {
+        if (s.isEmpty()) return "";
+
+        int min = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) < s.charAt(min)) {
+                min = i;
+            }
+            if (--counts[s.charAt(i) - 'a'] == 0) break;
+        }
+        return s.charAt(min)
+               + removeDuplicateLetters4(s.substring(min + 1)
+                                         .replaceAll("" + s.charAt(min), ""));
+    }
+
+    // https://discuss.leetcode.com/topic/31413/easy-to-understand-iterative-java-solution
+    // beats 35.09%(21 ms)
+    public String removeDuplicateLetters5(String s) {
+        if (s.length() <= 1) return s;
+
+        Map<Character, Integer> lastPosMap = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            lastPosMap.put(s.charAt(i), i);
+        }
+
+        char[] res = new char[lastPosMap.size()];
+        int begin = 0;
+        int end = findMinLastPos(lastPosMap);
+        for (int i = 0; ; i++) {
+            char minChar = 'z' + 1;
+            for (int j = begin; j <= end; j++) {
+                char c = s.charAt(j);
+                if (lastPosMap.containsKey(c) && c < minChar) {
+                    minChar = c;
+                    begin = j + 1;
+                }
+            }
+            res[i] = minChar;
+            if (i == res.length - 1) break;
+
+            lastPosMap.remove(minChar);
+            if (s.charAt(end) == minChar) {
+                end = findMinLastPos(lastPosMap);
+            }
+        }
+        return new String(res);
+    }
+
+    private int findMinLastPos(Map<Character, Integer> lastPosMap) {
+        if (lastPosMap.isEmpty()) return -1;
+
+        int minLastPos = Integer.MAX_VALUE;
+        for (int lastPos : lastPosMap.values()) {
+            minLastPos = Math.min(minLastPos, lastPos);
+        }
+        return minLastPos;
+    }
+
     void test(Function<String, String> remove, String name,
               String s, String expected) {
         long t1 = System.nanoTime();
@@ -130,6 +232,9 @@ public class RemoveDuplicateLetters {
             test(r::removeDuplicateLetters, "removeDuplicateLetters", s, expected);
         }
         test(r::removeDuplicateLetters2, "removeDuplicateLetters2", s, expected);
+        test(r::removeDuplicateLetters3, "removeDuplicateLetters3", s, expected);
+        test(r::removeDuplicateLetters4, "removeDuplicateLetters4", s, expected);
+        test(r::removeDuplicateLetters5, "removeDuplicateLetters5", s, expected);
     }
 
     @Test
