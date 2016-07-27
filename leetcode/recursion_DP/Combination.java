961d59e04bfe80c782dd0f46959fa776cd487bd2
Hui Zheng <xyzdll@gmail.com>
2016-07-28 01:27:58 +0800

    add 2 more solutions for Combination

--- leetcode/recursion_DP/Combination.java
+++ leetcode/recursion_DP/Combination.java
@@ -3,9 +3,11 @@ import java.util.*;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+// https://leetcode.com/problems/combinations/
+//
 // Return all possible combinations of k numbers out of 1 ... n.
 public class Combination {
-    // beats 30.91%
+    // beats 90.59%(4 ms)
     public List<List<Integer> > combine(int n, int k) {
         List<List<Integer> > res = new ArrayList<>();
         combine(n, k, res, new ArrayList<>());
@@ -28,14 +30,65 @@ public class Combination {
         }
     }
 
+    // https://discuss.leetcode.com/topic/12537/a-short-recursive-java-solution-based-on-c-n-k-c-n-1-k-1-c-n-1-k
+    // beats 74.37%(14 ms)
+    public List<List<Integer> > combine2(int n, int k) {
+        if (k == n || k == 0) {
+            List<Integer> row = new LinkedList<>();
+            for (int i = 1; i <= k; i++) {
+                row.add(i);
+            }
+            return new LinkedList<>(Arrays.asList(row));
+        }
+
+        List<List<Integer> > res = combine2(n - 1, k - 1);
+        // res.forEach(e -> e.add(n)); // slow(98 ms)
+        for (List<Integer> list : res) {
+            list.add(n);
+        }
+        res.addAll(combine2(n - 1, k));
+        return res;
+    }
+
+    // iterative by bit manipulation
+    // https://graphics.stanford.edu/~seander/bithacks.html#NextBitPermutation
+    // beats 86.24%(6 ms)
+    public List<List<Integer> > combine3(int n, int k) {
+        List<List<Integer>> res = new ArrayList<>();
+        for (int v = (1 << k) - 1; v < (1 << n); ) {
+            res.add(codeToList(v, n));
+            int t = v | (v - 1); // set least significant 0 bit to 1
+            // set to 1 the most significant bit to change,
+            // set to 0 the least significant ones, and add the necessary 1 bits.
+            v = (t + 1) | (((~t & -~t) - 1) >> (trailingZero(v) + 1));
+        }
+        return res;
+    }
+
+    private List<Integer> codeToList(int code, int n) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = 1, mask = 1; i <= n; i++, mask <<= 1) {
+            if ((code & mask) != 0) {
+                res.add(i);
+            }
+        }
+        return res;
+    }
+
+    private int trailingZero(int n) {
+        int count = 0;
+        for (int x = (n ^ (n - 1)) >> 1; x > 0; count++, x >>= 1) {}
+        return count;
+    }
+
     @FunctionalInterface
     interface Function<A, B, C> {
         public C apply(A a, B b);
     }
 
     void test(Function<Integer, Integer, List<List<Integer>>> combine,
-     String name, int n, int k, Integer[][] expected) {
-        List<List<Integer>> res = combine.apply(n, k);
+              String name, int n, int k, Integer[][] expected) {
+        List<List<Integer> > res = combine.apply(n, k);
         Integer[][] resArray = res.stream().map(
             a -> a.toArray(new Integer[0])).toArray(Integer[][]::new);
         sort(resArray);
@@ -58,6 +111,8 @@ public class Combination {
     void test(int n, int k, Integer[][] expected) {
         Combination c = new Combination();
         test(c::combine, "combine", n, k, expected);
+        test(c::combine2, "combine2", n, k, expected);
+        test(c::combine3, "combine3", n, k, expected);
     }
 
     @Test
