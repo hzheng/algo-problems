a49d16e15dafb84c09c23d61d5256f093dbc9c24
Hui Zheng <xyzdll@gmail.com>
2016-08-07 12:52:50 +0800

    add 2 more solutions for MaxSumSubmatrix

--- leetcode/recursion_DP/MaxSumSubmatrix.java
+++ leetcode/recursion_DP/MaxSumSubmatrix.java
@@ -1,9 +1,9 @@
-import java.util.*;
+// https://leetcode.com/problems/max-sum-of-sub-matrix-no-larger-than-k/
 
+import java.util.*;
+import java.util.NavigableSet;
 import org.junit.Test;
 import static org.junit.Assert.*;
-
-// https://leetcode.com/problems/max-sum-of-sub-matrix-no-larger-than-k/
 //
 // Given a non-empty 2D matrix matrix and an integer k, find the max sum of a
 // rectangle in the matrix such that its sum is no larger than k.
@@ -25,7 +25,7 @@ public class MaxSumSubmatrix {
                                      + sums[i][j + 1] - sums[i][j];
             }
         }
-    
+
         int diff = Integer.MAX_VALUE;
         for (int i1 = 0; i1 < m; i1++) {
             for (int j1 = 0; j1 < n; j1++) {
@@ -45,8 +45,78 @@ public class MaxSumSubmatrix {
         return k - diff;
     }
 
+    // https://discuss.leetcode.com/topic/48923/2-accepted-java-solution/5
+    // https://discuss.leetcode.com/topic/48854/java-binary-search-solution-time-complexity-min-m-n-2-max-m-n-log-max-m-n/2
+    // Dynamic Programming
+    // time complexity: O(M ^ 2 * N * log(N)), space complexity: O(M * N)
+    // Time Limit Exceeded
+    public int maxSumSubmatrix2(int[][] matrix, int k) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int[][] sums = new int[m + 1][n + 1];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                sums[i + 1][j + 1] = matrix[i][j] + sums[i + 1][j]
+                                     + sums[i][j + 1] - sums[i][j];
+            }
+        }
+
+        int maxSum = Integer.MIN_VALUE;
+        for (int i1 = 0; i1 < m; i1++) {
+            for (int i2 = i1; i2 < m; i2++) {
+                NavigableSet<Integer> set = new TreeSet<>();
+                set.add(0); // padding(needed)
+                for (int j = 0; j < n; j++) {
+                    int sum = sums[i2 + 1][j + 1] - sums[i1][j + 1];
+                    Integer ceiling = set.ceiling(sum - k);
+                    if (ceiling != null) {
+                        maxSum = Math.max(maxSum, sum - ceiling);
+                    }
+                    set.add(sum);
+                }
+            }
+        }
+        return maxSum;
+    }
+
+    // time complexity: O(min(M, N) ^ 2 * min(M, N) * log(max(M, N)), space complexity: O(max(M, N))
+    // https://discuss.leetcode.com/topic/48875/accepted-c-codes-with-explanation-and-references/5
+    // beats 53.01%(205 ms)
+    public int maxSumSubmatrix3(int[][] matrix, int k) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean biggerRow = m > n;
+        m = Math.max(m, n);
+        n = Math.min(matrix.length, n);
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            int sums[] = new int[m];
+            for (int j = i; j < n; j++) {
+                NavigableSet<Integer> set = new TreeSet<>();
+                // set.add(0); // needed only if area can be zero
+                int num = 0;
+                for (int l = 0; l < m; l++) {
+                    sums[l] += biggerRow ? matrix[l][j] : matrix[j][l];
+                    num += sums[l];
+                    if (num <= k) {
+                        maxSum = Math.max(maxSum, num);
+                    }
+                    Integer minDiff = set.ceiling(num - k);
+                    if (minDiff != null) {
+                        maxSum = Math.max(maxSum, num - minDiff);
+                    }
+                    set.add(num);
+                }
+            }
+        }
+        return maxSum;
+    }
+
     void test(int[][] matrix, int k, int expected) {
         assertEquals(expected, maxSumSubmatrix(matrix, k));
+        assertEquals(expected, maxSumSubmatrix2(matrix, k));
+        assertEquals(expected, maxSumSubmatrix3(matrix, k));
     }
 
     @Test
