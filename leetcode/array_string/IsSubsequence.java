9af8caa7447bcec4e972291c1588ec3367ae9c07
Hui Zheng <xyzdll@gmail.com>
2016-09-06 23:40:24 +0800

    add IsSubsequence.java

---
+++ leetcode/array_string/IsSubsequence.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+// LC392: https://leetcode.com/problems/is-subsequence/
+public class IsSubsequence {
+    // beats N/A(31 ms)
+    public boolean isSubsequence(String s, String t) {
+        int sLen = s.length();
+        int tLen = t.length();
+        int i = 0;
+        for (int j = 0; i < sLen && j < tLen; j++) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+        }
+        return i == sLen;
+    }
+
+    public boolean isSubsequence2(String s, String t) {
+        int sLen = s.length();
+        int tLen = t.length();
+        int i = 0;
+        for (int j = 0; i < sLen && j < tLen; j++) {
+            if (s.charAt(i) == t.charAt(j)) {
+                if (++i == sLen) return true;
+            }
+        }
+        return sLen == 0;
+    }
+
+    void test(String s, String t, boolean expected) {
+        assertEquals(expected, isSubsequence(s, t));
+        assertEquals(expected, isSubsequence2(s, t));
+    }
+
+    @Test
+    public void test1() {
+        test("", "ace", true);
+        test("ac", "ace", true);
+        test("ace", "abcde", true);
+        test("aec", "abcde", false);
+        test("abc", "ahbgdc", true);
+        test("axc", "ahbgdc", false);
+        test("abcd", "ahbagdbcccaaad", true);
+        test("abcd", "ahbagdbcccaaa", false);
+    }
+
+    public static void main(String[] args) {
+        org.junit.runner.JUnitCore.main("IsSubsequence");
+    }
+}
