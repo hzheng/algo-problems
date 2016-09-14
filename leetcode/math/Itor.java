0dda0b9f1f0103a8101d1035a992605a602d5c23
Hui Zheng <xyzdll@gmail.com>
2016-09-14 22:01:10 +0800

    add 2 solutions for Itor and Rtoi respectively

--- leetcode/math/Itor.java
+++ leetcode/math/Itor.java
@@ -1,13 +1,15 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+// LC012: https://leetcode.com/problems/integer-to-roman/
+//
 // Given an integer, convert it to a roman numeral.
 // Input is guaranteed to be within the range from 1 to 3999.
 public class Itor {
     private static final int[] ROMAN_VALUES = {1000, 500, 100, 50, 10, 5, 1};
     private static final char[] ROMAN_LETTERS = {'M', 'D', 'C', 'L', 'X', 'V', 'I'};
 
-    // beats 68.22%
+    // beats 68.22%(8 ms)
     public String intToRoman(int num) {
         if (num <= 0) return "";
 
@@ -15,6 +17,7 @@ public class Itor {
         for (int i = 0; i < ROMAN_LETTERS.length; i += 2) {
             int unit = ROMAN_VALUES[i];
             int digit = num / unit;
+            num -= digit * unit;
             if (digit == 9) {
                 sb.append(ROMAN_LETTERS[i]);
                 sb.append(ROMAN_LETTERS[i - 2]);
@@ -31,13 +34,49 @@ public class Itor {
                     sb.append(ROMAN_LETTERS[i]);
                 }
             }
-            num -= digit * ROMAN_VALUES[i];
+        }
+        return sb.toString();
+    }
+
+    // Solution of Choice
+    // https://discuss.leetcode.com/topic/12384/simple-solution
+    // beats %(6 ms)
+    private static final String M[] = {"", "M", "MM", "MMM"};
+    private static final String C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+    private static final String X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+    private static final String I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+
+    public String intToRoman2(int num) {
+        return M[num / 1000] + C[(num % 1000) / 100] + X[(num % 100) / 10] + I[num % 10];
+    }
+
+    // https://discuss.leetcode.com/topic/26543/easy-to-understand-java-solution
+    // beats %(15 ms)
+    private static enum Unit {
+        M(1000), CM(900), D(500), CD(400), C(100),
+        XC(90), L(50), XL(40), X(10), IX(9), V(5), IV(4), I(1);
+
+        private final int value;
+
+        Unit(int value) {
+            this.value = value;
+        }
+    };
+
+    public String intToRoman3(int num) {
+        StringBuilder sb = new StringBuilder();
+        for (Unit unit : Unit.values()) {
+            for (; num >= unit.value; num -= unit.value) {
+                sb.append(unit);
+            }
         }
         return sb.toString();
     }
 
     void test(int x, String expected) {
         assertEquals(expected, intToRoman(x));
+        assertEquals(expected, intToRoman2(x));
+        assertEquals(expected, intToRoman3(x));
     }
 
     @Test
@@ -56,9 +95,13 @@ public class Itor {
         test(1990, "MCMXC");
         test(1954, "MCMLIV");
         test(114, "CXIV");
+        test(94, "XCIV");
+        test(114, "CXIV");
         test(101, "CI");
         test(1001, "MI");
         test(999, "CMXCIX");
+        test(2999, "MMCMXCIX");
+        test(3999, "MMMCMXCIX");
     }
 
     public static void main(String[] args) {
--- leetcode/math/Rtoi.java
+++ leetcode/math/Rtoi.java
@@ -1,21 +1,23 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+// LC013: https://leetcode.com/problems/roman-to-integer/
+//
 // Given a roman numeral, convert it to an integer.
 // Input is guaranteed to be within the range from 1 to 3999.
 public class Rtoi {
     // more rigid on input which doesn't match the accepted answer
-    public int romanToInt(String str) {
-        if (str == null) return 0;
+    public int romanToInt(String s) {
+        if (s == null) return 0;
 
-        int len = str.length();
+        int len = s.length();
         if (len == 0) return 0;
 
         int val = 0;
         int count = 0;
         int lastUnit = 0;
         for (int i = 0; i < len; i++) {
-            char c = str.charAt(i);
+            char c = s.charAt(i);
             int curUnit = getValue(c);
             if (curUnit == 0) continue;
 
@@ -55,35 +57,88 @@ public class Rtoi {
         return (unit1 * 5 == unit2) || (unit1 * 10 == unit2);
     }
 
-    // beats 67.32% (82.35% if set cur = str.charAt(i)???)
-    public int romanToInt2(String str) {
-        int[] vals = new int[255];
-		vals['I'] = 1;
-        vals['V'] = 5;
-        vals['X'] = 10;
-        vals['L'] = 50;
-		vals['C'] = 100;
-        vals['D'] = 500;
-        vals['M'] = 1000;
-		int val = 0;
-        int len = str.length();
-        
-		for (int i = 0; i < len; i++) {
-            int cur = vals[str.charAt(i)];
-            int next = (i == len - 1) ? 0 : vals[str.charAt(i + 1)];
+    // beats 8X.??%(6 ms)
+    public int romanToInt2(String s) {
+        int[] vals = new int[26];
+        vals['I' - 'A'] = 1;
+        vals['V' - 'A'] = 5;
+        vals['X' - 'A'] = 10;
+        vals['L' - 'A'] = 50;
+        vals['C' - 'A'] = 100;
+        vals['D' - 'A'] = 500;
+        vals['M' - 'A'] = 1000;
+        int val = 0;
+        int len = s.length();
+
+        for (int i = 0; i < len; i++) {
+            int cur = vals[s.charAt(i) - 'A'];
+            int next = (i == len - 1) ? 0 : vals[s.charAt(i + 1) - 'A'];
             if (cur >= next) {
-                 val += cur;
-             } else {
-                 val += next - cur;
-                 ++i;
-             }
-		}
-		return val;
+                val += cur;
+            } else {
+                val += next - cur;
+                ++i;
+            }
+        }
+        return val;
+    }
+
+    // Solution of Choice
+    // beats 8X.??%(6 ms)
+    public int romanToInt3(String s) {
+        int[] vals = new int[26];
+        vals['I' - 'A'] = 1;
+        vals['V' - 'A'] = 5;
+        vals['X' - 'A'] = 10;
+        vals['L' - 'A'] = 50;
+        vals['C' - 'A'] = 100;
+        vals['D' - 'A'] = 500;
+        vals['M' - 'A'] = 1000;
+        int num = 0;
+        for (int i = s.length() - 1, last = 0; i >= 0; i--) {
+            int cur = vals[s.charAt(i) - 'A'];
+            num += (cur < last) ? -cur : cur;
+            last = cur;
+        }
+        return num;
+    }
+
+    // beats 82.35%(7 ms)
+    public int romanToInt4(String s) {
+        int num = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            switch (s.charAt(i)) {
+            case 'I':
+                num += (num < 5) ? 1 : -1;
+                break;
+            case 'V':
+                num += 5;
+                break;
+            case 'X':
+                num += (num < 50) ? 10 : -10;
+                break;
+            case 'L':
+                num += 50;
+                break;
+            case 'C':
+                num += (num < 500) ? 100 : -100;
+                break;
+            case 'D':
+                num += 500;
+                break;
+            case 'M':
+                num += 1000;
+                break;
+            }
+        }
+        return num;
     }
 
     void test(String x, int expected) {
         assertEquals(expected, romanToInt(x));
         assertEquals(expected, romanToInt2(x));
+        assertEquals(expected, romanToInt3(x));
+        assertEquals(expected, romanToInt4(x));
     }
 
     @Test
@@ -98,10 +153,12 @@ public class Rtoi {
         test("XL", 40);
         test("CD", 400);
         test("CM", 900);
+        test("XCIV", 94);
+        test("CXIV", 114);
         test("MMXIV", 2014);
         test("MCMXC", 1990);
         test("MCMLIV", 1954);
-        test("VIXC", 114);
+        test("MMMCMXCIX", 3999);
     }
 
     @Test
