813e988d7cf11ff323126635cd7a40d859731cdc
Hui Zheng <xyzdll@gmail.com>
2016-09-23 01:32:12 +0800

    add 1 more solutin for NextPermutation

--- leetcode/array_string/NextPermutation.java
+++ leetcode/array_string/NextPermutation.java
@@ -3,10 +3,12 @@ import java.util.*;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+// LC031: https://leetcode.com/problems/next-permutation/
+//
 // Implement next permutation, which rearranges numbers into the
 // lexicographically next greater permutation of numbers.
 public class NextPermutation {
-    // beats 69.33% (why sometimes 9.15%?)
+    // beats 64.50%(1 ms)
     public void nextPermutation(int[] nums) {
         int len = nums.length;
         int i = len - 1;
@@ -28,21 +30,43 @@ public class NextPermutation {
         }
     }
 
-    // beats 69.33%
+    // Solution of Choice
+    // beats 64.50%(1 ms)
     public void nextPermutation2(int[] nums) {
         int len = nums.length;
         int i = len - 1;
-        for (; i > 0 && nums[i - 1] >= nums[i]; i--);
+        for (; i > 0 && nums[i - 1] >= nums[i]; i--) {}
 
         if (i > 0) {
             int j = len - 1;
-            for ( ; j >= 0 && nums[j] <= nums[i - 1]; j--) ;
+            for ( ; j >= 0 && nums[j] <= nums[i - 1]; j--) {} // or binary search
             swap(nums, i - 1, j);
         }
         // reverse from i to the end
-        for (int j = len - 1; i < j; i++, j--) {
+        for (int j = len - 1; j > i; i++, j--) {
             swap(nums, i, j);
         }
+    }
+
+    // Binary Search
+    // beats 4.82%(22 ms)
+    public void nextPermutation3(int[] nums) {
+        int len = nums.length;
+        int i = len - 2;
+        for (; i >= 0 && nums[i] >= nums[i + 1]; i--) {}
+
+        for (int j = len - 1, k = i + 1; j > k; k++, j--) {
+            swap(nums, k, j);
+        }
+        if (i < 0) return;
+
+        int index = Arrays.binarySearch(nums, i + 1, len, nums[i]);
+        if (index < 0) {
+            index = -index - 1;
+        } else {
+            while (++index < len && nums[index] == nums[i]) {}
+        }
+        swap(nums, i, index);
     }
 
     private void swap(int[] nums, int i, int j) {
@@ -51,13 +75,23 @@ public class NextPermutation {
         nums[j] = tmp;
     }
 
-    void test(int[] nums, int[] expected) {
+    @FunctionalInterface
+    interface Function<A> {
+        public void apply(A a);
+    }
+
+    void test(Function<int[]> next, int[] nums, int[] expected) {
         int[] input = nums.clone();
-        nextPermutation(input);
+        next.apply(input);
+        System.out.println(Arrays.toString(input));
         assertArrayEquals(expected, input);
-        nextPermutation2(nums);
-        assertArrayEquals(expected, nums);
-        System.out.println(Arrays.toString(nums));
+    }
+
+    void test(int[] nums, int[] expected) {
+        NextPermutation n = new NextPermutation();
+        test(n::nextPermutation, nums, expected);
+        test(n::nextPermutation2, nums, expected);
+        test(n::nextPermutation3, nums, expected);
     }
 
     @Test
@@ -67,8 +101,14 @@ public class NextPermutation {
         test(new int[] {1, 2, 3}, new int[] {1, 3, 2});
         test(new int[] {3, 2, 1}, new int[] {1, 2, 3});
         test(new int[] {1, 1, 5}, new int[] {1, 5, 1});
-        test(new int[] {1, 3, 2}, new int[] {2, 1, 3});
         test(new int[] {1, 5, 1}, new int[] {5, 1, 1});
+        test(new int[] {1, 3, 2}, new int[] {2, 1, 3});
+        test(new int[] {1, 5, 8, 4, 7, 6, 5, 3, 1},
+             new int[] {1, 5, 8, 5, 1, 3, 4, 6, 7});
+        test(new int[] {2, 1, 2, 2, 2, 2, 2, 1},
+             new int[] {2, 2, 1, 1, 2, 2, 2, 2});
+        test(new int[] {2, 2, 7, 5, 4, 3, 2, 2, 1},
+             new int[] {2, 3, 1, 2, 2, 2, 4, 5, 7});
     }
 
     public static void main(String[] args) {
