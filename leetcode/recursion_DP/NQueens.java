ca4b20ab5cd6620a7b1a702ff614da0594a0c588
Hui Zheng <xyzdll@gmail.com>
2016-09-26 21:57:15 +0800

    add solutions for NQueens and merge EightQueens.java

--- leetcode/recursion_DP/NQueens.java
+++ leetcode/recursion_DP/NQueens.java
@@ -4,13 +4,16 @@ import java.util.function.Function;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+// LC051: https://leetcode.com/problems/n-queens/
+// Cracking the Coding Interview(5ed) Problem 9.9
+//
 // The n-queens puzzle
 public class NQueens {
-    // https://leetcode.com/problems/n-queens/
-    // beats 85.11%
+    // beats 85.11%(5 ms)
     public static final char QUEEN = 'Q';
     public static final char SPACE = '.';
 
+    // Backtracking
     public List<List<String> > solveNQueens(int n) {
         if (n == 0) return Collections.emptyList();
 
@@ -87,45 +90,10 @@ public class NQueens {
         }
     }
 
-    // N-Queens II
-    // https://leetcode.com/problems/n-queens-ii/
-    // beats 85.11%
-    // we can improve efficiency by removing board but that
-    // need change class Marker's code
-    public int totalNQueens(int n) {
-        if (n == 0) return 0;
-
-        char[][] board = new char[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                board[i][j] = SPACE;
-            }
-        }
-        int[] solutions = new int[1];
-        Marker marker = new Marker(board);
-        fill(board, 0, marker, solutions);
-        return solutions[0];
-    }
-
-    private void fill(char[][] board, int row, Marker marker,
-                      int[] solutions) {
-        int n = board.length;
-        if (row == n) {
-            solutions[0]++;
-            return;
-        }
-
-        for (int col = 0; col < n; col++) {
-            if (marker.safe(row, col)) {
-                marker.mark(row, col);
-                fill(board, row + 1, marker, solutions);
-                marker.unmark(row, col);
-            }
-        }
-    }
-
+    // Solution of Choice
+    // Backtracking
     // adapted from EightQueens.java
-    // beats 85.11%
+    // beats 85.11%(5 ms)
     public List<List<String> > solveNQueens2(int n) {
         List<List<String> > solutions = new ArrayList<>();
         placeQueens(solutions, new int[n], 0);
@@ -162,11 +130,112 @@ public class NQueens {
         for (int row2 = 0; row2 < row1; row2++) {
             int col2 = occupiedCols[row2];
             if (col1 == col2) return false;
-            if ((row1 - row2) == Math.abs(col2 - col1)) {
-                return false;
-            }
+            if ((row1 - row2) == Math.abs(col2 - col1)) return false;
         }
         return true;
+    }
+
+    //=======================================================
+    // LC052: https://leetcode.com/problems/n-queens-ii/
+    // N-Queens II
+    //
+    // Backtracking
+    // we can improve efficiency by removing board but that
+    // need change class Marker's code
+    // beats 34.55%(6 ms)
+    public int totalNQueens(int n) {
+        if (n == 0) return 0;
+
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = SPACE;
+            }
+        }
+        int[] solutions = new int[1];
+        Marker marker = new Marker(board);
+        fill(board, 0, marker, solutions);
+        return solutions[0];
+    }
+
+    private void fill(char[][] board, int row, Marker marker,
+                      int[] solutions) {
+        int n = board.length;
+        if (row == n) {
+            solutions[0]++;
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            if (marker.safe(row, col)) {
+                marker.mark(row, col);
+                fill(board, row + 1, marker, solutions);
+                marker.unmark(row, col);
+            }
+        }
+    }
+
+    // Backtracking
+    // beats 51.14%(4 ms)
+    public int totalNQueens2(int n) {
+        int[] solutions = new int[1];
+        placeQueens(solutions, new int[n], 0);
+        return solutions[0];
+    }
+
+    private void placeQueens(int[] solutions, int[] occupiedCols, int row) {
+        int n = occupiedCols.length;
+        if (row == n) {
+            solutions[0]++;
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            if (isSafe(row, col, occupiedCols)) {
+                occupiedCols[row] = col;
+                placeQueens(solutions, occupiedCols, row + 1);
+            }
+        }
+    }
+
+    // Solution of Choice
+    // Backtracking
+    // https://discuss.leetcode.com/topic/29626/easiest-java-solution-1ms-98-22
+    // beats 87.95%(2 ms)
+    public int totalNQueens3(int n) {
+        int[] solutions = new int[1];
+        placeQueens(n, solutions, 0, new boolean[n],
+                    new boolean[n * 2], new boolean[n * 2]);
+        return solutions[0];
+    }
+
+    private void placeQueens(int n, int[] solutions, int row, boolean[] cols,
+                             boolean[] diagonal1, boolean[] diagonal2) {
+        if (row == n) {
+            solutions[0]++;
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            int i1 = col - row + n;
+            int i2 = col + row;
+            if (cols[col] || diagonal1[i1] || diagonal2[i2]) continue;
+
+            cols[col] = diagonal1[i1] = diagonal2[i2] = true;
+            placeQueens(n, solutions, row + 1, cols, diagonal1, diagonal2);
+            cols[col] = diagonal1[i1] = diagonal2[i2] = false;
+        }
+    }
+
+    private void test(Function<Integer, List<List<String>>> placeQueens, int n) {
+        List<List<String> > results = placeQueens.apply(n);
+        for (List<String> result : results) {
+            for (int i = 0; i < result.size(); i++) {
+                System.out.format("(%d, %s) ", i, result.get(i));
+            }
+            System.out.println();
+        }
+        System.out.println("total solutions: " + results.size());
     }
 
     void test(Function<Integer, List<List<String>>> solve, String name,
@@ -183,6 +252,8 @@ public class NQueens {
         test(q::solveNQueens, "solveNQueens", expected, n);
         test(q::solveNQueens2, "solveNQueens2", expected, n);
         assertEquals(expected, totalNQueens(n));
+        assertEquals(expected, totalNQueens2(n));
+        assertEquals(expected, totalNQueens3(n));
     }
 
     @Test
@@ -193,6 +264,14 @@ public class NQueens {
         test(724, 10);
         test(14200, 12);
         // test(365596, 14);
+    }
+
+    // @Test
+    public void test2() {
+        NQueens q = new NQueens();
+        test(q::solveNQueens, 8);
+        System.out.println("=====================");
+        test(q::solveNQueens2, 8);
     }
 
     public static void main(String[] args) {
