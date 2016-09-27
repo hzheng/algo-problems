c29729d8095278daec754de91742d53027787623
Hui Zheng <xyzdll@gmail.com>
2016-09-27 13:24:09 +0800

    add 2 more solutions for ValidNumber and improve one

--- leetcode/math/ValidNumber.java
+++ leetcode/math/ValidNumber.java
@@ -1,11 +1,14 @@
 import java.util.*;
+import java.util.regex.Pattern;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+// LC065: https://leetcode.com/problems/valid-number/
+//
 // Validate if a given string is numeric.
 public class ValidNumber {
-    // beats 52.09%
+    // beats 58.20%(4 ms)
     public boolean isNumber(String s) {
         int len = s.length();
         int start = 0;
@@ -50,82 +53,89 @@ public class ValidNumber {
         return !needDigit;
     }
 
+    // Solution of Choice
     // finite automata
     // http://blog.csdn.net/kenden23/article/details/18696083
-    // beats 52.09%
+    // beats 90.78%(3 ms)
+    private static final int[] INPUT_MAP = new int[256];
+    static {
+        INPUT_MAP[' '] = 1;
+        INPUT_MAP['+'] = INPUT_MAP['-'] = 2;
+        for (char c = '0'; c <= '9'; c++) { INPUT_MAP[c] = 3; }
+        INPUT_MAP['.'] = 4;
+        INPUT_MAP['e'] = 5;
+    }
+
+    private static final int ERROR = -1; // error state
+    private static final int INIT = 0;  // initial state
+    private static final int DIGIT_STATE = 1;   // digit state
+    private static final int DOT_STATE = 2;   // dot state
+    private static final int SIGN_STATE = 3;   // sign state
+    private static final int NUM_DOT = 4;   // digit+dot state
+    private static final int EXP_STATE = 5;   // exponential state
+    private static final int EXP_SIGN = 6;   // exponential+sign state
+    private static final int EXP_NUM = 7;   // exponential+digit state
+    private static final int TERM = 8;   // terminate after space
+
+    private static final int transitionTable[][] = {
+        // SPACE SIGN       DIGIT        DOT        EXP
+        {INIT,  SIGN_STATE, DIGIT_STATE, DOT_STATE, ERROR},       // INIT
+        {TERM,  ERROR,      DIGIT_STATE, NUM_DOT,   EXP_STATE},   // DIGIT_STATE
+        {ERROR, ERROR,      NUM_DOT,     ERROR,     ERROR},       // DOT_STATE
+        {ERROR, ERROR,      DIGIT_STATE, DOT_STATE, ERROR},       // SIGN_STATE
+        {TERM,  ERROR,      NUM_DOT,     ERROR,     EXP_STATE},   // NUM_DOT
+        {ERROR, EXP_SIGN,   EXP_NUM,     ERROR,     ERROR},       // EXP_STATE
+        {ERROR, ERROR,      EXP_NUM,     ERROR,     ERROR},       // EXP_SIGN
+        {TERM,  ERROR,      EXP_NUM,     ERROR,     ERROR},       // EXP_NUM
+        {TERM,  ERROR,      ERROR,       ERROR,     ERROR}        // TERM
+    };
+
     public boolean isNumber2(String s) {
-        int i = 0;
-        // input values
-        final int SPACE = i++;
-        final int SIGN = i++;
-        final int DIGIT = i++;
-        final int DOT = i++;
-        final int EXP = i++;
-
-        // state values
-        i = -1;
-        final int ERROR = i++; // error state
-        final int INIT = i++;  // initial state
-        final int DIGIT_STATE = i++;   // digit state
-        final int DOT_STATE = i++;   // dot state
-        final int SIGN_STATE = i++;   // sign state
-        final int NUM_DOT = i++;   // digit+dot state
-        final int EXP_STATE = i++;   // exponential state
-        final int EXP_SIGN = i++;   // exponential+sign state
-        final int EXP_NUM = i++;   // exponential+digit state
-        final int TERM = i++;   // terminate after space
-
-        int transitionTable[][] = {
-            //SPACE SIGN        DIGIT        DOT        EXP
-            {INIT,  SIGN_STATE, DIGIT_STATE, DOT_STATE, ERROR},     // INIT
-            {TERM,  ERROR,      DIGIT_STATE, NUM_DOT,   EXP_STATE}, // DIGIT_STATE
-            {ERROR, ERROR,      NUM_DOT,     ERROR,     ERROR},     // DOT_STATE
-            {ERROR, ERROR,      DIGIT_STATE, DOT_STATE, ERROR},     // SIGN_STATE
-            {TERM,  ERROR,      NUM_DOT,     ERROR,     EXP_STATE}, // NUM_DOT
-            {ERROR, EXP_SIGN,   EXP_NUM,     ERROR,     ERROR},     // EXP_STATE
-            {ERROR, ERROR,      EXP_NUM,     ERROR,     ERROR},     // EXP_SIGN
-            {TERM,  ERROR,      EXP_NUM,     ERROR,     ERROR},     // EXP_NUM
-            {TERM,  ERROR,      ERROR,       ERROR,     ERROR}      // TERM
-        };
         int state = 0;
         for (char c : s.toCharArray()) {
-            int input;
-            switch (c) {
-            case ' ':
-                input = SPACE;
-                break;
-            case '+': case '-':
-                input = SIGN;
-                break;
-            case '.':
-                input = DOT;
-                break;
-            case 'e':
-                input = EXP;
-                break;
-            case '0': case '1': case '2': case '3': case '4':
-            case '5': case '6': case '7': case '8': case '9':
-                input = DIGIT;
-                break;
-            default:
-                return false;
-            }
+            int index = INPUT_MAP[c] - 1;
+            if (index < 0) return false;
 
-            state = transitionTable[state][input];
+            state = transitionTable[state][index];
             if (state == ERROR) return false;
         }
         return (state == DIGIT_STATE) || (state == NUM_DOT)
                || (state == EXP_NUM) || (state == TERM);
     }
 
-    // TODO: regular expression
-    // s = s.trim();
-    // if (s.matches("[+-]?\\d*\\.?\\d+")) return true;
-    // if (s.matches("[+-]?\\d+\\.?\\d*")) return true;
+    // Regex
+    // beats 26.02%(12 ms)
+    private static final Pattern VALID_PAT
+        = Pattern.compile(" *[+-]?(\\d+(\\.\\d*)?|\\.\\d+)(e[+-]?\\d+)? *");
+
+    public boolean isNumber3(String s) {
+        return VALID_PAT.matcher(s).matches();
+    }
+
+    // kind of cheating
+    // beats 19.47%(22 ms)
+    public boolean isNumber4(String s) {
+        try {
+            s = s.trim();
+            int n = s.length();
+            if (n == 0) return false;
+
+            char c = s.charAt(n - 1);
+            if (c != '.' && !Character.isDigit(c)) return false;
+
+            double i = Double.parseDouble(s);
+            return true;
+        }
+        catch (NumberFormatException e) {
+            return false;
+        }
+    }
 
     void test(String s, boolean expected) {
         assertEquals(expected, isNumber(s));
         assertEquals(expected, isNumber2(s));
+        assertEquals(expected, isNumber3(s));
+        assertEquals(expected, isNumber4(s));
     }
 
     @Test
