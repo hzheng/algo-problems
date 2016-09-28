97baaf260538c68c77c29c36a4d6b83b9a9a844b
Hui Zheng <xyzdll@gmail.com>
2016-09-28 17:10:57 +0800

    add 1 more solution for RemoveDupList

--- leetcode/linkedlist/RemoveDupList.java
+++ leetcode/linkedlist/RemoveDupList.java
@@ -6,9 +6,11 @@ import static org.junit.Assert.*;
 
 import common.ListNode;
 
+// LC083: https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+//
 // Given a sorted linked list, delete all duplicates.
 public class RemoveDupList {
-    // beats 16.98%
+    // beats 16.55%(1 ms)
     public ListNode deleteDuplicates(ListNode head) {
         if (head == null) return null;
 
@@ -22,6 +24,15 @@ public class RemoveDupList {
         return head;
     }
 
+    // Recursion
+    // beats 6.62%(2 ms)
+    public ListNode deleteDuplicates2(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        head.next = deleteDuplicates(head.next);
+        return head.val == head.next.val ? head.next : head;
+    }
+
     void test(Function<ListNode, ListNode> removeDup, int [] nums, int[] expected) {
         nums = nums.clone();
         int[] res = removeDup.apply(ListNode.of(nums)).toArray();
@@ -31,10 +42,13 @@ public class RemoveDupList {
     void test(int[] nums, int[] expected) {
         RemoveDupList rm = new RemoveDupList();
         test(rm::deleteDuplicates, nums, expected);
+        test(rm::deleteDuplicates2, nums, expected);
     }
 
     @Test
     public void test1() {
+        // test(new int[] {}, new int[] {});
+        test(new int[] {1}, new int[] {1});
         test(new int[] {1, 1}, new int[] {1});
         test(new int[] {1, 1, 2}, new int[] {1, 2});
         test(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9},
