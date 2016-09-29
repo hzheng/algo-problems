4d6b626e7b47663c43bf9c50f57b0e1e14b8c8d4
Hui Zheng <xyzdll@gmail.com>
2016-09-30 01:14:36 +0800

    add 1 more solution for RestoreIp

--- leetcode/recursion_DP/RestoreIp.java
+++ leetcode/recursion_DP/RestoreIp.java
@@ -4,13 +4,16 @@ import java.util.function.Function;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+// LC093: https://leetcode.com/problems/restore-ip-addresses/
+//
 // Given a string containing only digits, restore it by returning all possible
 // valid IP address combinations.
 public class RestoreIp {
-    // beats 54.78%
+    // Backtracking
+    // beats 58.01%(4 ms)
     public List<String> restoreIpAddresses(String s) {
         List<String> res = new ArrayList<>();
-        restoreIp(s, new ArrayList<String>(), res);
+        restoreIp(s, new ArrayList<>(4), res);
         return res;
     }
 
@@ -23,7 +26,6 @@ public class RestoreIp {
             }
             return;
         }
-
         if (len == 0) return;
 
         // try one digit
@@ -32,36 +34,34 @@ public class RestoreIp {
         restoreIp(s.substring(1), cur, res);
         cur.remove(size);
 
-        // try two digits
         if (first == '0' || len == 1) return;
-
+        // try two digits
         cur.add(s.substring(0, 2));
         restoreIp(s.substring(2), cur, res);
         cur.remove(size);
 
-        // try three digits
         if (len == 2) return;
-
+        // try three digits
         char second = s.charAt(1);
         if (first > '2' || first == '2'
             && (second > '5' || second == '5' && s.charAt(2) > '5')) {
             return;
         }
-
         cur.add(s.substring(0, 3));
         restoreIp(s.substring(3), cur, res);
         cur.remove(size);
     }
 
-    // beats 21.91%
+    // Solution of Choice
+    // Backtracking
+    // beats 36.02%(5 ms)
     public List<String> restoreIpAddresses2(String s) {
         List<String> res = new ArrayList<>();
-        restoreIp2(s, new ArrayList<String>(), res);
+        restoreIp2(s, new String[4], 0, res);
         return res;
     }
 
-    private void restoreIp2(String s, List<String> cur, List<String> res) {
-        int size = cur.size();
+    private void restoreIp2(String s, String[] cur, int size, List<String> res) {
         int len = s.length();
         if (size == 4) {
             if (len == 0) {
@@ -69,23 +69,55 @@ public class RestoreIp {
             }
             return;
         }
-
         for (int i = 1; i < 4 && i <= len; i++) {
             String segment = s.substring(0, i);
-            if (!isValid(segment)) continue;
-
-            cur.add(segment);
-            restoreIp2(s.substring(i), cur, res);
-            cur.remove(size);
+            if ((i == 1 || segment.charAt(0) != '0')
+                && (i < 3 || Integer.parseInt(segment) < 256)) {
+                cur[size] = segment;
+                restoreIp2(s.substring(i), cur, size + 1, res);
+            }
         }
     }
 
-    private boolean isValid(String s) {
-        if (s.charAt(0) == '0') {
-            return s.length() == 1;
-        }
+    // Solution of Choice
+    // Iteration
+    // beats 85.59%(3 ms)
+    public List<String> restoreIpAddresses3(String s) {
+        List<String> res = new ArrayList<>();
+        int len = s.length();
+        char[] chars = s.toCharArray();
+        for (int i = 1; i < 4 && i < len - 2; i++) {
+            if (len - i > 9) continue;
 
-        return Integer.parseInt(s) < 256;
+            for (int j = i + 1; j < i + 4 && j < len - 1; j++) {
+                if (len - j > 6) continue;
+
+                for (int k = j + 1; k < j + 4 && k < len; k++) {
+                    String s1 = validate(chars, 0, i);
+                    if (s1 == null) continue;
+
+                    String s2 = validate(chars, i, j);
+                    if (s2 == null) continue;
+
+                    String s3 = validate(chars, j, k);
+                    if (s3 == null) continue;
+
+                    String s4 = validate(chars, k, len);
+                    if (s4 == null) continue;
+
+                    res.add(s1 + "." + s2 + "." + s3 + "." + s4);
+                }
+            }
+        }
+        return res;
+    }
+
+    private String validate(char[] chars, int start, int end) {
+        int len = end - start;
+        if (len > 3 || len != 1 && chars[start] == '0') return null;
+
+        String s = new String(chars, start, len);
+        return Integer.parseInt(s) < 256 ? s : null;
     }
 
     void test(Function<String, List<String> > restore,
@@ -106,6 +138,7 @@ public class RestoreIp {
         RestoreIp r = new RestoreIp();
         test(r::restoreIpAddresses, s, expected);
         test(r::restoreIpAddresses2, s, expected);
+        test(r::restoreIpAddresses3, s, expected);
     }
 
     @Test
