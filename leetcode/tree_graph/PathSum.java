c1b23467001f44866e8ded29ff9a52ef4bc4e4b2
Hui Zheng <xyzdll@gmail.com>
2016-10-04 01:51:46 +0800

    add 1 more solution for PathSum

--- leetcode/tree_graph/PathSum.java
+++ leetcode/tree_graph/PathSum.java
@@ -5,10 +5,13 @@ import static org.junit.Assert.*;
 
 import common.TreeNode;
 
+// LC112: https://leetcode.com/problems/path-sum/
+//
 // Given a binary tree and a sum, determine if the tree has a root-to-leaf path
 // such that adding up all the values along the path equals the given sum.
 public class PathSum {
-    // beats 9.06%
+    // DFS + Recursion
+    // beats 14.31%(1 ms)
     public boolean hasPathSum(TreeNode root, int sum) {
         if (root == null) return false;
 
@@ -18,8 +21,8 @@ public class PathSum {
         return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
     }
 
-    // http://www.programcreek.com/2013/01/leetcode-path-sum/
-    // beats 1.64%
+    // BFS + Queue
+    // beats 7.44%(3 ms)
     public boolean hasPathSum2(TreeNode root, int sum) {
         if (root == null) return false;
 
@@ -27,32 +30,55 @@ public class PathSum {
         Queue<Integer> values = new LinkedList<>();
         nodes.add(root);
         values.add(root.val);
-
-        while (!nodes.isEmpty()){
+        while (!nodes.isEmpty()) {
             TreeNode cur = nodes.poll();
             int val = values.poll();
-            if (cur.left == null && cur.right == null && val == sum) {
-                return true;
-            }
+            if (cur.left == null && cur.right == null && val == sum) return true;
 
             if (cur.left != null) {
-                nodes.add(cur.left);
-                values.add(val + cur.left.val);
+                nodes.offer(cur.left);
+                values.offer(val + cur.left.val);
             }
-            if (cur.right != null){
-                nodes.add(cur.right);
-                values.add(val + cur.right.val);
+            if (cur.right != null) {
+                nodes.offer(cur.right);
+                values.offer(val + cur.right.val);
             }
         }
         return false;
     }
 
-    // TODO: DFS
+    // Stack(Postorder traversal)
+    // beats 3.83%(5 ms)
+    public boolean hasPathSum3(TreeNode root, int sum) {
+        Stack<TreeNode> stack = new Stack<>();
+        int curSum = 0;
+        for (TreeNode cur = root, prev = null; cur != null || !stack.isEmpty(); ) {
+            if (cur != null) {
+                stack.push(cur);
+                curSum += cur.val;
+                cur = cur.left;
+            } else {
+                cur = stack.peek();
+                if (cur.left == null && cur.right == null && curSum == sum) return true;
+
+                if (cur.right != null && prev != cur.right) {
+                    cur = cur.right;
+                } else {
+                    curSum -= cur.val;
+                    prev = cur;
+                    cur = null;
+                    stack.pop();
+                }
+            }
+        }
+        return false;
+    }
 
     void test(String s, int sum, boolean expected) {
         TreeNode root = TreeNode.of(s);
         assertEquals(expected, hasPathSum(root, sum));
         assertEquals(expected, hasPathSum2(root, sum));
+        assertEquals(expected, hasPathSum3(root, sum));
     }
 
     @Test
