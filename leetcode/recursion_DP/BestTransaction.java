91e08c2a0d253d19f2b4708d6f56083ec50f3194
Hui Zheng <xyzdll@gmail.com>
2016-10-05 21:48:25 +0800

    add 1 more solution for BestTransaction

--- leetcode/recursion_DP/BestTransaction.java
+++ leetcode/recursion_DP/BestTransaction.java
@@ -4,11 +4,13 @@ import java.util.function.Function;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+// LC121: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+//
 // Say you have an array for which the ith element is the price of a given stock
 // on day i. If you were only permitted to complete at most one transaction,
 // design an algorithm to find the maximum profit.
 public class BestTransaction {
-    // beats 51.18%
+    // beats 51.18%(2 ms)
     public int maxProfit(int[] prices) {
         if (prices.length == 0) return 0;
 
@@ -28,15 +30,41 @@ public class BestTransaction {
         return profit;
     }
 
-    // beats 94.51%
+    // beats 85.40%(1 ms)
     public int maxProfit2(int[] prices) {
         int min = Integer.MAX_VALUE;
         int profit = 0;
         for (int price : prices) {
-            min = price < min ? price : min;
+            min = Math.min(min, price);
             profit = Math.max(profit, price - min);
         }
         return profit;
+    }
+
+    // beats 85.40%(1 ms)
+    public int maxProfit3(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int profit = 0;
+        for (int price : prices) {
+            if (price < min) {
+                min = price;
+            } else if (price > min + profit) {
+                profit = price - min;
+            }
+        }
+        return profit;
+    }
+
+    // Kadane's algorithm
+    // beats 10.50%(3 ms)
+    public int maxProfit4(int[] prices) {
+        int max = 0; // global max
+        for (int i = 1, localMax = 0; i < prices.length; i++) {
+            localMax += prices[i] - prices[i - 1];
+            localMax = Math.max(0, localMax);
+            max = Math.max(localMax, max);
+        }
+        return max;
     }
 
     void test(Function<int[], Integer> maxProfit, int expected, int ... prices) {
@@ -47,6 +75,8 @@ public class BestTransaction {
         BestTransaction b = new BestTransaction();
         test(b::maxProfit, expected, prices);
         test(b::maxProfit2, expected, prices);
+        test(b::maxProfit3, expected, prices);
+        test(b::maxProfit4, expected, prices);
     }
 
     @Test
