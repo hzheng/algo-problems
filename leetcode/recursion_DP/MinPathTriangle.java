82aa3f5186cb7a8b224bea89080ad75b257ffc02
Hui Zheng <xyzdll@gmail.com>
2016-10-05 18:06:09 +0800

    improve MinPathTriangle

--- leetcode/recursion_DP/MinPathTriangle.java
+++ leetcode/recursion_DP/MinPathTriangle.java
@@ -4,26 +4,21 @@ import java.util.function.Function;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-// https://leetcode.com/problems/triangle/
+// LC120: https://leetcode.com/problems/triangle/
+//
 // Given a triangle, find the minimum path sum from top to bottom. Each step you
 // may move to adjacent numbers on the row below.
 // Bonus point if you are able to do this using only O(n) extra space, where n
 // is the total number of rows in the triangle.
 public class MinPathTriangle {
-    // beats 91.91%
+    // beats 87.33%(3 ms)
     public int minimumTotal(List<List<Integer> > triangle) {
-        int rowIndex = triangle.size() - 1;
-        if (rowIndex < 0) return 0;
-
+        int size = triangle.size();
         // beat rate drops dramatically to 0.19% if use stream
         // int[] sum = triangle.get(rowIndex).stream().mapToInt(i -> i).toArray();
-        int[] sum = new int[rowIndex + 1];
-        List<Integer> row = triangle.get(rowIndex);
-        for (int i = row.size() - 1; i >= 0; i--) {
-            sum[i] = row.get(i);
-        }
-        while (--rowIndex >= 0) {
-            row = triangle.get(rowIndex);
+        int[] sum = new int[size + 1];
+        for (int rowIndex = size - 1; rowIndex >= 0; rowIndex--) { // bottom-up
+            List<Integer> row = triangle.get(rowIndex);
             for (int i = 0; i <= rowIndex; i++) {
                 sum[i] = row.get(i) + Math.min(sum[i], sum[i + 1]);
             }
