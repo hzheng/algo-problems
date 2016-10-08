30804331fab6b19f9da3557b9cd174ebc47d749c
Hui Zheng <xyzdll@gmail.com>
2016-10-08 23:30:56 +0800

    add 1 more solution for WordBreak2

--- leetcode/recursion_DP/WordBreak2.java
+++ leetcode/recursion_DP/WordBreak2.java
@@ -4,13 +4,15 @@ import java.util.*;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-// https://leetcode.com/problems/word-break-ii/
+// LC140: https://leetcode.com/problems/word-break-ii/
 //
 // Given a string s and a dictionary of words dict, add spaces in s to construct
 // a sentence where each word is a valid dictionary word.
 // Return all such possible sentences.
 public class WordBreak2 {
-    // beats 90.65%
+    // Solution of Choice
+    // Backtracking
+    // beats 94.54%(7 ms)
     public List<String> wordBreak(String s, Set<String> wordDict) {
         List<String> res = new LinkedList<>();
         // will TLE if not check this
@@ -28,21 +30,22 @@ public class WordBreak2 {
             return;
         }
 
-        for (int i = start + 1; i <= len; i++) {
+        for (int i = start + 1, size = sb.length(); i <= len; i++) {
             String segment = s.substring(start, i);
-            int l = sb.length();
             if (wordDict.contains(segment)) {
-                if (l > 0) {
+                if (size > 0) {
                     sb.append(" ");
                 }
                 sb.append(segment);
                 wordBreak(s, wordDict, i, sb, res);
-                sb.delete(l, sb.length());
+                // sb.delete(size, sb.length());
+                sb.setLength(size);
             }
         }
     }
 
-    // beats 83.10%
+    // Backtracking + Memoization
+    // beats 91.38%(9 ms)
     public List<String> wordBreak2(String s, Set<String> wordDict) {
         List<String> res = new LinkedList<>();
         if (!canBreak(s, wordDict)) return res;
@@ -53,7 +56,7 @@ public class WordBreak2 {
 
     private void wordBreak2(String s, Set<String> wordDict, int start,
                             StringBuilder sb, List<String> res,
-                            Map<Integer, List<String>> cache) {
+                            Map<Integer, List<String> > cache) {
         int len = s.length();
         if (start == len) {
             res.add(sb.toString());
@@ -74,14 +77,15 @@ public class WordBreak2 {
             cache.put(start, candidates);
         }
 
-        int l = sb.length();
+        int size = sb.length();
         for (String candidate : candidates) {
-            if (l > 0) {
+            if (size > 0) {
                 sb.append(" ");
             }
             sb.append(candidate);
             wordBreak2(s, wordDict, start + candidate.length(), sb, res, cache);
-            sb.delete(l, sb.length());
+            // sb.delete(size, sb.length());
+            sb.setLength(size);
         }
     }
 
@@ -89,7 +93,6 @@ public class WordBreak2 {
         int len = s.length();
         boolean[] dp = new boolean[len + 1];
         dp[0] = true;
-
         for (int i = 0; i < len; i++) {
             for (int j = i; j >= 0; j--) {
                 if (dp[j] && wordDict.contains(s.substring(j, i + 1))) {
@@ -101,13 +104,13 @@ public class WordBreak2 {
         return dp[len];
     }
 
-    // beats 21.47%
+    // Dynamic Programming
+    // beats 21.47%(19 ms)
     public List<String> wordBreak3(String s, Set<String> wordDict) {
         int len = s.length();
         @SuppressWarnings("unchecked")
         List<String>[] dp = (List<String>[])Array.newInstance(List.class, len + 1);
         dp[0] = Collections.emptyList();
-
         for (int i = 0; i < len; i++) {
             List<String> words = null;
             for (int j = i; j >= 0; j--) {
@@ -144,6 +147,32 @@ public class WordBreak2 {
         }
     }
 
+    // Recursion + Memoization
+    // beats 19.62%(20 ms)
+    public List<String> wordBreak4(String s, Set<String> wordDict) {
+        return wordBreak4(s, wordDict, new HashMap<>());
+    }
+
+    private List<String> wordBreak4(String s, Set<String> wordDict,
+                                    Map<String, List<String> > map) {
+        if (map.containsKey(s)) return map.get(s);
+
+        List<String> res = new ArrayList<>();
+        for (int i = 1, len = s.length(); i <= len; i++) {
+            String left = s.substring(0, i);
+            if (wordDict.contains(left)) {
+                for (String word : wordBreak4(s.substring(i), wordDict, map)) {
+                    res.add(left + " " + word);
+                }
+                if (i == len) {
+                    res.add(left);
+                }
+            }
+        }
+        map.put(s, res);
+        return res;
+    }
+
     @FunctionalInterface
     interface Function<A, B, C> {
         public C apply(A a, B b);
@@ -166,13 +195,14 @@ public class WordBreak2 {
         test(w::wordBreak, "wordBreak", s, dict, expected);
         test(w::wordBreak2, "wordBreak2", s, dict, expected);
         test(w::wordBreak3, "wordBreak3", s, dict, expected);
+        test(w::wordBreak4, "wordBreak4", s, dict, expected);
     }
 
     @Test
     public void test1() {
         test("catsanddog", new String[] {"cat", "cats", "and", "sand", "dog"},
              "cats and dog", "cat sand dog");
-        test("aaaaaaa", new String[]{"aaaa", "aaa"}, "aaaa aaa", "aaa aaaa");
+        test("aaaaaaa", new String[] {"aaaa", "aaa"}, "aaaa aaa", "aaa aaaa");
         // test("aaaaaaaaaaaaaaaaaaaaaaa",
         //      new String[] {"a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa",
         //                    "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa"});
