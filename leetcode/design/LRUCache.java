6f9b37e61c3d69efc8779c79f1c0f43c7c7ab982
Hui Zheng <xyzdll@gmail.com>
2016-10-11 12:54:10 +0800

    add 1 more solution for LRUCache

--- leetcode/design/LRUCache.java
+++ leetcode/design/LRUCache.java
@@ -71,7 +71,9 @@ class LRUCache0 {
     }
 }
 
-// beats 70.33%
+// Solution of Choice
+// LinkedList + Hashtable
+// beats 59.41%(18 ms for 17 tests)
 public class LRUCache {
     static class Node {
         int key;
@@ -165,5 +167,27 @@ public class LRUCache {
         // org.junit.runner.JUnitCore.main("LRUCache");
         test1();
         test2();
+    }
+}
+
+// LinkedHashMap
+// beats 90.87%(15 ms for 17 tests)
+class LRUCache2 {
+    private LinkedHashMap<Integer, Integer> map;
+
+    public LRUCache2(int capacity) {
+        map = new LinkedHashMap<Integer, Integer>(capacity, 0.75f, true) {
+            protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+                return size() > capacity;
+            }
+        };
+    }
+
+    public int get(int key) {
+        return map.getOrDefault(key, -1);
+    }
+
+    public void set(int key, int value) {
+        map.put(key, value);
     }
 }
