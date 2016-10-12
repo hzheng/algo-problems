43ffdaf12cc3cd80251c7990df6e1761ad0a36ba
Hui Zheng <xyzdll@gmail.com>
2016-10-13 00:13:37 +0800

    add 2 more solutions for VersionComparison

--- leetcode/array_string/VersionComparison.java
+++ leetcode/array_string/VersionComparison.java
@@ -3,16 +3,16 @@ import java.util.*;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-// https://leetcode.com/problems/compare-version-numbers/
+// LC165: https://leetcode.com/problems/compare-version-numbers/
 //
 // Compare two version numbers version1 and version2.
 public class VersionComparison {
-    // beats 45.72%
+    // beats 45.72%(3 ms)
     public int compareVersion(String version1, String version2) {
         String[] v1 = version1.split("\\.");
         String[] v2 = version2.split("\\.");
         int i = 0;
-        for ( ; i < v1.length && i < v2.length; i++) {
+        for (; i < v1.length && i < v2.length; i++) {
             int n1 = Integer.parseInt(v1[i]);
             int n2 = Integer.parseInt(v2[i]);
             if (n1 > n2) return 1;
@@ -36,8 +36,43 @@ public class VersionComparison {
         return true;
     }
 
+    // beats 12.31%(4 ms for 71 tests)
+    public int compareVersion2(String version1, String version2) {
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+        for (int i = 0; i < v1.length || i < v2.length; i++) {
+            int num1 = i < v1.length ? Integer.parseInt(v1[i]) : 0;
+            int num2 = i < v2.length ? Integer.parseInt(v2[i]) : 0;
+            if (num1 < num2) return -1;
+            if (num1 > num2) return 1;
+        }
+        return 0;
+    }
+
+    // Solution of Choice
+    // beats 91.59%(0 ms)
+    public int compareVersion3(String version1, String version2) {
+        int len1 = version1.length();
+        int len2 = version2.length();
+        for (int i = 0, j = 0; i < len1 || j < len2; i++, j++) {
+            int num1 = 0;
+            while (i < len1 && version1.charAt(i) != '.') {
+                num1 = num1 * 10 + version1.charAt(i++) - '0';
+            }
+            int num2 = 0;
+            while (j < len2 && version2.charAt(j) != '.') {
+                num2 = num2 * 10 + version2.charAt(j++) - '0';
+            }
+            if (num1 < num2) return -1;
+            if (num1 > num2) return 1;
+        }
+        return 0;
+    }
+
     void test(int expected, String v1, String v2) {
         assertEquals(expected, compareVersion(v1, v2));
+        assertEquals(expected, compareVersion2(v1, v2));
+        assertEquals(expected, compareVersion3(v1, v2));
     }
 
     @Test
