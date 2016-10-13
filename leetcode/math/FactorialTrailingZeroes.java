3751cd58932aa2c0e6110d6b762d170dc3dea015
Hui Zheng <xyzdll@gmail.com>
2016-10-13 12:26:46 +0800

    add 1 more solution for FactorialTrailingZeroes

--- leetcode/math/FactorialTrailingZeroes.java
+++ leetcode/math/FactorialTrailingZeroes.java
@@ -3,11 +3,12 @@ import java.util.*;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-// https://leetcode.com/problems/factorial-trailing-zeroes/
+// LC172: https://leetcode.com/problems/factorial-trailing-zeroes/
 //
 // Given an integer n, return the number of trailing zeroes in n!
 public class FactorialTrailingZeroes {
-    // beats 74.44%
+    // Solution of Choice
+    // beats 5.71%(2 ms for 502 tests)
     public int trailingZeroes(int n) {
         int factorFives = 0;
         // for (int i = 5; i <= n; i *= 5) { // may overflow
@@ -19,8 +20,15 @@ public class FactorialTrailingZeroes {
         return factorFives;
     }
 
+    // Recursion
+    // beats 5.71%(2 ms for 502 tests)
+    public int trailingZeroes2(int n) {
+        return n == 0 ? 0 : n / 5 + trailingZeroes2(n / 5);
+    }
+
     void test(int x, int expected) {
         assertEquals(expected, trailingZeroes(x));
+        assertEquals(expected, trailingZeroes2(x));
     }
 
     @Test
