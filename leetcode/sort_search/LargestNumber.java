0b277bb61e3e7238a7c4e4d7f9b6fa461f5e6793
Hui Zheng <xyzdll@gmail.com>
2016-10-14 12:03:21 +0800

    add 2 more solutions LargestNumber

--- leetcode/sort_search/LargestNumber.java
+++ leetcode/sort_search/LargestNumber.java
@@ -3,7 +3,7 @@ import java.util.*;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-// https://leetcode.com/problems/largest-number/
+// LC179: https://leetcode.com/problems/largest-number/
 //
 // Given a list of non negative integers, arrange them such that they form the
 // largest number.
@@ -12,7 +12,7 @@ public class LargestNumber {
     public String largestNumber(int[] nums) {
         StringBuilder sb = new StringBuilder();
         Arrays.stream(nums).boxed().sorted(new NumComparator())
-                    .forEach(num -> sb.append(num));
+        .forEach(num -> sb.append(num));
         while (sb.length() > 1 && sb.charAt(0) == '0') {
             sb.deleteCharAt(0);
         }
@@ -48,7 +48,7 @@ public class LargestNumber {
     public String largestNumber2(int[] nums) {
         StringBuilder sb = new StringBuilder();
         Arrays.stream(nums).boxed().sorted(new NumComparator2())
-                    .forEach(num -> sb.append(num));
+        .forEach(num -> sb.append(num));
         while (sb.length() > 1 && sb.charAt(0) == '0') {
             sb.deleteCharAt(0);
         }
@@ -63,9 +63,39 @@ public class LargestNumber {
         }
     }
 
+    // Solution of Choice
+    // beas 43.21%(126 ms for 221 tests)
+    public String largestNumber3(int[] nums) {
+        String[] strs = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            strs[i] = String.valueOf(nums[i]);
+        }
+        Arrays.sort(strs, new Comparator<String>() {
+            public int compare(String s1, String s2) {
+                return (s2 + s1).compareTo(s1 + s2);
+            }
+        });
+        if (strs[0].charAt(0) == '0') return "0";
+
+        StringBuilder sb = new StringBuilder();
+        for (String s: strs) {
+            sb.append(s);
+        }
+        return sb.toString();
+    }
+
+    // beats 8.83%(168 ms for 221 tests)
+    public String largestNumber4(int[] nums) {
+        String[] strs = Arrays.stream(nums).mapToObj(String::valueOf).toArray(String[]::new);
+        Arrays.sort(strs, (s1, s2) -> (s2 + s1).compareTo(s1 + s2));
+        return Arrays.stream(strs).reduce((x, y) -> x.equals("0") ? y : x + y).get();
+    }
+
     void test(String expected, int ... nums) {
         assertEquals(expected, largestNumber(nums));
         assertEquals(expected, largestNumber2(nums));
+        assertEquals(expected, largestNumber3(nums));
+        assertEquals(expected, largestNumber4(nums));
     }
 
     @Test
