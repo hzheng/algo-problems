fb662f7c011c727d154770334d5b1bcf47ef0b34
Hui Zheng <xyzdll@gmail.com>
2016-10-15 21:32:16 +0800

    add 2 more solutions for HappyNumber

--- leetcode/math/HappyNumber.java
+++ leetcode/math/HappyNumber.java
@@ -3,7 +3,7 @@ import java.util.*;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-// https://leetcode.com/problems/happy-number/
+// LC202: https://leetcode.com/problems/happy-number/
 //
 // Write an algorithm to determine if a number is "happy".
 // A happy number is a number defined by the following process: Starting with
@@ -12,13 +12,13 @@ import static org.junit.Assert.*;
 // or it loops endlessly in a cycle which does not include 1. Those numbers for
 // which this process ends in 1 are happy numbers.
 public class HappyNumber {
-    // beats 31.23%(6 ms)
+    // Solution of Choice
+    // Hash Table
+    // beats 68.29%(4 ms for 401 tests)
     public boolean isHappy(int n) {
         Set<Integer> calculated = new HashSet<>();
         for (int i = n; i != 1; i = calculate(i)) {
-            if (calculated.contains(i)) return false;
-
-            calculated.add(i);
+            if (!calculated.add(i)) return false;
         }
         return true;
     }
@@ -32,8 +32,31 @@ public class HappyNumber {
         return res;
     }
 
+    // Floyd Cycle detection algorithm
+    // beats 86.99%(2 ms for 401 tests)
+    public boolean isHappy2(int n) {
+        int slow = n;
+        int fast = n;
+        do {
+            slow = calculate(slow);
+            fast = calculate(calculate(fast));
+        } while (slow != fast);
+        return slow == 1;
+    }
+
+    // https://discuss.leetcode.com/topic/12710/my-java-solution-find-1-or-7-when-happy-sum-is-a-single-digit
+    // beats 96.03%(1 ms for 401 tests)
+    public boolean isHappy3(int n) {
+        for (int sum = n; ; ) {
+            sum = calculate(sum);
+            if (sum < 10) return sum == 1 || sum == 7;
+        }
+    }
+
     void test(int n, boolean expected) {
         assertEquals(expected, isHappy(n));
+        assertEquals(expected, isHappy2(n));
+        assertEquals(expected, isHappy3(n));
     }
 
     @Test
