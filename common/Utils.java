11d59893f00fbaebe8cd29b8e32c1157cd3a0b30
Hui Zheng <xyzdll@gmail.com>
2016-10-16 21:22:05 +0800

    add methods for Utils

--- common/Utils.java
+++ common/Utils.java
@@ -13,6 +13,24 @@ public class Utils {
         }
     }
 
+    public static class StrListComparator implements Comparator<List<String>> {
+        public int compare(List<String> a, List<String> b) {
+            int len = Math.min(a.size(), b.size());
+            int i = 0;
+            for (; i < len && a.get(i).equals(b.get(i)); i++);
+            return (i == len) ? a.size() - b.size() : a.get(i).compareTo(b.get(i));
+        }
+    }
+
+    public static class StrArrayComparator implements Comparator<String[]> {
+        public int compare(String[] a, String[] b) {
+            int len = Math.min(a.length, b.length);
+            int i = 0;
+            for (; i < len && a[i].equals(b[i]); i++);
+            return (i == len) ? a.length - b.length : a[i].compareTo(b[i]);
+        }
+    }
+
     public static String[][] toStrArray(List<List<String> > listOfList) {
         return toArray(listOfList, String.class);
     }
