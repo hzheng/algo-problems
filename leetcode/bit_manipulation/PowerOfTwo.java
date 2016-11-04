67fe5940a5bce4f02f02bbb49a8e069aff21f779
Hui Zheng <xyzdll@gmail.com>
2016-11-04 13:23:20 +0800

    add 1 more solution for PowerOfTwo

--- leetcode/bit_manipulation/PowerOfTwo.java
+++ leetcode/bit_manipulation/PowerOfTwo.java
@@ -3,10 +3,11 @@ import java.util.*;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-// https://leetcode.com/problems/power-of-two/
+// LC231: https://leetcode.com/problems/power-of-two/
 //
 // Given an integer, write a function to determine if it is a power of two.
 public class PowerOfTwo {
+    // Solution of Choice
     // beats 20.40%(2 ms)
     public boolean isPowerOfTwo(int n) {
         return n > 0 && (n & (n - 1)) == 0;
@@ -22,9 +23,15 @@ public class PowerOfTwo {
         return n == 1;
     }
 
+    // beats 21.02%(2 ms for 1108 tests)
+    public boolean isPowerOfTwo3(int n) {
+        return n > 0 && ((1 << 30) % n == 0);
+    }
+
     void test(int n, boolean expected) {
         assertEquals(expected, isPowerOfTwo(n));
         assertEquals(expected, isPowerOfTwo2(n));
+        assertEquals(expected, isPowerOfTwo3(n));
     }
 
     @Test
