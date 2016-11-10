a54e0a0ed41fd81e1f60242cf3bee893ec93db32
Hui Zheng <xyzdll@gmail.com>
2016-11-10 22:21:38 +0800

    add 3 more solutions for PerfectSquares

--- leetcode/math/PerfectSquares.java
+++ leetcode/math/PerfectSquares.java
@@ -4,20 +4,19 @@ import java.util.function.Function;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-// https://leetcode.com/problems/perfect-squares/
+// LC279: https://leetcode.com/problems/perfect-squares/
 //
 // Given a positive integer n, find the least number of perfect square numbers
-//  which sum to n.
+// which sum to n.
 public class PerfectSquares {
+    // Dynamic Programming(Top-Down)
     // StackOverFlowError when n is big
     public int numSquares(int n) {
         return numSquares(n, new HashMap<>());
     }
 
     private int numSquares(int n, Map<Integer, Integer> cache) {
-        if (cache.containsKey(n)) {
-            return cache.get(n);
-        }
+        if (cache.containsKey(n)) return cache.get(n);
 
         int min = n;
         for (int i = 1;; i++) {
@@ -34,6 +33,7 @@ public class PerfectSquares {
         return min;
     }
 
+    // Dynamic Programming(Bottom-Up)
     // time complexity: O(N ^ (1.5)), space complexity: O(N)
     // beats 60.85%(68 ms)
     public int numSquares2(int n) {
@@ -51,11 +51,10 @@ public class PerfectSquares {
         return table[n];
     }
 
-    // Lagrange's four-square theorem
+    // Dynamic Programming(Bottom-Up)
     // time complexity: O(N ^ (1.5)), space complexity: O(N)
     // beats 71.36%(66 ms)
     public int numSquares3(int n) {
-        int[] table = new int[n + 1];
         int[] squares = new int[n > 10 ? n / 2 : n + 1];
         for (int i = 1;; i++) {
             int square = i * i;
@@ -64,7 +63,7 @@ public class PerfectSquares {
             squares[i] = square;
             if (square > n) break;
         }
-
+        int[] table = new int[n + 1];
         for (int i = 1; i <= n; i++) {
             int min = n;
             for (int j = 1;; j++) {
@@ -78,7 +77,7 @@ public class PerfectSquares {
         return table[n];
     }
 
-    // BFS
+    // BFS + Queue
     // Memory Limit Exceeded
     public int numSquares4(int n) {
         int[] squares = new int[n > 10 ? n / 2 : n + 1];
@@ -89,17 +88,11 @@ public class PerfectSquares {
             squares[i] = square;
             if (square > n) break;
         }
-
         Queue<Integer> queue = new LinkedList<>();
         add(queue, n, squares);
-
         for (int level = 2;; level++) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                int cur = queue.poll();
-                if (add(queue, cur, squares)) {
-                    return level;
-                }
+            for (int i = queue.size(); i > 0; i--) {
+                if (add(queue, queue.poll(), squares)) return level;
             }
         }
     }
@@ -114,8 +107,9 @@ public class PerfectSquares {
         }
     }
 
-    // BFS with cache
-    // beats 13.38%(146 ms)
+    // BFS + Queue + Set
+    // time complexity: O(N ^ (1.5)), space complexity: O(N)
+    // beats 11.30%(193 ms for 600 tests)
     public int numSquares5(int n) {
         int[] squares = new int[n > 10 ? n / 2 : n + 1];
         for (int i = 1;; i++) {
@@ -129,14 +123,9 @@ public class PerfectSquares {
         Queue<Integer> queue = new LinkedList<>();
         Set<Integer> added = new HashSet<>();
         add(queue, n, squares, added);
-
         for (int level = 2;; level++) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                int cur = queue.poll();
-                if (add(queue, cur, squares, added)) {
-                    return level;
-                }
+            for (int i = queue.size(); i > 0; i--) {
+                if (add(queue, queue.poll(), squares, added)) return level;
             }
         }
     }
@@ -155,7 +144,59 @@ public class PerfectSquares {
         }
     }
 
-    // DP
+    // Solution of Choice
+    // BFS + Queue + Set
+    // time complexity: O(N ^ (1.5)), space complexity: O(N)
+    // beats 15.98%(136 ms for 600 tests)
+    public int numSquares5_2(int n) {
+        Queue<Integer> queue = new LinkedList<>();
+        Set<Integer> visited = new HashSet<>();
+        queue.offer(0);
+        visited.add(0);
+        for (int level = 1; ; level++) {
+            for (int i = queue.size(); i > 0; i--) {
+                int k = queue.poll();
+                for (int j = 1; j * j <= n; j++) {
+                    int composite = k + j * j;
+                    if (composite == n) return level;
+                    if (composite > n) break;
+                    if (!visited.contains(composite)) {
+                        queue.offer(composite);
+                        visited.add(composite);
+                    }
+                }
+            }
+        }
+    }
+
+    // BFS + Queue + Set
+    // time complexity: O(N ^ (1.5)), space complexity: O(N)
+    // beats 15.98%(136 ms for 600 tests)
+    public int numSquares5_3(int n) {
+        int[] squares = new int[(int)Math.sqrt(n)];
+        Set<Integer> visited = new HashSet<>();
+        for (int i = 1; i <= squares.length; i++) {
+            squares[i - 1] = i * i;
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(n);
+        for (int level = 1; ; level++) {
+            for (int i = queue.size(); i > 0; i--) {
+                int cur = queue.poll();
+                for (int j : squares) {
+                    if (cur == j) return level;
+                    if (cur < j) break;
+                    if (!visited.contains(cur - j)) {
+                        queue.offer(cur - j);
+                        visited.add(cur - j);
+                    }
+                }
+            }
+        }
+    }
+
+    // Dynamic Programming(Bottom-Up)
+    // time complexity: O(N ^ (1.5)), space complexity: O(N)
     // beats 60.85%(68 ms)
     public int numSquares6(int n) {
         int[] table = new int[n + 1];
@@ -174,35 +215,50 @@ public class PerfectSquares {
         return table[n];
     }
 
+    // Solution of Choice
+    // Dynamic Programming(Bottom-Up)
+    // time complexity: O(N ^ (1.5)), space complexity: O(N)
+    // beats 44.68%(85 ms for 600 tests)
+    public int numSquares6_2(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, n);
+        dp[0] = 0;
+        for (int i = 0; i <= n; i++) {
+            for (int j = 1;; j++) {
+                int composite = i + j * j;
+                if (composite > n) break;
+
+                dp[composite] = Math.min(dp[i] + 1, dp[composite]);
+            }
+        }
+        return dp[n];
+    }
+
+    // Solution of Choice
     // Lagrange's four-square theorem & Legendre's three-square theorem
     // time complexity: O(N ^ (0.5)), space complexity: O(1)
     // beats 97.12%(2 ms)
     public int numSquares7(int n) {
-        while (n % 4 == 0) {
-            n /= 4;
-        }
-
-        if (n % 8 == 7) return 4;
+        for (; (n & 3) == 0; n >>= 2) {}
+        if ((n & 7) == 7) return 4;
 
         for (int i = 0; i * i <= n; i++) {
             int j = (int)Math.sqrt(n - i * i);
-            if (i * i + j * j == n) {
-                return (i > 0 ? 1 : 0) + (j > 0 ? 1 : 0);
-            }
+            if (i * i + j * j == n) return (i > 0 ? 1 : 0) + (j > 0 ? 1 : 0);
         }
         return 3;
     }
 
+    // Lagrange's four-square theorem & Legendre's three-square theorem
+    // time complexity: O(N ^ (0.5)), space complexity: O(1)
     // beats 97.12%(2 ms)
     public int numSquares8(int n) {
         if (isSquare(n)) return 1;
 
-        while ((n & 3) == 0) {
-            n >>= 2;
-        }
+        for (; (n & 3) == 0; n >>= 2) {}
         if ((n & 7) == 7) return 4;
 
-        for(int i = 1; i * i <= n; i++) {
+        for (int i = 1; i * i <= n; i++) {
             if (isSquare(n - i * i)) return 2;
         }
         return 3;
@@ -233,7 +289,10 @@ public class PerfectSquares {
             test(p::numSquares4, "numSquares4", n, expected);
         }
         test(p::numSquares5, "numSquares5", n, expected);
+        test(p::numSquares5_2, "numSquares5_2", n, expected);
+        test(p::numSquares5_3, "numSquares5_3", n, expected);
         test(p::numSquares6, "numSquares6", n, expected);
+        test(p::numSquares6_2, "numSquares6_2", n, expected);
         test(p::numSquares7, "numSquares7", n, expected);
         test(p::numSquares8, "numSquares8", n, expected);
     }
