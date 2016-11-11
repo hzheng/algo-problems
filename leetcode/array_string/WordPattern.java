e932c9086c6af2337840f04a69e29c878c0b65e3
Hui Zheng <xyzdll@gmail.com>
2016-11-11 22:56:21 +0800

    minor improvement

--- leetcode/array_string/WordPattern.java
+++ leetcode/array_string/WordPattern.java
@@ -3,13 +3,14 @@ import java.util.*;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-// https://leetcode.com/problems/word-pattern/
+// LC290: https://leetcode.com/problems/word-pattern/
 //
 // Given a pattern and a string str, find if str follows the same pattern.
 // Here follow means a full match, such that there is a bijection between a
 // letter in pattern and a non-empty word in str.
 public class WordPattern {
-    // beats 21.21%(3 ms)
+    // Hash Table
+    // beats 50.38%(2 ms for 29 tests)
     public boolean wordPattern(String pattern, String str) {
         // String[] words = str.split("\\s+"); // very slow
         String[] words = str.split(" ");
@@ -19,19 +20,15 @@ public class WordPattern {
         for (int i = 0; i < words.length; i++) {
             char c = pattern.charAt(i);
             String word = words[i];
-            // if (map.containsValue(word)) { // slower?
-            if (map.values().contains(word)) {
+            if (map.containsValue(word)) { // or (map.values().contains(word))
                 if (!word.equals(map.get(c))) return false;
-            } else {
-                if (map.containsKey(c)) return false;
-
-                map.put(c, word);
-            }
+            } else if (map.put(c, word) != null) return false;
         }
         return true;
     }
 
-    // beats 84.02%(2 ms)
+    // Hash Table + Set
+    // beats 96.73%(1 ms for 29 tests)
     public boolean wordPattern2(String pattern, String str) {
         String[] words = str.split(" ");
         if (pattern.length() != words.length) return false;
@@ -41,7 +38,6 @@ public class WordPattern {
         for (int i = 0; i < words.length; i++) {
             char c = pattern.charAt(i);
             String word = words[i];
-
             if (matchedWord.contains(word)) {
                 if (!word.equals(map[c])) return false;
             } else {
@@ -54,23 +50,25 @@ public class WordPattern {
         return true;
     }
 
-    // beats 21.21%(3 ms)
+    // Solution of Choice
+    // Hash Table
+    // beats 50.38%(2 ms for 29 tests)
     public boolean wordPattern3(String pattern, String str) {
         String[] words = str.split(" ");
         if (pattern.length() != words.length) return false;
 
         Map<Object, Integer> map = new HashMap<>();
         for (int i = 0; i < words.length; i++) {
-            char c = pattern.charAt(i);
-            String word = words[i];
-            if (!Objects.equals(map.put(c, i), map.put(word, i))) return false;
+            if (!Objects.equals(map.put(pattern.charAt(i), i),
+                                map.put(words[i], i))) return false;
             // we cannot use object == instead of equals unless i less than 128
-            // if (map.put(c, i) != map.put(word, i)) return false;
+            // if (map.put(pattern.charAt(i), i) != map.put(words[i], i)) return false;
         }
         return true;
     }
 
-    // beats 21.21%(3 ms)
+    // Hash Table
+    // beats 19.28%(3 ms for 29 tests)
     public boolean wordPattern4(String pattern, String str) {
         String[] words = str.split(" ");
         if (pattern.length() != words.length) return false;
@@ -99,6 +97,8 @@ public class WordPattern {
 
     @Test
     public void test1() {
+        test("aaa", "aa aa aa", true);
+        test("aaa", "aa aa aa aa", false);
         test("abba", "dog cat cat dog", true);
         test("abba", "dog cat cat fish", false);
         test("aaaa", "dog cat cat dog", false);
