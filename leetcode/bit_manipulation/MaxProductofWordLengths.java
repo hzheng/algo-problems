f6ba089bbf35b89a164fb887ad78424397aa3f16
Hui Zheng <xyzdll@gmail.com>
2016-11-19 12:37:53 +0800

    add 1 more solution for MaxProductofWordLengths

--- leetcode/bit_manipulation/MaxProductofWordLengths.java
+++ leetcode/bit_manipulation/MaxProductofWordLengths.java
@@ -3,15 +3,17 @@ import java.util.*;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-// https://leetcode.com/problems/maximum-product-of-word-lengths/
+// LC318: https://leetcode.com/problems/maximum-product-of-word-lengths/
 //
 // Given a string array words, find the maximum value of
 // length(word[i]) * length(word[j]) where the two words do not share common
 // letters. You may assume that each word will contain only lower case letters.
 // If no such two words exist, return 0.
 public class MaxProductofWordLengths {
-    // beats 94.90%(19 ms)
+    // Solution of Choice
+    // Bit Manipulation
     // time complexity: O(N ^ 2), space complexity: O(N)
+    // beats 89.80%(20 ms for 174 tests)
     public int maxProduct(String[] words) {
         int n = words.length;
         int[] codes = new int[n];
@@ -28,7 +30,7 @@ public class MaxProductofWordLengths {
         }
         int max = 0;
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+            for (int j = i + 1; j < n; j++) {
                 if ((codes[i] & codes[j]) == 0) {
                     max = Math.max(max, lens[i] * lens[j]);
                 }
@@ -37,6 +39,7 @@ public class MaxProductofWordLengths {
         return max;
     }
 
+    // Bit Manipulation + Hash Table
     // beats 21.65%(70 ms)
     public int maxProduct2(String[] words) {
         Map<Integer, Integer> maxLen = new HashMap<>();;
@@ -57,9 +60,40 @@ public class MaxProductofWordLengths {
         return max;
     }
 
-    void test(int expected, String... words) {
+    // Bit Manipulation + Sort
+    // time complexity: O(N ^ 2), space complexity: O(N)
+    // beats 37.03%(41 ms for 174 tests)
+    public int maxProduct3(String[] words) {
+        Arrays.sort(words, new Comparator<String>(){
+            public int compare(String a, String b) {
+                return b.length() - a.length();
+            }
+        });
+        int max = 0;
+        int n = words.length;
+        int[] codes = new int[n];
+        for (int i = n - 1; i >= 0; i--) {
+            for (char c: words[i].toCharArray()) {
+                codes[i] |= 1 << (c - 'a');
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            int len = words[i].length();
+            if (len * len <= max) break;
+            for (int j = i + 1; j < n; j++) {
+                if ((codes[i] & codes[j]) == 0) {
+                    max = Math.max(max, len * words[j].length());
+                    break;
+                }
+            }
+        }
+        return max;
+    }
+
+    void test(int expected, String ... words) {
         assertEquals(expected, maxProduct(words));
         assertEquals(expected, maxProduct2(words));
+        assertEquals(expected, maxProduct3(words));
     }
 
     @Test
