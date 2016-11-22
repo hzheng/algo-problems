d14c438a5e3d6cbf193212c2b73d299bd88cdb60
Hui Zheng <xyzdll@gmail.com>
2016-11-22 12:35:30 +0800

    improve and add 1 more solution for CountRangeSum

--- leetcode/recursion_DP/CountRangeSum.java
+++ leetcode/recursion_DP/CountRangeSum.java
@@ -3,7 +3,7 @@ import java.util.*;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-// https://leetcode.com/problems/count-of-range-sum/
+// LC327: https://leetcode.com/problems/count-of-range-sum/
 //
 // Given an integer array nums, return the number of range sums that lie in
 // [lower, upper] inclusive.
@@ -12,9 +12,9 @@ import static org.junit.Assert.*;
 // Note:
 // A naive algorithm of O(n ^ 2) is trivial. You MUST do better than that.
 public class CountRangeSum {
-    // Divide and Conquer + Binary Search
+    // Recursion + Divide and Conquer + Sort + Binary Search
     // time complexity: O(N * log(N) ^ 2), space complexity: O(N)
-    // beats 41.33%(37 ms)
+    // beats 35.13%(41 ms for 61 tests)
     public int countRangeSum(int[] nums, int lower, int upper) {
         int n = nums.length;
         if (n == 0) return 0;
@@ -23,49 +23,37 @@ public class CountRangeSum {
         for (int i = 0; i < n; i++) {
             sums[i + 1] = sums[i] + nums[i];
         }
-        return countRangeSum(nums, lower, upper, 0, n, sums, new long[n / 2]);
+        return countRangeSum(nums, lower, upper, 0, n, sums, new double[n / 2]);
     }
 
     private int countRangeSum(int[] nums, int lower, int upper,
-                              int start, int end, long[] sums, long[] buffer) {
-        int size = (end - start) / 2;
+                              int start, int end, long[] sums, double[] buffer) {
+        int size = (end - start) >>> 1;
         if (size == 0) {
             return nums[start] >= lower && nums[start] <= upper ? 1 : 0;
         }
-
         int mid = start + size;
         int count = countRangeSum(nums, lower, upper, start, mid, sums, buffer)
                     + countRangeSum(nums, lower, upper, mid, end, sums, buffer);
-
-        System.arraycopy(sums, start, buffer, 0, size);
+        for (int i = 0; i < size; i++) {
+            buffer[i] = sums[start + i];
+        }
         Arrays.sort(buffer, 0, size);
-        for (int i = mid; i < end; i++) {
-            int minPos = Arrays.binarySearch(buffer, 0, size, sums[i + 1] - upper);
-            if (minPos < 0) {
-                minPos = -minPos - 1;
-                if (minPos == size) continue;
-            } else { // in case of duplicates
-                for (int j = minPos - 1; j >= 0 && buffer[j] == buffer[j + 1];
-                     j--, minPos--) {}
+        for (int i = mid + 1; i <= end; i++) {
+            int min = -Arrays.binarySearch(buffer, 0, size, sums[i] - upper - 0.5);
+            if (min <= size) {
+                count += -Arrays.binarySearch(
+                    buffer, min - 1, size, sums[i] - lower + 0.5) - min;
             }
-
-            int maxPos = Arrays.binarySearch(buffer, minPos, size, sums[i + 1] - lower);
-            if (maxPos < 0) {
-                maxPos = -maxPos - 1;
-                if (maxPos <= 0) continue;
-            } else { // in case of duplicates
-                for (int j = ++maxPos; j < size && buffer[j] == buffer[j - 1];
-                     j++, maxPos++) {}
-            }
-            count += maxPos - minPos;
         }
         return count;
     }
 
-    // Merge Sort processing
-    // https://discuss.leetcode.com/topic/33738/share-my-solution/2
+    // Solution of Choice
+    // Recursion + Divide & Conquer + Merge Sort
+    // https://discuss.leetcode.com/topic/33738/share-my-solution/
     // time complexity: O(N * log(N)), space complexity: O(N)
-    // beats 60.48%(16 ms)
+    // beats 62.49%(16 ms for 61 tests)
     public int countRangeSum2(int[] nums, int lower, int upper) {
         int n = nums.length;
         long[] sums = new long[n + 1];
@@ -85,20 +73,20 @@ public class CountRangeSum {
                     + countInMergeSort(sums, mid, end, lower, upper, buffer);
         int k = mid;
         for (int i = start, j = 0, left = mid, right = mid; i < mid; i++, j++) {
-            while (left < end && sums[left] - sums[i] < lower) left++;
-            while (right < end && sums[right] - sums[i] <= upper) right++;
-            while (k < end && sums[k] < sums[i]) buffer[j++] = sums[k++];
-            buffer[j] = sums[i];
+            for (; left < end && sums[left] - sums[i] < lower; left++) {}
+            for (; right < end && sums[right] - sums[i] <= upper; right++) {}
             count += right - left;
+            for (; k < end && sums[k] < sums[i]; buffer[j++] = sums[k++]) {}
+            buffer[j] = sums[i];
         }
         System.arraycopy(buffer, 0, sums, start, k - start);
         return count;
     }
 
-    // Binary Indexed Tree
+    // Sort + Binary Search + Binary Indexed Tree
     // https://discuss.leetcode.com/topic/33749/an-o-n-log-n-solution-via-fenwick-tree
     // time complexity: O(N * log(N)), space complexity: O(N)
-    // beats 35.65%(41 ms)
+    // beats 35.13%(41 ms for 61 tests)
     public int countRangeSum3(int[] nums, int lower, int upper) {
         int n = nums.length;
         if (n == 0) return 0;
@@ -113,7 +101,6 @@ public class CountRangeSum {
             bounds[j++] = upper + sums[i];
         }
         Arrays.sort(bounds);
-
         int[] bit = new int[bounds.length];
         for (int i = 0; i <= n; i++) {
             add(bit, Arrays.binarySearch(bounds, sums[i]), 1);
@@ -141,9 +128,10 @@ public class CountRangeSum {
         return sum;
     }
 
+    // Sort + Binary Search + Binary Indexed Tree
     // https://maskray.me/leetcode/count-of-range-sum.cc
-    // beats 47.18%(31 ms)
     // time complexity: O(N * log(N)), space complexity: O(N)
+    // beats 46.48%(25 ms for 61 tests)
     public int countRangeSum4(int[] nums, int lower, int upper) {
         int n = nums.length;
         if (n == 0) return 0;
@@ -156,13 +144,13 @@ public class CountRangeSum {
         Arrays.sort(sums, 0, n + 1);
         long prefixSum = 0;
         int count = 0;
-        add(bit, bound(sums, 0, n + 1, 0, true));
+        add(bit, bound(sums, 0, true));
         for (int num : nums) {
             prefixSum += num;
-            int maxPos = bound(sums, 0, n + 1, prefixSum - lower, false);
-            int minPos = bound(sums, 0, n + 1, prefixSum - upper, true);
+            int maxPos = bound(sums, prefixSum - lower, false);
+            int minPos = bound(sums, prefixSum - upper, true);
             count += sum2(bit, maxPos) - sum2(bit, minPos);
-            add(bit, bound(sums, 0, n + 1, prefixSum, true));
+            add(bit, bound(sums, prefixSum, true));
         }
         return count;
     }
@@ -181,20 +169,83 @@ public class CountRangeSum {
         return sum;
     }
 
-    private int bound(long[] nums, int start, int end, long x, boolean lower) {
-        int pos = Arrays.binarySearch(nums, start, end, x);
+    private int bound(long[] nums, long x, boolean lower) {
+        int pos = Arrays.binarySearch(nums, x);
         return (pos >= 0) ? (lower ? pos : pos + 1) : -pos - 1;
     }
 
-    // TODO: Segment Tree
+    // Sort + Segment Tree + Recursion
+    // beats 39.67%(36 ms for 61 tests)
+    public int countRangeSum5(int[] nums, int lower, int upper) {
+        Set<Long> valSet = new HashSet<>();
+        long sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += (long)nums[i];
+            valSet.add(sum);
+        }
+        Long[] vals = valSet.toArray(new Long[0]);
+        Arrays.sort(vals);
+        SegmentTreeNode root = SegmentTreeNode.build(vals, 0, vals.length - 1);
+        int res = 0;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            root.update(sum);
+            sum -= (long)nums[i];
+            res += root.getCount((long)lower + sum, (long)upper + sum);
+        }
+        return res;
+    }
+
+    private static class SegmentTreeNode {
+        SegmentTreeNode left, right;
+        int count;
+        long min, max;
+
+        SegmentTreeNode(long min, long max) {
+            this.min = min;
+            this.max = max;
+        }
+
+        static SegmentTreeNode build(Long[] vals, int low, int high) {
+            if (low > high) return null;
+
+            SegmentTreeNode node = new SegmentTreeNode(vals[low], vals[high]);
+            if (low == high) return node;
+
+            int mid = (low + high) >>> 1;
+            node.left = build(vals, low, mid);
+            node.right = build(vals, mid + 1, high);
+            return node;
+        }
+
+        void update(long val) {
+            if (val < min || val > max) return;
+
+            count++;
+            if (left != null) {
+                left.update(val);
+            }
+            if (right != null) {
+                right.update(val);
+            }
+        }
+
+        int getCount(long min, long max) {
+            if (min > this.max || max < this.min) return 0;
+            if (min <= this.min && max >= this.max) return count;
+
+            return (left == null ? 0 : left.getCount(min, max))
+                   + (right == null ? 0 : right.getCount(min, max));
+        }
+    }
+
     // TODO: BST
-    // TODO: Order Statistic Tree
 
     void test(int[] nums, int lower, int upper, int expected) {
         assertEquals(expected, countRangeSum(nums, lower, upper));
         assertEquals(expected, countRangeSum2(nums, lower, upper));
         assertEquals(expected, countRangeSum3(nums, lower, upper));
         assertEquals(expected, countRangeSum4(nums, lower, upper));
+        assertEquals(expected, countRangeSum5(nums, lower, upper));
     }
 
     @Test
