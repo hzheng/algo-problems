7d0c2e0e8fc66e6410c908297a008dd712f81ea1
Hui Zheng <xyzdll@gmail.com>
2017-01-31 01:44:29 +0800

    add subtraction implementations

--- leetcode/bit_manipulation/IntegerSum.java
+++ leetcode/bit_manipulation/IntegerSum.java
@@ -24,6 +24,23 @@ public class IntegerSum {
         return sum;
     }
 
+    //////////////////////Subtract/////////////////////////
+    public int getSubtract(int a, int b) {
+        return b == 0 ? a : getSubtract(a ^ b, (~a & b) << 1);
+    }
+
+    public int getSubtract2(int a, int b) {
+        int sum = a;
+        for (int second = b, borrow; second != 0; sum ^= second, second = borrow) {
+            borrow = (~sum & second) << 1;
+        }
+        return sum;
+    }
+
+    public int getSubtract3(int a, int b) {
+        return getSum(a, getSum(~b, 1));
+    }
+
     void test(int a, int b) {
         assertEquals(a + b, getSum(a, b));
         assertEquals(a + b, getSum2(a, b));
@@ -36,6 +53,23 @@ public class IntegerSum {
         test(10, 18);
         test(113, 418);
         test(1993, 40018);
+    }
+
+    void test2(int a, int b) {
+        assertEquals(a - b, getSubtract(a, b));
+        assertEquals(a - b, getSubtract2(a, b));
+        assertEquals(a - b, getSubtract3(a, b));
+    }
+
+    @Test
+    public void test2() {
+        test2(-1, 1);
+        test2(1, 8);
+        test2(10, 18);
+        test2(113, 418);
+        test2(1993, 40018);
+        test2(-21993, 4990018);
+        test2(-21993, -4990018);
     }
 
     public static void main(String[] args) {
