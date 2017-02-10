899a557aaa3d713ad710088147e1e7daafa9a603
Hui Zheng <xyzdll@gmail.com>
2017-02-10 12:58:29 +0800

    add 3 more solutions for Utf8Validation

--- leetcode/bit_manipulation/Utf8Validation.java
+++ leetcode/bit_manipulation/Utf8Validation.java
@@ -24,41 +24,100 @@ import static org.junit.Assert.*;
 // The input is an array of integers. Only the least significant 8 bits of each
 // integer is used to store the data. This means each integer represents only 1 byte of data.
 public class Utf8Validation {
-    // beats 49.49%(8 ms)
+    // beats 39.77%(8 ms for 49 tests)
     public boolean validUtf8(int[] data) {
         int len = data.length;
         for (int i = 0; i < len; i++) {
-            int nBytes = 0;
-            for (int b = data[i], mask = 0x80; mask > 0; mask >>= 1, nBytes++) {
-                if ((b & mask) == 0) break;
-            }
-            if (nBytes > 4 || nBytes == 1) return false;
+            int bits = 0;
+            for (int b = data[i], mask = 0x80; (b & mask) != 0; mask >>= 1, bits++) {}
+            if (bits > 4 || bits == 1) return false;
 
-            for ( ; nBytes > 1; nBytes--) {
-                if (++i >= len) return false;
-
-                if ((data[i] >> 6) != 2) return false;
+            for (; bits > 1; bits--) {
+                if (++i >= len || (data[i] >> 6) != 2) return false;
             }
         }
         return true;
     }
 
+    // beats 69.02%(7 ms for 49 tests)
+    public boolean validUtf8_2(int[] data) {
+        int bits = 0;
+        for (int n : data) {
+            if (bits == 0) {
+                if ((n >> 5) == 0b110) {
+                    bits = 1;
+                } else if ((n >> 4) == 0b1110) {
+                    bits = 2;
+                } else if ((n >> 3) == 0b11110) {
+                    bits = 3;
+                } else if ((n >> 7) == 1) return false;
+            } else if ((n >> 6) == 0b10) {
+                bits--;
+            } else return false;
+        }
+        return bits == 0;
+    }
+
+    // Solution of Choice
+    // beats 39.77%(8 ms for 49 tests)
+    public boolean validUtf8_3(int[] data) {
+        int bits = 0;
+        for (int n : data) {
+            if (bits == 0) {
+                if ((n >>= 3) == 0b11110) {
+                    bits = 3;
+                } else if ((n >>= 1) == 0b1110) {
+                    bits = 2;
+                } else if ((n >>= 1) == 0b110) {
+                    bits = 1;
+                } else if ((n >> 2) == 1) return false;
+            } else if ((n >> 6) == 0b10) {
+                bits--;
+            } else return false;
+        }
+        return bits == 0;
+    }
+
+    // beats 39.77%(8 ms for 49 tests)
+    public boolean validUtf8_4(int[] data) {
+        int bits = 0;
+        for (int n : data) {
+            if (n >= 0b11000000) {
+                if (bits != 0 || n >= 0b11111000) return false;
+                if (n >= 0b11110000) {
+                    bits = 3;
+                } else if (n >= 0b11100000) {
+                    bits = 2;
+                } else {
+                    bits = 1;
+                }
+            } else if (n >= 0b10000000) {
+                if (--bits < 0) return false;
+            } else if (bits > 0) return false;
+        }
+        return bits == 0;
+    }
+
     void test(int[] data, boolean expected) {
         assertEquals(expected, validUtf8(data));
+        assertEquals(expected, validUtf8_2(data));
+        assertEquals(expected, validUtf8_3(data));
+        assertEquals(expected, validUtf8_4(data));
     }
 
     @Test
     public void test1() {
-        test(new int[]{197, 130, 1}, true);
-        test(new int[]{235, 140, 4}, false);
-        test(new int[]{0b11000101, 0b10101111}, true);
-        test(new int[]{0b11000101, 0b10101111, 0b10101101}, false);
-        test(new int[]{0b11100101, 0b10101111, 0b10101101}, true);
-        test(new int[]{0b11100101, 0b10101111, 0b10101111, 0b10101101}, false);
-        test(new int[]{0b11110101, 0b10101111, 0b10101111, 0b10101101}, true);
-        test(new int[]{0b11110101, 0b10101111, 0b10101111, 0b11101101}, false);
-        test(new int[]{0b11110101, 0b10101111, 0b10101111}, false);
-        test(new int[]{0b11000101, 0b10101111, 0b11100101, 0b10101111, 0b10101101}, true);
+        test(new int[] {197, 130, 1}, true);
+        test(new int[] {235, 140, 4}, false);
+        test(new int[] {248, 130, 130, 130}, false);
+        test(new int[] {0b11000101, 0b10101111}, true);
+        test(new int[] {0b11000101, 0b10101111, 0b10101101}, false);
+        test(new int[] {0b11100101, 0b10101111, 0b10101101}, true);
+        test(new int[] {0b11100101, 0b10101111, 0b10101111, 0b10101101}, false);
+        test(new int[] {0b11110101, 0b10101111, 0b10101111, 0b10101101}, true);
+        test(new int[] {0b11110101, 0b10101111, 0b10101111, 0b11101101}, false);
+        test(new int[] {0b11110101, 0b10101111, 0b10101111}, false);
+        test(new int[] {0b11000101, 0b10101111, 0b11100101, 0b10101111, 0b10101101}, true);
     }
 
     public static void main(String[] args) {
