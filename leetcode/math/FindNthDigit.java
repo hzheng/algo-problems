5ce6d00ef9159d64eae705e7d7d9afe20cd1cdbe
Hui Zheng <xyzdll@gmail.com>
2017-02-13 22:53:29 +0800

    minor improvement

--- leetcode/math/FindNthDigit.java
+++ leetcode/math/FindNthDigit.java
@@ -8,50 +8,45 @@ import static org.junit.Assert.*;
 // Find the nth digit of the infinite integer sequence 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ...
 // n is positive and will fit within the range of a 32-bit signed integer (n < 2 ^ 31).
 public class FindNthDigit {
-    // beats 39.39%(6 ms)
+    // beats 38.51%(6 ms for 70 tests)
     public int findNthDigit(int n) {
-        if (n < 10) return n;
-
         long power = 1;
-        long digits = 1;
-        long l = n - 1;
+        int digits = 1;
+        long offset = n - 1;
         for (;; power *= 10, digits++) {
-            long group = digits * (power * 9);
-            if (l >= group) {
-                l -= group;
+            long groupLen = digits * (power * 9);
+            if (offset >= groupLen) {
+                offset -= groupLen;
             } else break;
         }
-        long order = l / digits;
-        long num = power + order;
-        long res = 0;
-        for (long i = digits - (l - order * digits) - 1L; i >= 0; i--, num /= 10) {
-            res = num % 10L;
+        int res = 0;
+        for (long i = offset % digits, j = power + offset / digits; i < digits; i++, j /= 10) {
+            res = (int)(j % 10);
         }
-        return (int)res;
+        return res;
     }
 
-    // https://discuss.leetcode.com/topic/59314/java-solution
-    // beats 39.39%(6 ms)
+    // Solution of Choice
+    // beats 38.51%(6 ms for 70 tests)
     public int findNthDigit2(int n) {
-        int len = 1;
-        int start = 1;
-        for (long count = 9; n > len * count; count *= 10, start *= 10, len++) {
-            n -= len * count;
+        int digits = 1;
+        int power = 1;
+        int offset = n - 1;
+        for (long count = 9; offset > digits * count; count *= 10, power *= 10, digits++) {
+            offset -= digits * count;
         }
-        start += (n - 1) / len;
-        String s = Integer.toString(start);
-        return Character.getNumericValue(s.charAt((n - 1) % len));
+        return Integer.toString(power + offset / digits).charAt(offset % digits) - '0';
     }
 
-    // https://discuss.leetcode.com/topic/59378/short-python-java
-    // beats 39.39%(6 ms)
+    // beats 17.68%(7 ms for 70 tests)
     public int findNthDigit3(int n) {
         int digits = 1;
-        int first = 1;
-        for (n--; n / 9 / first / digits >= 1; first *= 10, digits++) {
-            n -= 9 * first * digits;
+        int power = 1;
+        int offset = n - 1;
+        for (; offset / 9 / power / digits >= 1; power *= 10, digits++) {
+            offset -= 9 * power * digits;
         }
-        return Integer.toString(first + n / digits).charAt(n % digits) - '0';
+        return Integer.toString(power + offset / digits).charAt(offset % digits) - '0';
     }
 
     void test(int n, int expected) {
