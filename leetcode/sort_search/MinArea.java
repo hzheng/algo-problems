063e1472b8c92130e1abaa9cfa6567694e9be4cc
Hui Zheng <xyzdll@gmail.com>
2017-03-18 20:44:04 +0800

    add 2 more solutions for MinArea

--- leetcode/sort_search/MinArea.java
+++ leetcode/sort_search/MinArea.java
@@ -14,6 +14,7 @@ public class MinArea {
     private static final int[][] MOVES = new int[][] {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
     // BFS + Queue
+    // time complexity: O(B), space complexity: O(M * N)
     // beats 7.45%(24 ms for 111 tests)
     public int minArea(char[][] image, int x, int y) {
         int m = image.length;
@@ -45,6 +46,7 @@ public class MinArea {
     }
 
     // Binary Search
+    // time complexity: O(M * log(N) + N * log(M)), space complexity: O(1)
     // beats 63.21%(1 ms for 111 tests)
     public int minArea2(char[][] image, int x, int y) {
         int leftMost = search(image, 0, y, false, true);
@@ -81,6 +83,7 @@ public class MinArea {
     }
 
     // Binary Search
+    // time complexity: O(M * log(N) + N * log(M)), space complexity: O(1)
     // beats 63.21%(1 ms for 111 tests)
     public int minArea3(char[][] image, int x, int y) {
         int m = image.length;
@@ -106,17 +109,64 @@ public class MinArea {
         return low;
     }
 
+    // DFS + Recursion
+    // time complexity: O(B), space complexity: O(M * N)
+    // beats 18.28%(8 ms for 111 tests)
+    public int minArea4(char[][] image, int x, int y) {
+        int[] boundaries = new int[] {x, 0, y, 0};
+        dfs(image, x, y, boundaries, new boolean[image.length][image[0].length]);
+        return (boundaries[1] - boundaries[0] + 1) * (boundaries[3] - boundaries[2] + 1);
+    }
+
+    private void dfs(char[][] image, int x, int y, int[] boundaries, boolean[][] visited) {
+        if (x < 0 || y < 0 || x >= image.length || y >= image[0].length
+            || image[x][y] == '0' || visited[x][y]) return;
+
+        visited[x][y] = true;
+        boundaries[0] = Math.min(boundaries[0], x);
+        boundaries[1] = Math.max(boundaries[1], x);
+        boundaries[2] = Math.min(boundaries[2], y);
+        boundaries[3] = Math.max(boundaries[3], y);
+        dfs(image, x + 1, y, boundaries, visited);
+        dfs(image, x - 1, y, boundaries, visited);
+        dfs(image, x, y - 1, boundaries, visited);
+        dfs(image, x, y + 1, boundaries, visited);
+    }
+
+    // Linear Search
+    // time complexity: O(M * N), space complexity: O(1)
+    // beats 18.28%(8 ms for 111 tests)
+    public int minArea5(char[][] image, int x, int y) {
+        int top = x;
+        int bottom = x;
+        int left = y;
+        int right = y;
+        for (int i = 0; i < image.length; ++i) {
+            for (int j = 0; j < image[0].length; j++) {
+                if (image[i][j] == '1') {
+                    top = Math.min(top, i);
+                    bottom = Math.max(bottom, i + 1);
+                    left = Math.min(left, j);
+                    right = Math.max(right, j + 1);
+                }
+            }
+        }
+        return (right - left) * (bottom - top);
+    }
+
     void test(String[] s, int x, int y, int expected) {
         char[][] image = Stream.of(s).map(a -> a.toCharArray()).toArray(char[][]::new);
         assertEquals(expected, minArea(image, x, y));
         assertEquals(expected, minArea2(image, x, y));
         assertEquals(expected, minArea3(image, x, y));
+        assertEquals(expected, minArea4(image, x, y));
+        assertEquals(expected, minArea5(image, x, y));
     }
 
     @Test
     public void test() {
-        test(new String[] {"0010", "0110", "0100"}, 0, 2, 6);
         test(new String[] {"01"}, 0, 1, 1);
+        test(new String[] {"0010", "0110", "0100"}, 0, 2, 6);
         test(new String[] {"1111111101", "1000000101", "1011110101", "1010010101",
                            "1010110101", "1010000101", "1011111101", "1000000001", "1111111111"}, 4, 4, 90);
     }
