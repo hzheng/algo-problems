265be0deede502bdf48bc622531433fca66ac07c
Hui Zheng <xyzdll@gmail.com>
2017-04-17 12:10:12 +0800

    add 2 more solutions for AttendanceRecord2

--- leetcode/recursion_DP/AttendanceRecord2.java
+++ leetcode/recursion_DP/AttendanceRecord2.java
@@ -142,12 +142,87 @@ public class AttendanceRecord2 {
         return (int)a1l0;
     }
 
+    // Dynamic Programming
+    // time complexity: O(N), space complexity: O(N)
+    // beats N/A(359 ms for 58 tests)
+    public int checkRecord6(int n) {
+        int[][][] dp = new int[n + 1][2][3];
+        dp[0] = new int[][] {{1, 1, 1}, {1, 1, 1}};
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < 2; j++) {
+                for (int k = 0; k < 3; k++) {
+                    int count = dp[i - 1][j][2]; // P
+                    if (j > 0) {
+                        count = (count + dp[i - 1][j - 1][2]) % MOD; // A
+                    }
+                    if (k > 0) {
+                        count = (count + dp[i - 1][j][k - 1]) % MOD; // L
+                    }
+                    dp[i][j][k] = count;
+                }
+            }
+        }
+        return dp[n][1][2];
+    }
+
+    // Matrix
+    // https://discuss.leetcode.com/topic/86526/improving-the-runtime-from-o-n-to-o-log-n
+    // time complexity: O(log(N)), space complexity: O(1)
+    // beats N/A(14 ms for 58 tests)
+    public int checkRecord7(int n) {
+        // f[i][0][0]   | 0 0 1 0 0 0 |   f[i-1][0][0]
+        // f[i][0][1]   | 1 0 1 0 0 0 |   f[i-1][0][1]
+        // f[i][0][2] = | 0 1 1 0 0 0 | * f[i-1][0][2]
+        // f[i][1][0]   | 0 0 1 0 0 1 |   f[i-1][1][0]
+        // f[i][1][1]   | 0 0 1 1 0 1 |   f[i-1][1][1]
+        // f[i][1][2]   | 0 0 1 0 1 1 |   f[i-1][1][2]
+        int[][] matrix = {
+            {0, 0, 1, 0, 0, 0},
+            {1, 0, 1, 0, 0, 0},
+            {0, 1, 1, 0, 0, 0},
+            {0, 0, 1, 0, 0, 1},
+            {0, 0, 1, 1, 0, 1},
+            {0, 0, 1, 0, 1, 1},
+        };
+        return pow(matrix, n + 1)[5][2];
+    }
+
+    private int[][] pow(int[][] matrix, int n) {
+        int N = matrix.length;
+        int[][] res = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            res[i][i] = 1;
+        }
+        for (; n > 0; n /= 2) {
+            if (n % 2 == 1) {
+                res = multiply(res, matrix);
+            }
+            matrix = multiply(matrix, matrix);
+        }
+        return res;
+    }
+
+    private int[][] multiply(int[][] A, int[][] B) {
+        int N = A.length;
+        int[][] C = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                for (int k = 0; k < N; k++) {
+                    C[i][j] = (int)((C[i][j] + (long)A[i][k] * B[k][j]) % MOD);
+                }
+            }
+        }
+        return C;
+    }
+
     void test(int n, int expected) {
         assertEquals(expected, checkRecord(n));
         assertEquals(expected, checkRecord2(n));
         assertEquals(expected, checkRecord3(n));
         assertEquals(expected, checkRecord4(n));
         assertEquals(expected, checkRecord5(n));
+        assertEquals(expected, checkRecord6(n));
+        assertEquals(expected, checkRecord7(n));
     }
 
     @Test
