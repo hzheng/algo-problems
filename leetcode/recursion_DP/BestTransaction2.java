b7b741a3c47a22a3ba51033ccdc98210102352c6
Hui Zheng <xyzdll@gmail.com>
2018-08-20 23:58:44 -0400

    add one more solution for BestTransaction2.java

--- leetcode/recursion_DP/BestTransaction2.java
+++ leetcode/recursion_DP/BestTransaction2.java
@@ -9,22 +9,19 @@ import static org.junit.Assert.*;
 // on day i. Design an algorithm to find the maximum profit. You may complete as
 // many transactions as you like.
 public class BestTransaction2 {
-    // beats 11.51%(2 ms)
+    // beats 7.73%(2 ms for 201 tests)
     public int maxProfit(int[] prices) {
         if (prices.length == 0) return 0;
 
         int profit = 0;
-        int last = prices[0];
         int low = prices[0];
         for (int i = 1; i < prices.length; i++) {
-            int price = prices[i];
-            if (price < last) {
-                profit += last - low;
-                low = price;
+            if (prices[i] < prices[i - 1]) {
+                profit += prices[i - 1] - low;
+                low = prices[i];
             }
-            last = price;
         }
-        return profit + last - low;
+        return profit + prices[prices.length - 1] - low;
     }
 
     // Solution of Choice
@@ -37,6 +34,25 @@ public class BestTransaction2 {
         return profit;
     }
 
+    // beats 99.96%(1 ms for 201 tests)
+    public int maxProfit3(int[] prices) {
+        int n = prices.length;
+        if (n == 0) return 0;
+
+        int res = 0;
+        for (int i = 1, min = prices[0]; i < n; ) {
+            for (; i < n && prices[i] <= prices[i - 1]; i++) {
+                min = prices[i]; 
+            }
+            int max = min;
+            for (; i < n && prices[i] >= prices[i - 1]; i++) {
+                max = prices[i]; 
+            }
+            res += max - min;
+        }
+        return res;
+    }
+
     void test(Function<int[], Integer> maxProfit, int expected, int ... prices) {
         assertEquals(expected, (int)maxProfit.apply(prices));
     }
@@ -45,6 +61,7 @@ public class BestTransaction2 {
         BestTransaction2 b = new BestTransaction2();
         test(b::maxProfit, expected, prices);
         test(b::maxProfit2, expected, prices);
+        test(b::maxProfit3, expected, prices);
     }
 
     @Test
@@ -55,6 +72,8 @@ public class BestTransaction2 {
     }
 
     public static void main(String[] args) {
-        org.junit.runner.JUnitCore.main("BestTransaction2");
+        String clazz =
+            new Object() {}.getClass().getEnclosingClass().getSimpleName();
+        org.junit.runner.JUnitCore.main(clazz);
     }
 }
