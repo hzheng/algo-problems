af2443e609190b67c4ff21a7b86cda2fd851e7c0
Hui Zheng <xyzdll@gmail.com>
2018-08-30 13:36:04 -0400

    improve reflection part test code in FreqStack.java

--- leetcode/design/FreqStack.java
+++ leetcode/design/FreqStack.java
@@ -1,20 +1,16 @@
 import java.util.*;
 
+import java.lang.reflect.Constructor;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 // LC895: https://leetcode.com/problems/maximum-frequency-stack/
 //
 public class FreqStack {
-    static interface IFreqStack {
-        public void push(int x);
-
-        public int pop();
-    }
-
     // Deque + SortedSet + Hash Table
     // beats %(147 ms for 36 tests)
-    class FreqStack1 implements IFreqStack {
+    class FreqStack1 {
         private Map<Integer, Deque<Integer> > map = new HashMap<>();
         private NavigableSet<Deque<Integer> > set = new TreeSet<>(
             new Comparator<Deque<Integer> >() {
@@ -24,6 +20,8 @@ public class FreqStack {
             }
         });
         private int num = 0;
+
+        public FreqStack1() {}
 
         // time complexity: O(log(N))
         public void push(int x) {
@@ -51,10 +49,12 @@ public class FreqStack {
 
     // Stack + Hash Table
     // beats %(111 ms for 36 tests)
-    class FreqStack2 implements IFreqStack {
+    class FreqStack2 {
         private Map<Integer, Stack<Integer> > group = new HashMap<>();
         private Map<Integer, Integer> freq = new HashMap<>();
         private int maxFreq;
+
+        public FreqStack2() {}
 
         // time complexity: O(1)
         public void push(int x) {
@@ -84,9 +84,11 @@ public class FreqStack {
 
     // Stack + Hash Table
     // beats %(139 ms for 36 tests)
-    class FreqStack3 implements IFreqStack {
+    class FreqStack3 {
         private List<Stack<Integer>> bucket = new ArrayList<>();
         private Map<Integer, Integer> count = new HashMap<>();
+
+        public FreqStack3() {}
 
         // time complexity: O(1)
         public void push(int x) {
@@ -110,104 +112,104 @@ public class FreqStack {
         }
     }
 
-    void test1(IFreqStack obj) {
-        test(obj, new String[] { "FreqStack", "push", "push", "push", "push",
+    void test1(String className) throws Exception {
+        final FreqStack outerObj = new FreqStack();
+        final Object[] VOID = new Object[]{};
+        test(new String[] { className, "push", "push", "push", "push",
                                  "push", "push", "pop", "pop", "pop", "pop"},
-             new int[][] { new int[]{}, new int[]{5}, new int[]{7}, new int[]{5},
-                           new int[]{7}, new int[]{4}, new int[]{5},
-                           new int[]{}, new int[]{}, new int[]{}, new int[]{}},
+             new Object[][] { new Object[]{ outerObj }, 
+                           new Integer[]{5}, new Integer[]{7}, new Integer[]{5},
+                           new Integer[]{7}, new Integer[]{4}, new Integer[]{5},
+                           VOID, VOID, VOID, VOID},
              new Integer[] { null, null, null, null, null, null, null,
                             5, 7, 5, 4 });
-        test(obj, new String[] { "FreqStack", "push", "push", "push", "push", "push",
-                                 "push", "push", "push", "push", "push",
-                                 "push", "push", "push", "push", "push", "push",
-                                 "push", "push", "push", "push", "push", "push",
-                                 "push",
-                                 "push", "push", "push", "pop", "push", "pop",
-                                 "push", "pop", "push", "pop", "push", "pop",
-                                 "push",
-                                 "pop", "push", "pop", "push", "pop", "push",
-                                 "pop", "push", "pop", "push", "pop", "push",
-                                 "pop", "push",
-                                 "pop", "push", "pop", "push", "pop", "push",
-                                 "pop", "push", "pop", "push", "pop", "push",
-                                 "pop", "push",
-                                 "pop", "push", "pop", "push", "pop", "push",
-                                 "pop", "push", "pop", "push", "pop", "pop",
-                                 "pop", "pop",
-                                 "pop", "pop", "pop", "pop", "pop", "pop",
-                                 "pop", "pop", "pop", "pop", "pop", "pop",
-                                 "pop", "pop", "pop",
-                                 "pop", "pop", "pop", "pop", "pop", "pop",
-                                 "pop" },
-             new int[][] { new int[] {}, new int[] { 30 }, new int[] { 40 },
-                           new int[] { 4 }, new int[] { 25 },
-                           new int[] { 20 }, new int[] { 40 }, new int[] { 48 },
-                           new int[] { 21 }, new int[] { 12 },
-                           new int[] { 44 }, new int[] { 1 }, new int[] { 16 },
-                           new int[] { 20 }, new int[] { 9 },
-                           new int[] { 34 }, new int[] { 26 }, new int[] { 12 },
-                           new int[] { 21 }, new int[] { 35 },
-                           new int[] { 16 }, new int[] { 3 }, new int[] { 23 },
-                           new int[] { 9 }, new int[] { 31 },
-                           new int[] { 10 }, new int[] { 6 }, new int[] {},
-                           new int[] { 45 }, new int[] {},
-                           new int[] { 16 }, new int[] {}, new int[] { 14 },
-                           new int[] {}, new int[] { 27 }, new int[] {},
-                           new int[] { 35 }, new int[] {}, new int[] { 34 },
-                           new int[] {}, new int[] { 40 }, new int[] {},
-                           new int[] { 13 }, new int[] {}, new int[] { 21 },
-                           new int[] {}, new int[] { 18 }, new int[] {},
-                           new int[] { 26 }, new int[] {}, new int[] { 29 },
-                           new int[] {}, new int[] { 32 }, new int[] {},
-                           new int[] { 3 }, new int[] {}, new int[] { 18 },
-                           new int[] {}, new int[] { 36 }, new int[] {},
-                           new int[] { 1 }, new int[] {}, new int[] { 38 },
-                           new int[] {}, new int[] { 34 }, new int[] {},
-                           new int[] { 20 }, new int[] {}, new int[] { 22 },
-                           new int[] {}, new int[] { 13 }, new int[] {},
-                           new int[] { 37 }, new int[] {}, new int[] { 24 },
-                           new int[] {}, new int[] {}, new int[] {},
-                           new int[] {}, new int[] {}, new int[] {},
-                           new int[] {}, new int[] {}, new int[] {},
-                           new int[] {}, new int[] {}, new int[] {},
-                           new int[] {}, new int[] {}, new int[] {},
-                           new int[] {}, new int[] {}, new int[] {},
-             new int[] {}, new int[] {}, new int[] {},
-                           new int[] {}, new int[] {}, new int[] {}, new int[] {}, new int[] {} },
-             new Integer[] { null, null, null, null, null, null, null, null, null, null, null, null, null, null,
-                             null, null, null, null, null, null, null, null, null, null, null, null, null, 9, null, 16, null,
-                             16, null, 21, null, 12, null, 35, null, 34, null, 40, null, 20, null, 21, null, 40, null, 26,
-                             null, 29, null, 32, null, 3, null, 18, null, 36, null, 1, null, 38, null, 34, null, 20, null,
-                             22, null, 13, null, 37, null, 24, 18, 13, 27, 14, 45, 6, 10, 31, 23, 3, 35, 26, 34, 9, 16, 1,
-                             44, 12, 21, 48, 20, 25, 4, 40, 30 });
+        test(new String[] { className, "push", "push", "push", "push", "push",
+                            "push", "push", "push", "push", "push", "push", 
+                            "push", "push", "push", "push", "push", "push",
+                            "push", "push", "push", "push", "push", "push", 
+                            "push", "push", "push", "pop", "push", "pop", "push",
+                            "pop", "push", "pop", "push", "pop", "push", "pop", 
+                            "push", "pop", "push", "pop", "push", "pop", "push",
+                            "pop", "push", "pop", "push", "pop", "push", "pop",
+                            "push", "pop", "push", "pop", "push", "pop", "push",
+                            "pop", "push", "pop", "push", "pop", "push", "pop",
+                            "push", "pop", "push", "pop", "push", "pop", "push",
+                            "pop", "push", "pop", "pop", "pop", "pop", "pop",
+                            "pop", "pop", "pop", "pop", "pop", "pop", "pop",
+                            "pop", "pop", "pop", "pop", "pop", "pop", "pop",
+                            "pop", "pop", "pop", "pop", "pop", "pop", "pop" },
+             new Object[][] { new Object[] {outerObj},
+                           new Integer[] { 30 }, new Integer[] { 40 },
+                           new Integer[] { 4 }, new Integer[] { 25 },
+                           new Integer[] { 20 }, new Integer[] { 40 }, new Integer[] { 48 },
+                           new Integer[] { 21 }, new Integer[] { 12 },
+                           new Integer[] { 44 }, new Integer[] { 1 }, new Integer[] { 16 },
+                           new Integer[] { 20 }, new Integer[] { 9 },
+                           new Integer[] { 34 }, new Integer[] { 26 }, new Integer[] { 12 },
+                           new Integer[] { 21 }, new Integer[] { 35 },
+                           new Integer[] { 16 }, new Integer[] { 3 }, new Integer[] { 23 },
+                           new Integer[] { 9 }, new Integer[] { 31 },
+                           new Integer[] { 10 }, new Integer[] { 6 }, VOID,
+                           new Integer[] { 45 }, VOID,
+                           new Integer[] { 16 }, VOID, new Integer[] { 14 },
+                           VOID, new Integer[] { 27 }, VOID,
+                           new Integer[] { 35 }, VOID, new Integer[] { 34 },
+                           VOID, new Integer[] { 40 }, VOID,
+                           new Integer[] { 13 }, VOID, new Integer[] { 21 },
+                           VOID, new Integer[] { 18 }, VOID,
+                           new Integer[] { 26 }, VOID, new Integer[] { 29 },
+                           VOID, new Integer[] { 32 }, VOID,
+                           new Integer[] { 3 }, VOID, new Integer[] { 18 },
+                           VOID, new Integer[] { 36 }, VOID,
+                           new Integer[] { 1 }, VOID, new Integer[] { 38 },
+                           VOID, new Integer[] { 34 }, VOID,
+                           new Integer[] { 20 }, new Integer[] {}, new Integer[] { 22 },
+                           VOID, new Integer[] { 13 }, VOID,
+                           new Integer[] { 37 }, VOID, new Integer[] { 24 },
+                           VOID, VOID, VOID, VOID, VOID, VOID, VOID, VOID, VOID,
+                           VOID, VOID, VOID, VOID, VOID, VOID, VOID, VOID, VOID,
+                           VOID, VOID, VOID, VOID, VOID, VOID, VOID, VOID },
+             new Integer[] { null, null, null, null, null, null, null, null, 
+                             null, null, null, null, null, null, null, null,
+                             null, null, null, null, null, null, null, null,
+                             null, null, null, 9, null, 16, null, 16, null, 21,
+                             null, 12, null, 35, null, 34, null, 40, null, 20,
+                             null, 21, null, 40, null, 26, null, 29, null, 32, 
+                             null, 3, null, 18, null, 36, null, 1, null, 38,
+                             null, 34, null, 20, null, 22, null, 13, null, 37,
+                             null, 24, 18, 13, 27, 14, 45, 6, 10, 31, 23, 3, 35,
+                             26, 34, 9, 16, 1, 44, 12, 21, 48, 20, 25, 4, 40, 30 });
     }
 
-    void test(IFreqStack obj, String[] methods, int[][] args, Integer[] expected) {
-        try {
-            Class<?> clazz = obj.getClass();
-            for (int i = 1; i < methods.length; i++) {
-                int[] arg = args[i];
-                Object res = null;
-                if (arg.length == 0) {
-                    res = clazz.getMethod(methods[i]).invoke(obj);
-                } else {
-                    res = clazz.getMethod(methods[i], int.class).invoke(obj, arg[0]);
-                }
-                if (expected[i] != null) {
-                    assertEquals(expected[i], res);
-                }
+    void test(String[] methods, Object[][] args, Integer[] expected)
+        throws Exception {
+        String name = new Object() {}.getClass().getEnclosingClass().getSimpleName();
+        Class<?> clazz = Class.forName(name + "$" + methods[0]);
+        Constructor<?> ctor = clazz.getConstructors()[0];
+        Object obj = ctor.newInstance(args[0]);
+        for (int i = 1; i < methods.length; i++) {
+            Object[] arg = args[i];
+            Object res = null;
+            if (arg.length == 0) {
+                res = clazz.getMethod(methods[i]).invoke(obj);
+            } else {
+                res = clazz.getMethod(methods[i], int.class).invoke(obj, arg[0]);
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+            if (expected[i] != null) {
+                assertEquals(expected[i], res);
+            }
         }
     }
 
     @Test
     public void test1() {
-        test1(new FreqStack1());
-        test1(new FreqStack2());
-        test1(new FreqStack3());
+        try {
+            test1("FreqStack1");
+            test1("FreqStack2");
+            test1("FreqStack3");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
