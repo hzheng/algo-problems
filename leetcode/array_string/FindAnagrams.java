69e3ff5564a07bbe4c8606038d72884dd24d0db2
Hui Zheng <xyzdll@gmail.com>
2018-09-25 23:45:39 -0400

    add one more solution for FindAnagrams.java

--- leetcode/array_string/FindAnagrams.java
+++ leetcode/array_string/FindAnagrams.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.math.BigInteger;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -18,7 +19,8 @@ public class FindAnagrams {
         }
         List<Integer> res = new ArrayList<>();
         int[] curCount = new int[26];
-        for (int i = 0, matched = 0, toMatch = p.length(); i < s.length(); i++) {
+        for (int i = 0, matched = 0, toMatch = p.length(); i < s.length();
+             i++) {
             int index = s.charAt(i) - 'a';
             if (count[index] == 0) {
                 curCount = new int[26];
@@ -42,14 +44,15 @@ public class FindAnagrams {
     }
 
     // Hash Table + Two Pointers
-    // beats 56.51(16 ms for 36 tests)
+    // beats 56.51%(16 ms for 36 tests)
     public List<Integer> findAnagrams2(String s, String p) {
         List<Integer> res = new ArrayList<>();
         int[] count = new int[26];
         for (char c : p.toCharArray()) {
             count[c - 'a']++;
         }
-        for (int i = 0, j = 0, len = p.length(), toMatch = len; j < s.length(); ) {
+        for (int i = 0, j = 0, len = p.length(), toMatch = len; j < s.length();
+             ) {
             if (count[s.charAt(j++) - 'a']-- > 0) {
                 toMatch--;
             }
@@ -64,7 +67,7 @@ public class FindAnagrams {
     }
 
     // Hash Table + Two Pointers
-    // beats 24.81(43 ms for 36 tests)
+    // beats 24.81%(43 ms for 36 tests)
     public List<Integer> findAnagrams3(String s, String p) {
         List<Integer> res = new ArrayList<>();
         int len = p.length();
@@ -94,13 +97,49 @@ public class FindAnagrams {
         return res;
     }
 
+    // Math
+    // beats 41.87%(29 ms for 36 tests)
+    public List<Integer> findAnagrams4(String s, String p) {
+        int mod = 1000_000_007;
+        int primes[] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 
+                         53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101 };
+        int invs[] = new int[primes.length];
+        for (int i = 0; i < primes.length; i++) {
+            invs[i] = BigInteger.valueOf(primes[i])
+                      .modInverse(BigInteger.valueOf(mod)).intValue();
+        }
+        long target = 1;
+        for (char c : p.toCharArray()) {
+            target *= primes[(c - 'a')];
+            target %= mod;
+        }
+        int pLen = p.length();
+        int sLen = s.length();
+        long src = 1;
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < Math.min(pLen - 1, sLen); i++) {
+            src *= primes[(s.charAt(i) - 'a')];
+            src %= mod;
+        }
+        for (int i = pLen - 1; i < sLen; i++) {
+            src *= primes[(s.charAt(i) - 'a')];
+            src %= mod;
+            if (src == target) {
+                res.add(i - pLen + 1); // actually still need to verify here!
+            }
+            src *= invs[s.charAt(i - pLen + 1) - 'a'];
+            src %= mod;
+        }
+        return res;
+    }
+
     @FunctionalInterface
     interface Function<A, B, C> {
         public C apply(A a, B b);
     }
 
-    void test(Function<String, String, List<Integer>> findAnagrams,
-              String s, String p, Integer ... expected) {
+    void test(Function<String, String, List<Integer> > findAnagrams, String s,
+              String p, Integer ... expected) {
         List<Integer> res = findAnagrams.apply(s, p);
         assertArrayEquals(expected, res.toArray(new Integer[0]));
     }
@@ -110,6 +149,7 @@ public class FindAnagrams {
         test(f::findAnagrams, s, p, expected);
         test(f::findAnagrams2, s, p, expected);
         test(f::findAnagrams3, s, p, expected);
+        test(f::findAnagrams4, s, p, expected);
     }
 
     @Test
@@ -118,6 +158,7 @@ public class FindAnagrams {
         test("cbaebabacd", "abc", 0, 6);
         test("cbac", "abc", 0, 1);
         test("abab", "ab", 0, 1, 2);
+        test("aaaaaaaaaa", "aaaaaaaaaaaaa");
     }
 
     public static void main(String[] args) {
