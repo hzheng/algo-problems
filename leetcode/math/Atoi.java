4ffa72c4c73d6364784cc8cf8f19a9edf04c8aef
Hui Zheng <xyzdll@gmail.com>
2018-11-01 22:51:56 -0400

    add one more solution for Atoi.java

--- leetcode/math/Atoi.java
+++ leetcode/math/Atoi.java
@@ -7,9 +7,6 @@ import static org.junit.Assert.*;
 public class Atoi {
     // beats 14.48% (5 ms) (35.97% 4 ms if use str.trim)
     public int myAtoi(String str) {
-        if (str == null) return 0;
-        // str = str.trim();
-
         int len = str.length();
         if (len == 0) return 0;
 
@@ -75,15 +72,12 @@ public class Atoi {
         return x * sign;
     }
 
-    // Solution of Choice
-    // https://discuss.leetcode.com/topic/2666/my-simple-solution
-    // beats 81.44%(3 ms)
+    // beats 42.47%(32 ms for 1079 tests)
     public int myAtoi2(String str) {
         int len = str.length();
-        if (len == 0) return 0;
-
         int i = 0;
-        while (str.charAt(i) == ' ') i++;
+        for (; i < len && str.charAt(i) == ' '; i++) {}
+        if (i == len) return 0;
 
         int sign = 1;
         if (str.charAt(i) == '-' || str.charAt(i) == '+') {
@@ -103,15 +97,42 @@ public class Atoi {
         return res * sign;
     }
 
+    // beats 54.20%(28 ms for 1079 tests)
+    public int myAtoi3(String str) {
+        int sign = 0;
+        long res = 0;
+        for (char c : str.toCharArray()) {
+            if (c >= '0' && c <= '9') {
+                if (sign == 0) {
+                    sign = 1;
+                }
+                res = res * 10 + (c - '0');
+                if (res > Integer.MAX_VALUE) break;
+            } else if (sign == 0) {
+                if (c == ' ') continue;
+                if (c == '+') {
+                    sign = 1;
+                } else if (c == '-') {
+                    sign = -1;
+                } else return 0;
+            } else break;
+        }
+        res *= sign;
+        if (res > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+        return (res < Integer.MIN_VALUE) ? Integer.MIN_VALUE : (int)res;
+    }
+
     void test(String x, int expected) {
         assertEquals(expected, myAtoi(x));
         assertEquals(expected, myAtoi2(x));
+        assertEquals(expected, myAtoi3(x));
     }
 
     @Test
     public void test1() {
         test(" 010", 10);
         test("", 0);
+        test(" ", 0);
         test("0", 0);
         test("-0", 0);
         test("1", 1);
@@ -139,9 +160,11 @@ public class Atoi {
         test("-2147483648ab ", -2147483648);
         test("-2147483649", -2147483648);
         test("12147483649", 2147483647);
+        test("9223372036854775808", 2147483647);
     }
 
     public static void main(String[] args) {
-        org.junit.runner.JUnitCore.main("Atoi");
+        String clazz = new Object() {}.getClass().getEnclosingClass().getSimpleName();
+        org.junit.runner.JUnitCore.main(clazz);
     }
 }
