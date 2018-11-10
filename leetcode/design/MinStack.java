b2864812313e22af0a0d0ff305a1c105c7e7b2d0
Hui Zheng <xyzdll@gmail.com>
2018-11-10 17:33:05 -0500

    improve tests for MinStack.java

--- leetcode/design/MinStack.java
+++ leetcode/design/MinStack.java
@@ -1,5 +1,7 @@
 import java.util.*;
 
+import java.lang.reflect.Constructor;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -8,20 +10,14 @@ import static org.junit.Assert.*;
 // Design a stack that supports push, pop, top, and retrieving the
 // minimum element in constant time.
 public class MinStack {
-    static interface IMinStack {
-        public void pop();
-        public int top();
-        public void push(int x);
-        public int getMin();
-    }
-
     // Linked List + Heap
     // beats 13.61%(138 ms)
-    static class MinStack1 implements IMinStack {
+    class MinStack1 {
         private class Node {
             int val;
             Node next;
             Node prev;
+
             Node(int x) {
                 val = x;
             }
@@ -66,9 +62,12 @@ public class MinStack {
 
     // Two Stacks
     // beats 27.97%(131 ms)
-    static class MinStack2 implements IMinStack {
+    class MinStack2 {
         private Stack<Integer> stack = new Stack<>();
         private Stack<Integer> minStack = new Stack<>();
+
+        public MinStack2() {
+        }
 
         public void push(int x) {
             stack.push(x);
@@ -96,13 +95,13 @@ public class MinStack {
 
     // Linked List
     // beats 73.88%(120 ms)
-    static class MinStack3 implements IMinStack {
-        static class Node {
+    class MinStack3 {
+        class Node {
             int val;
             int min;
             Node next;
 
-            Node(int val, int min){
+            Node(int val, int min) {
                 this.val = val;
                 this.min = min;
             }
@@ -141,9 +140,12 @@ public class MinStack {
     // Solution of Choice
     // One Stack
     // beats 26.64%(131 ms for 18 tests)
-    static class MinStack4 implements IMinStack {
+    class MinStack4 {
         private Stack<Integer> stack = new Stack<>();
         private int min = Integer.MAX_VALUE;
+
+        public MinStack4() {
+        }
 
         public void push(int x) {
             if (x <= min) {
@@ -168,31 +170,7 @@ public class MinStack {
         }
     }
 
-    void test1(IMinStack obj) {
-        obj.push(4);
-        obj.push(2);
-        obj.push(3);
-        obj.push(1);
-        assertEquals(1, obj.getMin());
-        obj.pop();
-        assertEquals(2, obj.getMin());
-        obj.push(0);
-        obj.push(-1);
-        obj.push(2);
-        assertEquals(-1, obj.getMin());
-        assertEquals(2, obj.top());
-        obj.pop();
-        obj.pop();
-        assertEquals(0, obj.getMin());
-        obj.pop();
-        obj.pop();
-        assertEquals(2, obj.getMin());
-        obj.pop();
-        assertEquals(4, obj.getMin());
-        obj.pop();
-    }
-
-    void test2(IMinStack obj) {
+    void test2(MinStack2 obj) {
         obj.push(512);
         obj.push(-1024);
         obj.push(-1024);
@@ -205,23 +183,58 @@ public class MinStack {
         assertEquals(512, obj.getMin());
     }
 
-    @Test
-    public void test1() {
-        test1(new MinStack1());
-        test1(new MinStack2());
-        test1(new MinStack3());
-        test1(new MinStack4());
+    static final Object[] VOID = new Object[] {};
+
+    void test1(String className) throws Exception {
+        Object outerObj = new Object() {}.getClass().getEnclosingClass().newInstance();
+        test(new String[] {className, "push", "push", "push", "getMin", "pop", "top", "getMin"},
+             new Object[][] {new Object[] {outerObj}, {-2}, {0}, {-3}, {}, {}, {}, {}},
+             new Integer[] {null, null, null, null, -3, null, 0, -2});
+
+        test(new String[] {className, "push", "push", "push", "push", "getMin", "pop", "getMin",
+                           "push", "push", "push", "getMin", "top", "pop", "pop", "getMin", "pop",
+                           "pop", "getMin", "pop", "getMin", "top"},
+             new Object[][] {new Object[] {outerObj}, {4}, {2}, {3}, {1}, {}, {}, {}, {0}, {-1},
+                             {2}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}},
+             new Integer[] {null, null, null, null, null, 1, null, 2, null, null, null, -1, 2, null,
+                            null, 0, null, null, 2, null, 4, 4});
+    }
+
+    void test(String[] methods, Object[][] args, Integer[] expected) throws Exception {
+        final String name = new Object() {}.getClass().getEnclosingClass().getSimpleName();
+        Class<?> clazz = Class.forName(name + "$" + methods[0]);
+        Constructor<?> ctor = clazz.getConstructors()[0];
+        Object obj = ctor.newInstance(args[0]);
+        for (int i = 1; i < methods.length; i++) {
+            Object[] arg = args[i];
+            Object res = null;
+            if (arg.length == 0) {
+                res = clazz.getMethod(methods[i]).invoke(obj);
+            } else if (arg.length == 1) {
+                res = clazz.getMethod(methods[i], int.class).invoke(obj, arg);
+            } else if (arg.length == 2) {
+                res = clazz.getMethod(methods[i], int.class, int.class).invoke(obj, arg);
+            }
+            if (expected[i] != null) {
+                assertEquals(expected[i], res);
+            }
+        }
     }
 
     @Test
-    public void test2() {
-        test2(new MinStack1());
-        test2(new MinStack2());
-        test2(new MinStack3());
-        test2(new MinStack4());
+    public void test1() {
+        try {
+            test1("MinStack1");
+            test1("MinStack2");
+            test1("MinStack3");
+            test1("MinStack4");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
-        org.junit.runner.JUnitCore.main("MinStack");
+        String clazz = new Object() {}.getClass().getEnclosingClass().getSimpleName();
+        org.junit.runner.JUnitCore.main(clazz);
     }
 }
