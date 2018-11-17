8579eca0efd1242ee71bde0e658b75514b6f023e
Hui Zheng <xyzdll@gmail.com>
2018-11-17 16:16:04 -0500

    add 2 more solutions for IslandNumber.java

--- leetcode/sort_search/IslandNumber.java
+++ leetcode/sort_search/IslandNumber.java
@@ -13,12 +13,12 @@ import java.util.function.Function;
 public class IslandNumber {
     private static final char LAND = '1';
     private static final char MARK = ' ';
-    private static final int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    private static final int[][] MOVES = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
 
     // Solution of Choice
     // BFS + Queue
-    // beats 20.18%(7 ms for 47 tests)
     // time complexity: O(N * M), space complexity: O(1)
+    // beats 25.69%(8 ms for 47 tests)
     public int numIslands(char[][] grid) {
         int nRow = grid.length;
         if (nRow == 0) return 0;
@@ -35,23 +35,22 @@ public class IslandNumber {
         }
         // restore grid if required
         // for (int i = 0; i < nRow; i++) {
-        //     for (int j = 0; j < nCol; j++) {
-        //         if (grid[i][j] == MARK) {
-        //             grid[i][j] = LAND;
-        //         }
-        //     }
+        // for (int j = 0; j < nCol; j++) {
+        // if (grid[i][j] == MARK) {
+        // grid[i][j] = LAND;
+        // }
+        // }
         // }
         return islands;
     }
 
-    private void markIsland(char[][] grid, int nRow, int nCol,
-                            int startX, int startY) {
+    private void markIsland(char[][] grid, int nRow, int nCol, int startX, int startY) {
         Queue<int[]> island = new LinkedList<>();
         island.offer(new int[] {startX, startY});
         grid[startX][startY] = MARK;
         while (!island.isEmpty()) {
             int[] pos = island.poll();
-            for (int[] dir : dirs) {
+            for (int[] dir : MOVES) {
                 int x = pos[0] + dir[0];
                 int y = pos[1] + dir[1];
                 if (x >= 0 && y >= 0 && x < nRow && y < nCol && grid[x][y] == LAND) {
@@ -63,8 +62,8 @@ public class IslandNumber {
     }
 
     // DFS + Recursion
-    // beats 39.89%(4 ms for 47 tests)
     // time complexity: O(N * M), space complexity: O(1)
+    // beats 33.18%(7 ms for 47 tests)
     public int numIslands2(char[][] grid) {
         int nRow = grid.length;
         if (nRow == 0) return 0;
@@ -83,10 +82,9 @@ public class IslandNumber {
         return islands;
     }
 
-    private void markIsland2(char[][] grid, int nRow, int nCol,
-                             int x, int y) {
+    private void markIsland2(char[][] grid, int nRow, int nCol, int x, int y) {
         grid[x][y] = MARK;
-        for (int[] dir : dirs) {
+        for (int[] dir : MOVES) {
             int i = x + dir[0];
             int j = y + dir[1];
             if (i >= 0 && j >= 0 && i < nRow && j < nCol && grid[i][j] == LAND) {
@@ -95,10 +93,35 @@ public class IslandNumber {
         }
     }
 
-    // Solution of Choice
+    // DFS + Recursion
+    // time complexity: O(N * M), space complexity: O(1)
+    // beats 95.19%(4 ms for 47 tests)
+    public int numIslands2_2(char[][] grid) {
+        int islands = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == '1') {
+                    islands++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return islands;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[i].length || grid[i][j] == '0') return;
+
+        grid[i][j] = '0';
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
+
     // Union Find
     // time complexity: O(N * M * log(K)), space complexity: O(N * M)
-    // beats 16.27%(8 ms for 47 tests)
+    // beats 33.18%(7 ms for 47 tests)
     public int numIslands3(char[][] grid) {
         int nRow = grid.length;
         if (nRow == 0) return 0;
@@ -108,23 +131,20 @@ public class IslandNumber {
         int islands = 0;
         for (int i = 0; i < nRow; i++) {
             for (int j = 0; j < nCol; j++) {
-                if (grid[i][j] == LAND) {
+                if (grid[i][j] == '1') {
                     root[i * nCol + j] = i * nCol + j;
                     islands++;
                 }
             }
         }
-        final int[] dx = {-1, 1, 0, 0};
-        final int[] dy = {0, 0, -1, 1};
         for (int i = 0; i < nRow; i++) {
             for (int j = 0; j < nCol; j++) {
-                if (grid[i][j] != LAND) continue;
+                if (grid[i][j] != '1') continue;
 
-                for (int k = 0; k < 4; k++) {
-                    int x = i + dx[k];
-                    int y = j + dy[k];
-                    if (x >= 0 && x < nRow && y >= 0 && y < nCol
-                        && grid[x][y] == LAND) {
+                for (int[] move : MOVES) {
+                    int x = i + move[0];
+                    int y = j + move[1];
+                    if (x >= 0 && x < nRow && y >= 0 && y < nCol && grid[x][y] == '1') {
                         int root1 = getRoot(root, i * nCol + j);
                         int root2 = getRoot(root, x * nCol + y);
                         if (root1 != root2) {
@@ -139,14 +159,61 @@ public class IslandNumber {
     }
 
     private int getRoot(int[] id, int i) {
-        while (i != id[i]) {
-            id[i] = id[id[i]];
-            i = id[i];
-        }
+        for (; i != id[i]; i = id[i] = id[id[i]]) {}
         return i;
     }
 
-    void test(Function<char[][], Integer> count, int expected, String ... s) {
+    // Solution of Choice
+    // Union Find
+    // time complexity: O(N * M * log(K)), space complexity: O(N * M)
+    // beats 33.18%(7 ms for 47 tests)
+    public int numIslands4(char[][] grid) {
+        int m = grid.length;
+        if (m == 0) return 0;
+
+        int n = grid[0].length;
+        int[] id = new int[m * n];
+        Arrays.fill(id, -1);
+        int islands = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '0') continue;
+
+                islands++;
+                for (int[] move : MOVES) {
+                    int x = i + move[0];
+                    int y = j + move[1];
+                    if (x >= 0 && x < m && y >= 0 && y < n && grid[x][y] == '1') {
+                        islands -= union(id, i * n + j, x * n + y) ? 1 : 0;
+                    }
+                }
+            }
+        }
+        return islands;
+    }
+
+    private int parent(int[] id, int x) {
+        int p = x;
+        for (; id[p] >= 0; p = id[p]) {}
+        return p;
+    }
+
+    private boolean union(int[] id, int x, int y) {
+        int px = parent(id, x);
+        int py = parent(id, y);
+        if (px == py) return false;
+
+        if (id[py] < id[px]) {
+            int tmp = px;
+            px = py;
+            py = tmp;
+        }
+        id[px] += id[py];
+        id[py] = px;
+        return true;
+    }
+
+    void test(Function<char[][], Integer> count, int expected, String... s) {
         assertEquals(expected, (int)count.apply(convert(s)));
     }
 
@@ -158,21 +225,25 @@ public class IslandNumber {
         return board;
     }
 
-    void test(int expected, String ... s) {
+    void test(int expected, String... s) {
         IslandNumber i = new IslandNumber();
         test(i::numIslands, expected, s);
         test(i::numIslands2, expected, s);
+        test(i::numIslands2_2, expected, s);
         test(i::numIslands3, expected, s);
+        test(i::numIslands4, expected, s);
     }
 
     @Test
     public void test1() {
         test(1, "111", "010", "111");
+        test(1, "1111", "1101", "1100");
         test(1, "11110", "11010", "11000", "00000");
         test(3, "11000", "11000", "00100", "00011");
     }
 
     public static void main(String[] args) {
-        org.junit.runner.JUnitCore.main("IslandNumber");
+        String clazz = new Object() {}.getClass().getEnclosingClass().getSimpleName();
+        org.junit.runner.JUnitCore.main(clazz);
     }
 }
