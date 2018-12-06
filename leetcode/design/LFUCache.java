b2fa4df1138955a53d3a36b90a16f32bc88770f8
Hui Zheng <xyzdll@gmail.com>
2018-12-05 23:55:42 -0500

    add one more solution for LFUCache.java

--- leetcode/design/LFUCache.java
+++ leetcode/design/LFUCache.java
@@ -1,5 +1,7 @@
 import java.util.*;
 
+import java.lang.reflect.Constructor;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -16,16 +18,11 @@ import static org.junit.Assert.*;
 // Follow up:
 // Could you do both operations in O(1) time complexity?
 public class LFUCache {
-    static interface ILFUCache {
-        public int get(int key);
-
-        public void put(int key, int value);
-    }
-
     static class Node<T> {
         T val;
         Node<T> prev;
         Node<T> next;
+
         Node(T val) {
             this.val = val;
         }
@@ -77,6 +74,7 @@ public class LFUCache {
         int key;
         int val;
         Node<Bucket> bucket;
+
         BucketVal(int key, int val, Node<Bucket> bucket) {
             this.key = key;
             this.val = val;
@@ -87,6 +85,7 @@ public class LFUCache {
     static class Bucket {
         int freq;
         DoublyLinkedList<BucketVal> list = new DoublyLinkedList<>();
+
         Bucket(int freq) {
             this.freq = freq;
         }
@@ -94,11 +93,11 @@ public class LFUCache {
 
     // DoublyLinkedList + Hashtable
     // beats 96.35%(163 ms for 23 tests)
-    static class LFUCache1 implements ILFUCache {
+    class LFUCache1 {
         private int capacity;
         private int count;
         DoublyLinkedList<Bucket> freqList = new DoublyLinkedList<>();
-        private Map<Integer, Node<BucketVal> > map = new HashMap<>();
+        private Map<Integer, Node<BucketVal>> map = new HashMap<>();
 
         public LFUCache1(int capacity) {
             this.capacity = capacity;
@@ -116,7 +115,7 @@ public class LFUCache {
             Node<Bucket> bucketNode = node.val.bucket;
             Bucket bucket = bucketNode.val;
             Node<Bucket> nextNode = bucketNode.next;
-            if (nextNode.val == null ||  nextNode.val.freq != bucket.freq + 1) {
+            if (nextNode.val == null || nextNode.val.freq != bucket.freq + 1) {
                 nextNode = new Node<>(new Bucket(bucket.freq + 1));
                 freqList.insertAfter(bucketNode, nextNode);
             }
@@ -147,7 +146,7 @@ public class LFUCache {
                 }
             }
             Node<Bucket> nextNode = freqList.head.next;
-            if (nextNode.val == null ||  nextNode.val.freq != 1) {
+            if (nextNode.val == null || nextNode.val.freq != 1) {
                 nextNode = new Node<>(new Bucket(1));
                 freqList.insertAfter(freqList.head, nextNode);
             }
@@ -161,10 +160,10 @@ public class LFUCache {
     // https://discuss.leetcode.com/topic/69737/java-o-1-very-easy-solution-using-3-hashmaps-and-linkedhashset
     // DoublyLinkedSet + Hashtable
     // beats 66.44%(199 ms for 23 tests)
-    static class LFUCache2 implements ILFUCache {
+    class LFUCache2 {
         Map<Integer, Integer> vals = new HashMap<>();
         Map<Integer, Integer> visits = new HashMap<>();
-        Map<Integer, Set<Integer> > lists = new HashMap<>();
+        Map<Integer, Set<Integer>> lists = new HashMap<>();
         int capacity;
         int min = -1;
 
@@ -198,9 +197,9 @@ public class LFUCache {
                 return;
             }
             if (vals.size() >= capacity) {
-                int evit = lists.get(min).iterator().next();
-                lists.get(min).remove(evit);
-                vals.remove(evit);
+                int evict = lists.get(min).iterator().next();
+                lists.get(min).remove(evict);
+                vals.remove(evict);
             }
             vals.put(key, value);
             visits.put(key, 1);
@@ -209,45 +208,154 @@ public class LFUCache {
         }
     }
 
-    static void test1(ILFUCache cache) {
-        cache.put(1, 1);
-        cache.put(2, 2);
-        assertEquals(1, cache.get(1));
-        cache.put(3, 3);    // evicts key 2
-        assertEquals(-1, cache.get(2));
-        assertEquals(3, cache.get(3));
-        cache.put(4, 4);    // evicts key 1
-        assertEquals(-1, cache.get(1));
-        assertEquals(3, cache.get(3));
-        assertEquals(4, cache.get(4));
-        cache.put(5, 5);    // evicts key 4
-        assertEquals(-1, cache.get(4));
+    // TODO: use LinkedHashMap to implement
+
+    // DoublyLinkedList + Hashtable
+    // beats 96.57%(88 ms for 23 tests)
+    class LFUCache3 {
+        private Map<Integer, Node> map = new HashMap<>();
+        private List<DoublyLinkedList> keyLists = new ArrayList<>();
+        private int capacity;
+
+        public LFUCache3(int capacity) {
+            this.capacity = capacity;
+        }
+
+        public int get(int key) {
+            Node node = map.get(key);
+            if (node == null) return -1;
+
+            visitNode(node);
+            return node.value;
+        }
+
+        public void put(int key, int value) {
+            Node node = map.get(key);
+            if (node == null) {
+                node = new Node(key);
+                if (map.size() >= capacity) {
+                    evict();
+                }
+                if (capacity > 0) {
+                    map.put(key, node);
+                }
+            }
+            node.value = value;
+            visitNode(node);
+        }
+
+        private void visitNode(Node node) {
+            if (node.visited >= 0) {
+                keyLists.get(node.visited).remove(node);
+            }
+            if (++node.visited >= keyLists.size()) {
+                keyLists.add(new DoublyLinkedList());
+            }
+            keyLists.get(node.visited).append(node);
+        }
+
+        private void evict() {
+            for (DoublyLinkedList list : keyLists) {
+                if (!list.isEmpty()) {
+                    map.remove(list.head.next.key);
+                    list.remove(list.head.next);
+                    return;
+                }
+            }
+        }
+
+        private class DoublyLinkedList {
+            Node head = new Node(0);
+            Node tail = new Node(0);
+
+            DoublyLinkedList() {
+                head.next = tail;
+                tail.prev = head;
+            }
+
+            void append(Node node) {
+                tail.prev.next = node;
+                node.prev = tail.prev;
+                tail.prev = node;
+                node.next = tail;
+            }
+
+            void remove(Node node) {
+                node.prev.next = node.next;
+                node.next.prev = node.prev;
+            }
+
+            boolean isEmpty() {
+                return head.next == tail;
+            }
+        }
+
+        private class Node {
+            int key;
+            int value;
+            int visited = -1;
+            Node prev;
+            Node next;
+
+            Node(int key) {
+                this.key = key;
+            }
+        }
     }
 
-    static void test2(ILFUCache cache) {
-        cache.put(0, 0);
-        assertEquals(-1, cache.get(0));
+    static final Object[] VOID = new Object[] {};
+
+    void test1(String className) throws Exception {
+        Object outerObj = new Object() {
+        }.getClass().getEnclosingClass().newInstance();
+        test(new String[] {className, "put", "put", "get", "put", "get", "get", "put", "get", "get",
+                           "get", "put", "get"},
+             new Object[][] {new Object[] {outerObj, 2}, {1, 1}, {2, 2}, {1}, {3, 3}, {2}, {3},
+                             {4, 4}, {1}, {3}, {4}, {5, 5}, {4}},
+             new Integer[] {null, null, null, 1, null, -1, 3, null, -1, 3, 4, null, -1});
+        test(new String[] {className, "put", "get"},
+             new Object[][] {new Object[] {outerObj, 0}, {0, 0}, {0}},
+             new Integer[] {null, null, -1});
+        test(new String[] {className, "put", "put", "put", "get", "get", "get"},
+             new Object[][] {new Object[] {outerObj, 1101}, {253, 668}, {202, 206}, {1231, 3177},
+                             {465}, {1333}, {1231}},
+             new Integer[] {null, null, null, null, -1, -1, 3177});
     }
 
-    static void test3(ILFUCache cache) {
-        cache.put(253, 668);
-        cache.put(202, 206);
-        cache.put(1231,3177);
-        assertEquals(-1, cache.get(465));
-        assertEquals(-1, cache.get(1333));
+    void test(String[] methods, Object[][] args, Integer[] expected) throws Exception {
+        final String name = new Object() {}.getClass().getEnclosingClass().getSimpleName();
+        Class<?> clazz = Class.forName(name + "$" + methods[0]);
+        Constructor<?> ctor = clazz.getConstructors()[0];
+        Object obj = ctor.newInstance(args[0]);
+        for (int i = 1; i < methods.length; i++) {
+            Object[] arg = args[i];
+            Object res = null;
+            if (arg.length == 0) {
+                res = clazz.getMethod(methods[i]).invoke(obj);
+            } else if (arg.length == 1) {
+                res = clazz.getMethod(methods[i], int.class).invoke(obj, arg);
+            } else if (arg.length == 2) {
+                res = clazz.getMethod(methods[i], int.class, int.class).invoke(obj, arg);
+            }
+            if (expected[i] != null) {
+                assertEquals(expected[i], res);
+            }
+        }
     }
 
     @Test
     public void test1() {
-        test1(new LFUCache1(2));
-        test1(new LFUCache2(2));
-        test2(new LFUCache1(0));
-        test2(new LFUCache2(0));
-        test3(new LFUCache1(1101));
-        test3(new LFUCache1(1101));
+        try {
+            test1("LFUCache1");
+            test1("LFUCache2");
+            test1("LFUCache3");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
-        org.junit.runner.JUnitCore.main("LFUCache");
+        String clazz = new Object() {}.getClass().getEnclosingClass().getSimpleName();
+        org.junit.runner.JUnitCore.main(clazz);
     }
 }
