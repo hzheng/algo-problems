8b5b7d9521c1c20820bd63ff73c636e10c5864bf
Hui Zheng <xyzdll@gmail.com>
2020-05-18 14:18:07 -0400

    add a new solution for MaxProductSubarray.java

--- leetcode/recursion_DP/MaxProductSubarray.java
+++ leetcode/recursion_DP/MaxProductSubarray.java
@@ -1,4 +1,5 @@
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 import java.util.function.Function;
@@ -75,9 +76,47 @@ public class MaxProductSubarray {
         return product;
     }
 
+    // time complexity: O(N), space complexity: O(1)
+    // beats 94.56%(1 ms for 184 tests)
+    public int maxProduct1(int[] nums) {
+        int n = nums.length;
+        if (n == 1) return nums[0];
+
+        int res = 0;
+        for (int i = 0, left = 0, leftNeg = -1, rightNeg = -1, totalNeg = 0; i <= n; i++) {
+            int curVal = (i == n) ? 0 : nums[i];
+            if (curVal > 0) continue;
+
+            if (curVal < 0) {
+                if (leftNeg < 0) {
+                    leftNeg = i;
+                }
+                rightNeg = i;
+                totalNeg++;
+                continue;
+            }
+
+            int start = left;
+            int end = i;
+            if (totalNeg % 2 == 1) {
+                int v1 = product(nums, left, leftNeg + 1);
+                int v2 = product(nums, rightNeg , i);
+                if (v1 < v2 || v1 == v2 && leftNeg != left) {
+                    end = rightNeg;
+                } else  {
+                    start = leftNeg + 1;
+                }
+            }
+            res = Math.max(res, product(nums, start, end));
+            left = i + 1; leftNeg = -1; rightNeg = -1; totalNeg = 0;
+        }
+        return res;
+    }
+
     // Solution of Choice
     // 0-D Dynamic Programming
-    // beats 49.97%(3 ms for 183 tests)
+    // time complexity: O(N), space complexity: O(1)
+    // beats 94.56%(1 ms for 184 tests)
     public int maxProduct2(int[] nums) {
         int localMax = 1;
         int localMin = 1;
@@ -118,7 +157,6 @@ public class MaxProductSubarray {
     // beats 80.08%(3 ms)
     public int maxProduct4(int[] nums) {
         int n = nums.length;
-        if (n == 0) return 0;
         if (n == 1) return nums[0];
 
         boolean isPostive = false;
@@ -153,7 +191,6 @@ public class MaxProductSubarray {
         return max;
     }
 
-    // Solution of Choice
     // 0-D Dynamic Programming
     // beats 49.97%(3 ms for 183 tests)
     public int maxProduct5(int[] nums) {
@@ -170,6 +207,20 @@ public class MaxProductSubarray {
         return max;
     }
 
+    // Solution of Choice
+    // 0-D Dynamic Programming
+    // time complexity: O(N), space complexity: O(1)
+    // beats 94.56%(1 ms for 184 tests)
+    public int maxProduct5_2(int[] nums) {
+        int res = nums[0];
+        for (int i = 0, left = 0, right = 0, n = nums.length; i < n; i++) {
+            left =  (left == 0 ? 1 : left) * nums[i];
+            right =  (right == 0 ? 1 : right) * nums[n - 1 - i];
+            res = Math.max(res, Math.max(left, right));
+        }
+        return res;
+    }
+
     void test(Function<int[], Integer> maxProduct, int expected, int ... nums) {
         assertEquals(expected, (int)maxProduct.apply(nums));
     }
@@ -177,21 +228,27 @@ public class MaxProductSubarray {
     void test(int expected, int ... nums) {
         MaxProductSubarray m = new MaxProductSubarray();
         test(m::maxProduct, expected, nums);
+        test(m::maxProduct1, expected, nums);
         test(m::maxProduct2, expected, nums);
         test(m::maxProduct3, expected, nums);
         test(m::maxProduct4, expected, nums);
         test(m::maxProduct5, expected, nums);
+        test(m::maxProduct5_2, expected, nums);
     }
 
     @Test
     public void test1() {
         test(0, -4, 0, -2);
         test(-4, -4);
+        test(0, 0);
+        test(10, 10);
         test(3, -2, 3);
         test(2, 0, 2);
         test(6, 2, 3, -2, 4);
         test(12, -3, -4);
         test(12, -3, -4, 0, -2, -5);
+        test(1, 0, -3, 1, 1);
+        test(1, -3, 0, 1, -2);
         test(4320, -2, -3, 8, 9, 10, 0, 3, 8, 9, -2, 5, 3);
     }
 
