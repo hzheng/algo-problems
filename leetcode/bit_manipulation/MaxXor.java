94a473ae8d356ae13e79704fe97e7534a217d021
Hui Zheng <xyz.dll@gmail.com>
2020-12-27 23:19:50 -0500

    improve MaxXor.java

--- leetcode/bit_manipulation/MaxXor.java
+++ leetcode/bit_manipulation/MaxXor.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 // LC421: https://leetcode.com/problems/maximum-xor-of-two-numbers-in-an-array/
@@ -10,37 +11,29 @@ import static org.junit.Assert.*;
 public class MaxXor {
     // Trie
     // time complexity: O(N), space complexity: O(N)
-    // beats 88.60%(76 ms for 29 tests)
+    // 26 ms(91.01%), 47.9 MB(34.08%) for 39 tests
     public int findMaximumXOR(int[] nums) {
-        int totalOr = or(nums);
-        if (totalOr == 0) return 0;
-
-        int hiBit = 30;
-        // this calculation is just for efficiency
-        for (int mask = (1 << hiBit); (mask & totalOr) == 0; mask >>= 1, hiBit--) {}
-
+        int hiBit = getHighestBit(nums);
         BinaryNode root = new BinaryNode();
         for (int num : nums) {
             BinaryNode cur = root;
             for (int bitPos = hiBit; bitPos >= 0; bitPos--) {
-                cur = cur.set((num >>> bitPos) & 1);
+                cur = cur.getChild((num >>> bitPos) & 1);
             }
         }
         int max = 0;
         for (int num : nums) {
             BinaryNode cur = root;
-            int val = 0;
+            int xor = 0;
             for (int bitPos = hiBit; bitPos >= 0; bitPos--) {
-                int index = ((num >>> bitPos) & 1) ^ 1;
-                if (cur.children[index] == null) {
+                int index = (num >>> bitPos) & 1;
+                if (cur.children[index ^ 1] != null) { // try to maximize xor
                     index ^= 1;
+                    xor |= 1 << bitPos;
                 }
                 cur = cur.children[index];
-                if (index > 0) {
-                    val |= 1 << bitPos;
-                }
             }
-            max = Math.max(max, num ^ val);
+            max = Math.max(max, xor);
         }
         return max;
     }
@@ -48,7 +41,7 @@ public class MaxXor {
     private static class BinaryNode {
         BinaryNode[] children = new BinaryNode[2];
 
-        BinaryNode set(int bit) {
+        BinaryNode getChild(int bit) {
             if (children[bit] == null) {
                 children[bit] = new BinaryNode();
             }
@@ -56,25 +49,24 @@ public class MaxXor {
         }
     }
 
-    private int or(int[] nums) {
-        int res = 0;
+    private int getHighestBit(int[] nums) {
+        int totalOr = 0;
         for (int num : nums) {
-            res |= num;
+            totalOr |= num;
         }
-        return res;
+        if (totalOr == 0) { return 0; }
+
+        int hiBit = Integer.SIZE - 2;
+        // this calculation is just for efficiency
+        for (int mask = (1 << hiBit); (mask & totalOr) == 0; mask >>= 1, hiBit--) {}
+        return hiBit;
     }
 
     // Trie
     // time complexity: O(N), space complexity: O(N)
-    // beats 91.22%(66 ms for 29 tests)
+    // 20 ms(95.69%), 47.5 MB(39.33%) for 39 tests
     public int findMaximumXOR2(int[] nums) {
-        int totalOr = or(nums);
-        if (totalOr == 0) return 0;
-
-        int hiBit = 30;
-        // this calculation is just for efficiency
-        for (int mask = (1 << hiBit); (mask & totalOr) == 0; mask >>= 1, hiBit--) {}
-
+        int hiBit = getHighestBit(nums);
         int max = 0;
         BinaryNode root = new BinaryNode();
         for (int num : nums) {
@@ -83,7 +75,7 @@ public class MaxXor {
             BinaryNode complementNode = root;
             for (int bitPos = hiBit; bitPos >= 0; bitPos--) {
                 int index = (num >>> bitPos) & 1;
-                complementNode = complementNode.set(index ^ 1);
+                complementNode = complementNode.getChild(index ^ 1);
                 if (cur.children[index] != null) {
                     curMax |= (1 << bitPos);
                     cur = cur.children[index];
@@ -96,16 +88,11 @@ public class MaxXor {
         return max;
     }
 
-    // Hash Table
-    // beats 77.81%(97 ms for 29 tests)
+    // Set
+    // time complexity: O(N), space complexity: O(N)
+    // 71 ms(60.30%), 40.2 MB(66.10%) for 39 tests
     public int findMaximumXOR3(int[] nums) {
-        int totalOr = or(nums);
-        if (totalOr == 0) return 0;
-
-        int hiBit = 30;
-        // this calculation is just for efficiency
-        for (int mask = (1 << hiBit); (mask & totalOr) == 0; mask >>= 1, hiBit--) {}
-
+        int hiBit = getHighestBit(nums);
         int max = 0;
         for (int i = hiBit, mask = 1 << i; i >= 0; mask |= (1 << --i)) {
             Set<Integer> prefixSet = new HashSet<>();
@@ -129,18 +116,19 @@ public class MaxXor {
         assertEquals(expected, findMaximumXOR3(nums));
     }
 
-    @Test
-    public void test1() {
-        // test(new int[] {Integer.MAX_VALUE, 1}, 2147483646);
-        // test(new int[] {Integer.MAX_VALUE, 2, 1}, 2147483646);
-        // test(new int[] {0}, 0);
+    @Test public void test1() {
         test(new int[] {0b11, 0b1010, 0b101, 0b11001, 0b10, 0b1000}, 0b11100);
         test(new int[] {3, 10, 5, 25, 2, 8}, 28);
         test(new int[] {3, 10, 5, 25, 2, 8, 7, 8, 11, 23}, 31);
         test(new int[] {300, 109, 35, 25, 12, 18, 73, 68, 101, 213}, 505);
+        test(new int[] {0}, 0);
+        test(new int[] {Integer.MAX_VALUE, 1}, 2147483646);
+        test(new int[] {Integer.MAX_VALUE, 2, 1}, 2147483646);
     }
 
     public static void main(String[] args) {
-        org.junit.runner.JUnitCore.main("MaxXor");
+        String clazz = new Object() {
+        }.getClass().getEnclosingClass().getSimpleName();
+        org.junit.runner.JUnitCore.main(clazz);
     }
 }
