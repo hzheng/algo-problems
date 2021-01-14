a8164bf6b111288afb33f836043484136920be18
Hui Zheng <xyz.dll@gmail.com>
2021-01-14 17:32:50 -0500

    add two methods in Utils

--- lib/common/Utils.java
+++ lib/common/Utils.java
@@ -20,7 +20,7 @@ public class Utils {
         return cloned;
     }
 
-    public static<T> T[][] clone(T[][] items) {
+    public static <T> T[][] clone(T[][] items) {
         T[][] cloned = items.clone();
         for (int i = 0; i < items.length; i++) {
             cloned[i] = items[i].clone();
@@ -28,7 +28,7 @@ public class Utils {
         return cloned;
     }
 
-    public static<T> List<List<T>> toList(T[][] items) {
+    public static <T> List<List<T>> toList(T[][] items) {
         List<List<T>> res = new ArrayList<>();
         for (T[] item : items) {
             res.add(Arrays.asList(item));
@@ -40,7 +40,7 @@ public class Utils {
         public int compare(int[] a, int[] b) {
             int len = Math.min(a.length, b.length);
             int i = 0;
-            for (; i < len && (a[i] == b[i]); i++);
+            for (; i < len && (a[i] == b[i]); i++) {}
             return (i == len) ? a.length - b.length : a[i] - b[i];
         }
     }
@@ -49,7 +49,7 @@ public class Utils {
         public int compare(List<String> a, List<String> b) {
             int len = Math.min(a.size(), b.size());
             int i = 0;
-            for (; i < len && a.get(i).equals(b.get(i)); i++);
+            for (; i < len && a.get(i).equals(b.get(i)); i++) {}
             return (i == len) ? a.size() - b.size() : a.get(i).compareTo(b.get(i));
         }
     }
@@ -58,25 +58,25 @@ public class Utils {
         public int compare(String[] a, String[] b) {
             int len = Math.min(a.length, b.length);
             int i = 0;
-            for (; i < len && a[i].equals(b[i]); i++);
+            for (; i < len && a[i].equals(b[i]); i++) {}
             return (i == len) ? a.length - b.length : a[i].compareTo(b[i]);
         }
     }
 
-    public static String[][] toStrArray(List<List<String> > listOfList) {
+    public static String[][] toStrArray(List<List<String>> listOfList) {
         return toArray(listOfList, String.class);
     }
 
-    public static Integer[][] toIntArray(List<List<Integer> > listOfList) {
+    public static Integer[][] toIntArray(List<List<Integer>> listOfList) {
         return toArray(listOfList, Integer.class);
     }
 
     public static int[] toArray(List<Integer> list) {
-        return list.stream().mapToInt(i->i).toArray();
+        return list.stream().mapToInt(i -> i).toArray();
     }
 
-    public static int[][] toInts(List<List<Integer> > listOfList) {
-        return listOfList.stream().map(i->toArray(i)).toArray(int[][]::new);
+    public static int[][] toInts(List<List<Integer>> listOfList) {
+        return listOfList.stream().map(i -> toArray(i)).toArray(int[][]::new);
     }
 
     public static int[][] sort(int[][] arrays) {
@@ -84,8 +84,8 @@ public class Utils {
         return arrays;
     }
 
-    public static int[][] toSortedInts(List<List<Integer> > listOfList) {
-        int[][] res = listOfList.stream().map(i->toArray(i)).toArray(int[][]::new);
+    public static int[][] toSortedInts(List<List<Integer>> listOfList) {
+        int[][] res = listOfList.stream().map(i -> toArray(i)).toArray(int[][]::new);
         return sort(res);
     }
 
@@ -94,13 +94,13 @@ public class Utils {
         return arrays;
     }
 
-    public static String[][] toSortedStrs(List<List<String> > listOfList) {
+    public static String[][] toSortedStrs(List<List<String>> listOfList) {
         String[][] res = toStrArray(listOfList);
         return sort(res);
     }
 
-    @SuppressWarnings("unchecked")
-    public static <T> T[][] toArray(List<List<T>> listOfList, Class<T> clazz) {
+    @SuppressWarnings("unchecked") public static <T> T[][] toArray(List<List<T>> listOfList,
+                                                                   Class<T> clazz) {
         int size = listOfList.size();
         T[][] array = (T[][])Array.newInstance(clazz, size, 0);
         for (int i = 0; i < size; i++) {
@@ -110,5 +110,28 @@ public class Utils {
             }
         }
         return array;
+    }
+
+    public static int[] readIntArray(String str) {
+        String[] arr = str.strip().split("[\\[,\\]]");
+        int[] res = new int[arr.length];
+        int i = 0;
+        for (String a : arr) {
+            a = a.strip();
+            if (!a.isEmpty()) {
+                res[i++] = Integer.parseInt(a);
+            }
+        }
+        return Arrays.copyOfRange(res, 0, i);
+    }
+
+    public static int[][] readInt2Array(String str) {
+        String[] arr = str.strip().split(",(?= *\\[)");
+        int[][] res = new int[arr.length][];
+        int i = 0;
+        for (String a : arr) {
+            res[i++] = readIntArray(a);
+        }
+        return res;
     }
 }
