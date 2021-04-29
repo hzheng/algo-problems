bf9ddd95c1eb6b2e907c3863c45eed12dfd17c91
Hui Zheng <xyz.dll@gmail.com>
2021-04-28 21:59:12 -0400

    minor change

--- leetcode/array_string/SumBase.java
+++ leetcode/array_string/SumBase.java
@@ -2,8 +2,10 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-// LC1837: https://leetcode.com/problems/sum-of-digits-in-base-k/Given an integer n (in base 10)
-// and a base k, return the sum of the digits of n after converting n from base 10 to base k.
+// LC1837: https://leetcode.com/problems/sum-of-digits-in-base-k/
+//
+// Given an integer n (in base 10) and a base k, return the sum of the digits of n after converting
+// n from base 10 to base k.
 // After converting, each digit should be interpreted as a base 10 number, and the sum should be
 // returned in base 10.
 // Given a string S, you are allowed to convert it to a palindrome by adding
