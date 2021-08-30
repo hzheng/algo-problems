59b4d94c86cd1dbd4994b2b554dfbd56de110d16
Hui Zheng <xyz.dll@gmail.com>
2021-08-29 23:01:59 -0400

    add one more solution for InsertInterval.java

--- leetcode/array_string/InsertInterval.java
+++ leetcode/array_string/InsertInterval.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 import common.Interval;
@@ -48,14 +49,16 @@ public class InsertInterval {
     // find the leftmost index of interval that covers or is behind the target
     private int getIndex(List<Interval> intervals, int offset, int target) {
         int n = intervals.size();
-        if (n == 0) return 0;
+        if (n == 0)
+            return 0;
 
         int high = n - 1;
         int low = offset;
         while (low <= high) {
             int mid = (low + high) >>> 1;
             int midInterval = intervals.get(mid).start;
-            if (midInterval == target) return mid;
+            if (midInterval == target)
+                return mid;
 
             if (midInterval > target) {
                 high = mid - 1;
@@ -63,11 +66,11 @@ public class InsertInterval {
                 low = mid + 1;
             }
         }
-        if (high < 0) return 0;
+        if (high < 0)
+            return 0;
         return (intervals.get(high).end >= target) ? high : high + 1;
     }
 
-    // Solution of Choice
     // http://www.jiuzhang.com/solutions/insert-interval/
     // beats 70.43%(4 ms)
     public List<Interval> insert2(List<Interval> intervals, Interval newInterval) {
@@ -88,19 +91,38 @@ public class InsertInterval {
         return res;
     }
 
-    @FunctionalInterface
-    interface Function<A, B, C> {
-        public C apply(A a, B b);
+    // Solution of Choice
+    public List<Interval> insert3(List<Interval> intervals, Interval newInterval) {
+        List<Interval> res = new ArrayList<>();
+        int i = 0;
+        int start = newInterval.start;
+        int end = newInterval.end;
+        for (; i < intervals.size() && intervals.get(i).end < start; i++) {
+            res.add(intervals.get(i));
+        }
+        for (; i < intervals.size() && intervals.get(i).start <= end; i++) {
+            start = Math.min(start, intervals.get(i).start);
+            end = Math.max(end, intervals.get(i).end);
+        }
+        res.add(new Interval(start, end));
+        for (; i < intervals.size(); i++) {
+            res.add(intervals.get(i));
+        }
+        return res;
     }
 
-    void test(Function<List<Interval>, Interval, List<Interval> > insert,
-              String name, int[] intervals, int[] newInterval, int[] expected) {
+    @FunctionalInterface interface Function<A, B, C> {
+        C apply(A a, B b);
+    }
+
+    void test(Function<List<Interval>, Interval, List<Interval>> insert, String name,
+              int[] intervals, int[] newInterval, int[] expected) {
         List<Interval> intervalList = new ArrayList<>();
         for (int i = 0; i < intervals.length; i += 2) {
             intervalList.add(new Interval(intervals[i], intervals[i + 1]));
         }
-        List<Interval> inserted = insert.apply(
-            intervalList, new Interval(newInterval[0], newInterval[1]));
+        List<Interval> inserted =
+                insert.apply(intervalList, new Interval(newInterval[0], newInterval[1]));
         int i = 0;
         int[] res = new int[expected.length];
         for (Interval interval : inserted) {
@@ -110,14 +132,14 @@ public class InsertInterval {
         assertArrayEquals(expected, res);
     }
 
-    void test(int[] intervals, int[] newInterval, int ... expected) {
+    void test(int[] intervals, int[] newInterval, int... expected) {
         InsertInterval i = new InsertInterval();
         test(i::insert, "insert", intervals, newInterval, expected);
         test(i::insert2, "insert2", intervals, newInterval, expected);
+        test(i::insert3, "insert3", intervals, newInterval, expected);
     }
 
-    @Test
-    public void test1() {
+    @Test public void test1() {
         test(new int[] {1, 5}, new int[] {6, 7}, 1, 5, 6, 7);
         test(new int[] {1, 5}, new int[] {0, 3}, 0, 5);
         test(new int[] {1, 5}, new int[] {2, 3}, 1, 5);
@@ -127,11 +149,12 @@ public class InsertInterval {
         test(new int[] {2, 3, 6, 8}, new int[] {3, 5}, 2, 5, 6, 8);
         test(new int[] {2, 3, 6, 8}, new int[] {3, 6}, 2, 8);
         test(new int[] {1, 3, 6, 9}, new int[] {2, 5}, 1, 5, 6, 9);
-        test(new int[] {1, 2, 3, 5, 6, 7, 8, 10, 12, 16},
-             new int[] {4, 9}, 1, 2, 3, 10, 12, 16);
+        test(new int[] {1, 2, 3, 5, 6, 7, 8, 10, 12, 16}, new int[] {4, 9}, 1, 2, 3, 10, 12, 16);
     }
 
     public static void main(String[] args) {
-        org.junit.runner.JUnitCore.main("InsertInterval");
+        String clazz = new Object() {
+        }.getClass().getEnclosingClass().getSimpleName();
+        org.junit.runner.JUnitCore.main(clazz);
     }
 }
