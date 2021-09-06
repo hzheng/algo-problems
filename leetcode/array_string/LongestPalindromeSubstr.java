56172152fac122bda5a60eb3a128bba4cafdf14b
Hui Zheng <xyz.dll@gmail.com>
2021-09-06 13:12:11 -0400

    add one more solution for LongestPalindromeSubstr.java

--- leetcode/array_string/LongestPalindromeSubstr.java
+++ leetcode/array_string/LongestPalindromeSubstr.java
@@ -1,26 +1,30 @@
 import java.util.Arrays;
 
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 // LC005: https://leetcode.com/problems/longest-palindromic-substring/
 //
-// Given a string S, find the longest palindromic substring. Assume the maximum
-// length of S is 1000, and there exists 1 unique longest palindromic substring.
+// Given a string s, return the longest palindromic substring in s.
+//
+// Constraints:
+// 1 <= s.length <= 1000
+// s consist of only digits and English letters.
 public class LongestPalindromeSubstr {
     // Dynamic Programming
     // time complexity: O(N ^ 2), space complexity: O(N ^ 2)
     // beats 35.67%(72 ms for 103 tests)
     public String longestPalindrome(String s) {
         int len = s.length();
-        if (len == 0) return "";
+        if (len == 0) {return "";}
 
         int[][] dp = new int[len][len];
         int maxLen = 0;
         int maxIndex = 0;
         for (int i = 0; i < len; i++) {
             for (int j = 0; j < len; j++) {
-                if (s.charAt(i) != s.charAt(len - 1 - j)) continue;
+                if (s.charAt(i) != s.charAt(len - 1 - j)) {continue;}
 
                 if (i == 0 || j == 0) {
                     dp[i][j] = 1;
@@ -47,10 +51,33 @@ public class LongestPalindromeSubstr {
         int maxIndex = 0;
         for (int d = 0; d < n; d++) {
             for (int i = 0, j = i + d; j < n; i++, j++) {
-                if (s.charAt(i) != s.charAt(j)) continue;
+                if (s.charAt(i) != s.charAt(j)) {continue;}
 
                 dp[i][j] = (j - i) < 2 || dp[i + 1][j - 1];
                 if (dp[i][j] && (j - i + 1 > maxLen)) {
+                    maxLen = j - i + 1;
+                    maxIndex = i;
+                }
+            }
+        }
+        return s.substring(maxIndex, maxIndex + maxLen);
+    }
+
+    // Solution of Choice
+    // Dynamic Programming
+    // time complexity: O(N ^ 2), space complexity: O(N ^ 2)
+    // 174 ms(36.25%), 43.3 MB(25.00%) for 177 tests
+    public String longestPalindrome_3(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int maxIndex = 0;
+        int maxLen = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                if (s.charAt(i) != s.charAt(j)) {continue;}
+
+                dp[i][j] = (j - i) < 2 || dp[i + 1][j - 1];
+                if (dp[i][j] && j - i + 1 > maxLen) {
                     maxLen = j - i + 1;
                     maxIndex = i;
                 }
@@ -74,7 +101,7 @@ public class LongestPalindromeSubstr {
                 break;
             }
             i--;
-            for (int k = j;; i--, k++) {
+            for (int k = j; ; i--, k++) {
                 if (i < 0 || k >= len || s.charAt(i) != s.charAt(k)) {
                     if (k - i - 1 > maxLen) {
                         maxLen = k - i - 1;
@@ -88,7 +115,7 @@ public class LongestPalindromeSubstr {
     }
 
     // time complexity: O(N ^ 2), space complexity: O(1)
-    // beats 94.26%(11 ms for 103 tests)
+    // 42 ms(50.98%), 40.0 MB(40.25%) for 177 tests
     public String longestPalindrome3(String s) {
         int start = 0;
         int end = -1;
@@ -105,7 +132,7 @@ public class LongestPalindromeSubstr {
     }
 
     private int expandAroundCenter(String s, int left, int right) {
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+        for (int n = s.length(); left >= 0 && right < n && s.charAt(left) == s.charAt(right); ) {
             left--;
             right++;
         }
@@ -118,8 +145,6 @@ public class LongestPalindromeSubstr {
     // time complexity: O(N), space complexity: O(N)
     // beats 99.58%(9 ms for 103 tests)
     public String longestPalindrome4(String s) {
-        if (s.length() == 0) return "";
-
         char[] s2 = addBoundaries(s.toCharArray());
         int[] p = new int[s2.length]; // max palindrome at i
         int center = 0; // center of the max palindrome currently known
@@ -173,7 +198,7 @@ public class LongestPalindromeSubstr {
     }
 
     private String removeBoundaries(char[] cs) {
-        if (cs.length < 3) return "";
+        if (cs.length < 3) {return "";}
 
         char[] cs2 = new char[(cs.length - 1) / 2];
         for (int i = 0; i < cs2.length; i++) {
@@ -199,9 +224,9 @@ public class LongestPalindromeSubstr {
     }
 
     private boolean isPalindrome(String s, int begin, int end) {
-        if (begin < 0) return false;
+        if (begin < 0) {return false;}
         while (begin < end) {
-            if (s.charAt(begin++) != s.charAt(end--)) return false;
+            if (s.charAt(begin++) != s.charAt(end--)) {return false;}
         }
         return true;
     }
@@ -211,15 +236,15 @@ public class LongestPalindromeSubstr {
     void test(String s, String expected) {
         assertEquals(expected, longestPalindrome(s));
         assertEquals(expected, longestPalindrome_2(s));
+        assertEquals(expected, longestPalindrome_3(s));
         assertEquals(expected, longestPalindrome2(s));
         assertEquals(expected, longestPalindrome3(s));
         assertEquals(expected, longestPalindrome4(s));
         assertEquals(expected, longestPalindrome5(s));
     }
 
-    @Test
-    public void test1() {
-        test("", "");
+    @Test public void test1() {
+        //        test("", "");
         test("c", "c");
         // test("ab", "a"); // no unique palindrom, answers vary
         test("aab", "aa");
@@ -233,7 +258,8 @@ public class LongestPalindromeSubstr {
     }
 
     public static void main(String[] args) {
-        String clazz = new Object() {}.getClass().getEnclosingClass().getSimpleName();
+        String clazz = new Object() {
+        }.getClass().getEnclosingClass().getSimpleName();
         org.junit.runner.JUnitCore.main(clazz);
     }
 }
