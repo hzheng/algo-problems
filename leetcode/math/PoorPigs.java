5d124dc13da993bd2726875b226e2a3c55004180
Hui Zheng <xyz.dll@gmail.com>
2021-09-20 10:55:20 -0400

    minor change

--- leetcode/math/PoorPigs.java
+++ leetcode/math/PoorPigs.java
@@ -1,19 +1,40 @@
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
-// LC
+// LC458: https://leetcode.com/problems/poor-pigs/
+//
+// There are buckets buckets of liquid, where exactly one of the buckets is poisonous. To figure
+// out which one is poisonous, you feed some number of (poor) pigs the liquid to see whether they
+// will die or not. Unfortunately, you only have minutesToTest minutes to determine which bucket is
+// poisonous.
+//
+// You can feed the pigs according to these steps:
+// Choose some live pigs to feed.
+// For each pig, choose which buckets to feed it. The pig will consume all the chosen buckets
+// simultaneously and will take no time.
+// Wait for minutesToDie minutes. You may not feed any other pigs during this time.
+// After minutesToDie minutes have passed, any pigs that have been fed the poisonous bucket will
+// die, and all others will survive.
+// Repeat this process until you run out of time.
+// Given buckets, minutesToDie, and minutesToTest, return the minimum number of pigs needed to
+// figure out which bucket is poisonous within the allotted time.
+//
+// Constraints:
+// 1 <= buckets <= 1000
+// 1 <= minutesToDie <= minutesToTest <= 100
 public class PoorPigs {
     // Dynamic Programming
     // beats 100.00%(0 ms for 6 tests)
     public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
-        if (buckets < 2) return 0;
+        if (buckets < 2) {return 0;}
 
-        int times = minutesToTest / minutesToDie; 
+        int times = minutesToTest / minutesToDie;
         for (int pigs = 1; ; pigs++) {
-            if (maxBucket(pigs, times) >= buckets) return pigs;
+            if (maxBucket(pigs, times) >= buckets) {return pigs;}
         }
     }
-    
+
     private int maxBucket(int pigs, int times) {
         int[][] dp = new int[pigs + 1][times + 1];
         for (int i = 1; i <= times; i++) {
@@ -46,11 +67,11 @@ public class PoorPigs {
     // each pig control one dimension, each round correspond one line(row)
     // beats 100.00%(0 ms for 6 tests)
     public int poorPigs2(int buckets, int minutesToDie, int minutesToTest) {
-        if (buckets < 2) return 0;
+        if (buckets < 2) {return 0;}
 
-        int times = minutesToTest / minutesToDie; 
+        int times = minutesToTest / minutesToDie;
         for (int pigs = 1; ; pigs++) {
-            if (Math.pow(times + 1, pigs) >= buckets) return pigs;
+            if (Math.pow(times + 1, pigs) >= buckets) {return pigs;}
         }
     }
 
@@ -66,8 +87,7 @@ public class PoorPigs {
         assertEquals(expected, poorPigs3(buckets, minutesToDie, minutesToTest));
     }
 
-    @Test
-    public void test() {
+    @Test public void test() {
         test(1, 1, 1, 0);
 
         test(1000, 15, 60, 5);
@@ -80,8 +100,8 @@ public class PoorPigs {
     }
 
     public static void main(String[] args) {
-        String clazz =
-            new Object() {}.getClass().getEnclosingClass().getSimpleName();
+        String clazz = new Object() {
+        }.getClass().getEnclosingClass().getSimpleName();
         org.junit.runner.JUnitCore.main(clazz);
     }
 }
