4201e93173ac23ba3e624fc47fd659b6e2690df6
Hui Zheng <xyz.dll@gmail.com>
2021-10-07 23:46:19 -0400

    minor improvement

--- leetcode/recursion_DP/MaxLength.java
+++ leetcode/recursion_DP/MaxLength.java
@@ -13,37 +13,35 @@ import static org.junit.Assert.*;
 // 1 <= arr[i].length <= 26
 // arr[i] contains only lower case English letters.
 public class MaxLength {
-    // Recursion + Dynamic Programming(Top-Down)
-    // 2 ms(95.30%), 34.5 MB(100%) for 83 tests
+    // Recursion + Dynamic Programming(Top-Down) + Set + Bit Manipulation
+    // 2 ms(93.44%), 36.7 MB(87.51%) for 89 tests
     public int maxLength(List<String> arr) {
         Set<Integer> cand = new HashSet<>();
         for (String a : arr) {
-            int code = encode(a);
-            if (code != 0) {
-                cand.add(code);
-            }
+            cand.add(encode(a));
         }
+        cand.remove(0);
         return dfs(cand.toArray(new Integer[0]), 0, 0, new int[1]);
     }
 
-    private int dfs(Integer[] candidates, int index, int sum, int[] res) {
-        if (res[0] == 26) { return res[0]; }
+    private int dfs(Integer[] candidates, int index, int mask, int[] res) {
+        if (res[0] == 26) {return res[0];}
 
         if (index == candidates.length) {
-            return res[0] = Math.max(res[0], Integer.bitCount(sum));
+            return res[0] = Math.max(res[0], Integer.bitCount(mask));
         }
 
         int cur = candidates[index];
-        if ((cur & sum) == 0) {
-            dfs(candidates, index + 1, sum | cur, res);
+        if ((cur & mask) == 0) {
+            dfs(candidates, index + 1, mask | cur, res);
         }
-        return dfs(candidates, index + 1, sum, res);
+        return dfs(candidates, index + 1, mask, res);
     }
 
     private int encode(String word) {
         int[] count = new int[26];
         for (char c : word.toCharArray()) {
-            if (++count[c - 'a'] > 1) { return 0; }
+            if (++count[c - 'a'] > 1) {return 0;}
         }
         int res = 0;
         for (int i = count.length - 1; i >= 0; i--) {
@@ -54,8 +52,8 @@ public class MaxLength {
         return res;
     }
 
-    // List
-    // 6 ms(81.87%), 35.9 MB(100%) for 83 tests
+    // Bit Manipulation
+    // 6 ms(83.88%), 38.8 MB(64.41%) for 89 tests
     public int maxLength2(List<String> arr) {
         List<Integer> candidates = new ArrayList<>();
         candidates.add(0);
@@ -65,7 +63,7 @@ public class MaxLength {
             int code = 0;
             for (char c : s.toCharArray()) {
                 int mask = 1 << (c - 'a');
-                if ((code & mask) > 0) { continue outer; }
+                if ((code & mask) > 0) {continue outer;}
                 code |= mask;
             }
             for (int i = candidates.size() - 1; i >= 0; i--) {
@@ -86,13 +84,15 @@ public class MaxLength {
 
     // test
     @Test public void test() {
-        test(new String[]{"un", "iq", "ue"}, 4);
-        test(new String[]{"cha", "r", "act", "ers"}, 6);
-        test(new String[]{"abcdefghijklmnopqrstuvwxyz"}, 26);
+        test(new String[] {"un", "iq", "ue"}, 4);
+        test(new String[] {"cha", "r", "act", "ers"}, 6);
+        test(new String[] {"cha", "r", "act", "ss", "ers"}, 6);
+        test(new String[] {"abcdefghijklmnopqrstuvwxyz"}, 26);
     }
 
     public static void main(String[] args) {
-        String clazz = new Object() {}.getClass().getEnclosingClass().getSimpleName();
+        String clazz = new Object() {
+        }.getClass().getEnclosingClass().getSimpleName();
         org.junit.runner.JUnitCore.main(clazz);
     }
 }
